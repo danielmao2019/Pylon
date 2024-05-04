fc85faf643d0c0c1c0432cc0bb3ae32f8ee79655
Daniel Mao <danielmao2019@gmail.com>
2024-05-04 16:07:44 -0400

    [Datasets][Transforms] Huge upgrades
    1. Re-implement BaseTransform to handle multiple args.
    2. Let all concrete transforms inherit from BaseTransform.
    3. Implement Compose class.

--- datasets/transforms/__init__.py
+++ datasets/transforms/__init__.py
@@ -2,6 +2,7 @@
 DATASETS.TRANSFORMS API
 """
 from datasets.transforms.base_transform import BaseTransform
+from datasets.transforms.compose import Compose
 from datasets.transforms import resize
 from datasets.transforms.normalize_image import NormalizeImage
 from datasets.transforms.normalize_depth import NormalizeDepth
@@ -10,6 +11,7 @@ from datasets.transforms.normalize_depth import NormalizeDepth
 
 __all__ = (
     'BaseTransform',
+    'Compose',
     'resize',
     'NormalizeImage',
     'NormalizeDepth',
