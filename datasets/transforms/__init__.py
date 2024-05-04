187135216d5c5a3aa79d8b5281c323637d975868
Daniel Mao <danielmao2019@gmail.com>
2024-05-04 15:05:10 -0400

    [Datasets][Transforms] Restructure datasets.transforms module

--- datasets/transforms/__init__.py
+++ datasets/transforms/__init__.py
@@ -2,16 +2,15 @@
 DATASETS.TRANSFORMS API
 """
 from datasets.transforms.base_transform import BaseTransform
+from datasets.transforms import resize
 from datasets.transforms.normalize_image import NormalizeImage
 from datasets.transforms.normalize_depth import NormalizeDepth
-from datasets.transforms.resize_bboxes import ResizeBBoxes
-from datasets.transforms.resize_normal_estimation import ResizeNormalEstimation
+
 
 
 __all__ = (
     'BaseTransform',
+    'resize',
     'NormalizeImage',
     'NormalizeDepth',
-    'ResizeBBoxes',
-    'ResizeNormalEstimation',
 )
