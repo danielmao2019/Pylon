8ec7ca906d517544a0e14fcbd63a227d707c40e6
Daniel Mao <danielmao2019@gmail.com>
2024-05-04 15:05:10 -0400

    [Datasets][Transforms] Restructure datasets.transforms module

---
+++ datasets/transforms/resize/__init__.py
@@ -0,0 +1,11 @@
+"""
+DATASETS.TRANSFORMS.RESIZE API
+"""
+from datasets.transforms.resize.bboxes import ResizeBBoxes
+from datasets.transforms.resize.normals import ResizeNormals
+
+
+__all__ = (
+    'ResizeBBoxes',
+    'ResizeNormals',
+)
