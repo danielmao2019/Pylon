3befaece8eefc0491a4a4613ffd501dd1b04a00f
Daniel Mao <danielmao2019@gmail.com>
2024-05-08 22:42:26 -0400

    [Criteria, Metrics] Allow y_pred and y_true to be different resolution

--- utils/input_checks/tensor_types.py
+++ utils/input_checks/tensor_types.py
@@ -20,7 +20,6 @@ def check_semantic_segmentation(y_pred: Any, y_true: Any) -> Tuple[torch.Tensor,
     check_semantic_segmentation_pred(tensor=y_pred)
     check_semantic_segmentation_true(tensor=y_true)
     assert y_pred.shape[0] == y_true.shape[0], f"{y_pred.shape=}, {y_true.shape=}"
-    assert y_pred.shape[-2:] == y_true.shape[-2:], f"{y_pred.shape=}, {y_true.shape=}"
     return y_pred, y_true
 
 
