ff69235825281726a1750f136a7caf7064f1fb45
Daniel Mao <danielmao2019@gmail.com>
2024-05-14 21:49:58 -0400

    [Datasets][Transforms] Restructure normalize transforms

---
+++ datasets/transforms/normalize/__init__.py
@@ -0,0 +1,11 @@
+"""
+DATASETS.TRANSFORMS.NORMALIZE API
+"""
+from datasets.transforms.normalize.normalize_depth import NormalizeDepth
+from datasets.transforms.normalize.normalize_image import NormalizeImage
+
+
+__all__ = (
+    'NormalizeDepth',
+    'NormalizeImage',
+)
