ba260a79c2bee72350cadc1927820c91df481760
Daniel Mao <danielmao2019@gmail.com>
2024-05-14 22:22:03 -0400

    [Utils][Conversions] Implement mask2bbox utility

--- utils/conversions/__init__.py
+++ utils/conversions/__init__.py
@@ -1,9 +1,11 @@
 """
 UTILS.CONVERSIONS API
 """
+from utils.conversions.mask2bbox import mask2bbox
 from utils.conversions.poly2mask import poly2mask
 
 
 __all__ = (
+    'mask2bbox',
     'poly2mask',
 )
