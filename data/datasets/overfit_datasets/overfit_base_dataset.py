73209b12d0873567e9d4721bd24bb614b5d201d1
Daniel Mao <danielmao2019@gmail.com>
2024-05-18 17:37:52 -0400

    [Data][Datasets] Implement OverfitBaseDataset

---
+++ data/datasets/overfit_datasets/overfit_base_dataset.py
@@ -0,0 +1,70 @@
+from typing import Tuple, List, Dict, Callable, Any, Union, Optional
+import torch
+from ..base_dataset import BaseDataset
+
+
+class OverfitBaseDataset(BaseDataset):
+
+    def __init__(
+        self,
+        num_examples: int,
+        gen_func_config: Dict[str, Dict[str, Tuple[Callable, dict]]],
+        initial_seed: Optional[int] = None,
+    ):
+        # init num examples
+        assert type(num_examples) == int, f"{type(num_examples)=}"
+        assert num_examples >= 0, f"{num_examples=}"
+        self.num_examples = num_examples
+        # init gen func config
+        self._init_gen_func_config_(config=gen_func_config)
+        # init generator
+        self._init_generator_(initial_seed)
+        # init transform
+        super(OverfitBaseDataset, self).__init__()
+
+    def _init_gen_func_config_(self, config: Dict[str, Dict[str, Tuple[Callable, dict]]]) -> None:
+        assert type(config) == dict, f"{type(config)=}"
+        assert set(config.keys()) == set(['inputs', 'labels']), f"{config.keys()=}"
+        for key1 in config:
+            assert type(config[key1]) == dict, f"{type(config[key1])=}"
+            for key2 in config[key1]:
+                assert type(config[key1][key2]) == tuple, f"{type(config[key1][key2])=}"
+                assert len(config[key1][key2]) == 2, f"{len(config[key1][key2])=}"
+                assert callable(config[key1][key2][0]), f"{type(config[key1][key2][0])=}"
+                assert type(config[key1][key2][1]) == dict, f"{type(config[key1][key2][1])=}"
+        self.gen_func_config = config
+
+    def _init_generator_(self, initial_seed: Optional[int]) -> None:
+        self.generator = torch.Generator()
+        if initial_seed is not None:
+            self.generator.manual_seed(initial_seed)
+        self.initial_seed = self.generator.initial_seed()
+
+    def _init_annotations_all_(
+        self,
+        split: Optional[Union[str, Tuple[float, ...]]],
+        indices: Optional[Union[List[int], Dict[str, List[int]]]],
+    ) -> None:
+        r"""Intentionally doing nothing.
+        """
+        pass
+
+    def _init_annotations_(self, split: Optional[str]) -> None:
+        r"""Intentionally doing nothing.
+        """
+        pass
+
+    def __len__(self):
+        return self.num_examples
+
+    def _load_example_(self, idx: int) -> Tuple[
+        Dict[str, torch.Tensor], Dict[str, torch.Tensor], Dict[str, Any],
+    ]:
+        seed = self.initial_seed + idx
+        self.generator.manual_seed(seed)
+        inputs, labels = tuple({
+            key2: self.gen_func_config[key1][key2][0](**self.gen_func_config[key1][key2][1], generator=self.generator)
+            for key2 in self.gen_func_config[key1]
+        } for key1 in ['inputs', 'labels'])
+        meta_info = {'seed': seed}
+        return inputs, labels, meta_info
--- data/datasets/overfit_datasets/semantic_segmentation.py
+++ data/datasets/overfit_datasets/semantic_segmentation.py
@@ -1,47 +1,35 @@
-from typing import Dict, Tuple, Any, Optional
+from typing import Optional
 import math
 import torch
-import torchvision
-from ..base_dataset import BaseDataset
+from .overfit_base_dataset import OverfitBaseDataset
 
 
-class SemanticSegmentationOverfitDataset(BaseDataset):
+class SemanticSegmentationOverfitDataset(OverfitBaseDataset):
 
-    def __init__(self, num_classes: int, num_examples: int, initial_seed: Optional[int] = None):
+    def __init__(
+        self,
+        num_classes: int,
+        num_examples: int,
+        initial_seed: Optional[int] = None,
+    ) -> None:
+        # init num classes
         assert type(num_classes) == int, f"{type(num_classes)=}"
         assert num_classes > 0, f"{num_classes=}"
-        self.num_classes = num_classes
-        assert type(num_examples) == int, f"{type(num_examples)=}"
-        self.sample_res = math.ceil(math.sqrt(num_classes))
-        assert num_examples >= 0, f"{num_examples=}"
-        self.num_examples = num_examples
-        self._init_generator_(initial_seed)
-        self._init_transform_(transforms=None)
-
-    def _init_annotations_(self, split: str) -> None:
-        r"""Intentionally doing nothing.
-        """
-        pass
-
-    def _init_generator_(self, initial_seed: Optional[int]):
-        self.generator = torch.Generator()
-        if initial_seed is not None:
-            self.generator.manual_seed(initial_seed)
-        self.initial_seed = self.generator.initial_seed()
-
-    def __len__(self):
-        return self.num_examples
-
-    def _load_example_(self, idx: int) -> Tuple[
-        Dict[str, torch.Tensor], Dict[str, torch.Tensor], Dict[str, Any],
-    ]:
-        seed = self.initial_seed + idx
-        self.generator.manual_seed(seed)
-        inputs = {'image': torch.rand(size=(3, 512, 512), generator=self.generator, dtype=torch.float32)}
-        mask = torch.randint(size=(self.sample_res, self.sample_res), low=0, high=self.num_classes, generator=self.generator, dtype=torch.int64)
-        mask = torchvision.transforms.Resize(
-            size=(512, 512), interpolation=torchvision.transforms.functional.InterpolationMode.NEAREST,
-        )(mask.unsqueeze(0)).squeeze(0)
-        labels = {'mask': mask}
-        meta_info = {'seed': seed}
-        return inputs, labels, meta_info
+        # init gen func config
+        gen_func_config = {
+            'inputs': {
+                'image': (
+                    torch.rand,
+                    {'size': (3, 512, 512), 'dtype': torch.float32},
+                ),
+            },
+            'labels': {
+                'mask': (
+                    torch.randint,
+                    {'size': (math.ceil(math.sqrt(num_classes)),) * 2, 'low': 0, 'high': num_classes, 'dtype': torch.int64}
+                ),
+            },
+        }
+        super(SemanticSegmentationOverfitDataset, self).__init__(
+            num_examples=num_examples, gen_func_config=gen_func_config, initial_seed=initial_seed,
+        )
