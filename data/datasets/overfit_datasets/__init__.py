a1568c6f16a66ace64e70fa7cd67d5464a2f4992
Daniel Mao <danielmao2019@gmail.com>
2024-05-18 17:42:07 -0400

    [Data][Datasets] Implement ClassificationOverfitDataset

--- data/datasets/overfit_datasets/__init__.py
+++ data/datasets/overfit_datasets/__init__.py
@@ -2,10 +2,12 @@
 DATA.DATASETS.OVERFIT_DATASETS API
 """
 from data.datasets.overfit_datasets.overfit_base_dataset import OverfitBaseDataset
+from data.datasets.overfit_datasets.classification import ClassificationOverfitDataset
 from data.datasets.overfit_datasets.semantic_segmentation import SemanticSegmentationOverfitDataset
 
 
 __all__ = (
     'OverfitBaseDataset',
+    'ClassificationOverfitDataset',
     'SemanticSegmentationOverfitDataset',
 )
