086084444f2cceb570e02fe66a1d08d11b17393e
Daniel Mao <danielmao2019@gmail.com>
2024-05-18 17:42:07 -0400

    [Data][Datasets] Implement ClassificationOverfitDataset

---
+++ data/datasets/overfit_datasets/classification.py
@@ -0,0 +1,35 @@
+from typing import Tuple, Optional
+import torch
+from .overfit_base_dataset import OverfitBaseDataset
+
+
+class ClassificationOverfitDataset(OverfitBaseDataset):
+
+    def __init__(
+        self,
+        num_classes: int,
+        num_examples: int,
+        image_res: Tuple[int, int],
+        initial_seed: Optional[int] = None,
+    ) -> None:
+        # init num classes
+        assert type(num_classes) == int, f"{type(num_classes)=}"
+        assert num_classes > 0, f"{num_classes=}"
+        # init gen func config
+        gen_func_config = {
+            'inputs': {
+                'image': (
+                    torch.rand,
+                    {'size': (3, image_res, image_res), 'dtype': torch.float32},
+                ),
+            },
+            'labels': {
+                'target': (
+                    torch.randint,
+                    {'size': (), 'low': 0, 'high': num_classes, 'dtype': torch.int64}
+                ),
+            },
+        }
+        super(ClassificationOverfitDataset, self).__init__(
+            num_examples=num_examples, gen_func_config=gen_func_config, initial_seed=initial_seed,
+        )
