55412244abe8fb8b6a250823bb7cb5e7425b3e5f
Daniel Mao <danielmao2019@gmail.com>
2024-05-19 19:48:03 -0400

    [Criteria] Implement PytorchCriterionWrapper

--- criteria/__init__.py
+++ criteria/__init__.py
@@ -2,6 +2,7 @@
 CRITERIA API
 """
 from criteria.base_criterion import BaseCriterion
+from criteria.pytorch_criteria_wrapper import PyTorchCriterionWrapper
 from criteria.auxiliary_outputs_criterion import AuxiliaryOutputsCriterion
 from criteria.multi_task_criterion import MultiTaskCriterion
 from criteria.depth_estimation_criterion import DepthEstimationCriterion
@@ -13,6 +14,7 @@ from criteria.ccdm_criterion import CCDMCriterion
 
 __all__ = (
     'BaseCriterion',
+    'PyTorchCriterionWrapper',
     'AuxiliaryOutputsCriterion',
     'MultiTaskCriterion',
     'DepthEstimationCriterion',
---
+++ criteria/pytorch_criteria_wrapper.py
@@ -0,0 +1,14 @@
+from typing import Callable
+import torch
+from .base_criterion import BaseCriterion
+
+
+class PyTorchCriterionWrapper(BaseCriterion):
+
+    def __init__(self, criterion: Callable) -> None:
+        super(PyTorchCriterionWrapper, self).__init__()
+        assert callable(criterion), f"{type(criterion)=}"
+        self.criterion = criterion
+
+    def _compute_loss_(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        return self.criterion(input=y_pred, target=y_true)
