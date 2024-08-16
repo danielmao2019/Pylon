e043968ffb9f8ef193d4126bb1fd6dbea0aa05b0
Daniel Mao <danielmao2019@gmail.com>
2024-08-15 20:29:58 -0400

    [Utils][Builder] Add test for kwargs in build_from_config

--- utils/builder.py
+++ utils/builder.py
@@ -3,10 +3,20 @@ from typing import Any
 
 def build_from_config(config: Any, **kwargs) -> Any:
     """This function recursively builds objects provided by the config.
+
+    Args:
+        config (Any): A config dict for building objects or any built object.
+        kwargs: keyword arguments only used for building objects from `config`.
     """
     if type(config) == dict and set(config.keys()) == set(['class', 'args']):
+        # merge args
+        assert type(kwargs) == dict, f"{type(kwargs)=}"
+        assert set(config.keys()) & set(kwargs.keys()) == set(), f"{config.keys()=}, {kwargs.keys()=}"
+        config['args'].update(kwargs)
+        # build args
         for key in config['args']:
             config['args'][key] = build_from_config(config['args'][key])
-        return config['class'](**config['args'], **kwargs)
+        # build self
+        return config['class'](**config['args'])
     else:
         return config
