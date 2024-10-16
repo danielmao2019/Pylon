891ad13ab83ade2a49ef962deb7faa3898f643f0
Daniel Mao <danielmao2019@gmail.com>
2024-10-15 20:59:17 -0400

    [Configs] Fix NYUD-MT dataset and models configs

--- configs/common/datasets/nyu_v2.py
+++ configs/common/datasets/nyu_v2.py
@@ -1,29 +1,28 @@
 import torch
 import torchvision
-import datasets
+import data
 import criteria
 import metrics
-import grad_measures
 
 
 transforms_config = {
-    'class': datasets.transforms.Compose,
+    'class': data.transforms.Compose,
     'args': {
         'transforms': [
             (
-                torchvision.transforms.Resize(size=(512,)*2, antialias=True),
+                torchvision.transforms.Resize(size=(288, 384), antialias=True),
                 ('inputs', 'image'),
             ),
             (
-                datasets.transforms.resize.ResizeMaps(size=(512,)*2, antialias=True),
+                data.transforms.resize.ResizeMaps(size=(288, 384), antialias=True),
                 ('labels', 'depth_estimation'),
             ),
             (
-                datasets.transforms.resize.ResizeNormals(target_size=(512,)*2),
+                data.transforms.resize.ResizeNormals(target_size=(288, 384)),
                 ('labels', 'normal_estimation'),
             ),
             (
-                datasets.transforms.resize.ResizeMaps(size=(512,)*2, interpolation=torchvision.transforms.functional.InterpolationMode.NEAREST),
+                data.transforms.resize.ResizeMaps(size=(288, 384), interpolation=torchvision.transforms.functional.InterpolationMode.NEAREST),
                 ('labels', 'semantic_segmentation'),
             ),
         ],
@@ -31,7 +30,7 @@ transforms_config = {
 }
 
 collate_fn_config = {
-    'class': datasets.collators.BaseCollator,
+    'class': data.collators.BaseCollator,
     'args': {
         'collators': {
             'meta_info': {
@@ -43,12 +42,12 @@ collate_fn_config = {
 
 config = {
     'train_dataset': {
-        'class': datasets.NYUv2Dataset,
+        'class': data.datasets.NYUv2Dataset,
         'args': {
-            'data_root': "./datasets/datasets/soft_links/NYUD_MT",
+            'data_root': "./data/datasets/soft_links/NYUD_MT",
             'split': "train",
             'indices': None,
-            'transforms': transforms_config,
+            'transforms_cfg': transforms_config,
         },
     },
     'train_dataloader': {
@@ -60,12 +59,12 @@ config = {
         },
     },
     'val_dataset': {
-        'class': datasets.NYUv2Dataset,
+        'class': data.datasets.NYUv2Dataset,
         'args': {
-            'data_root': "./datasets/datasets/soft_links/NYUD_MT",
+            'data_root': "./data/datasets/soft_links/NYUD_MT",
             'split': "val",
             'indices': None,
-            'transforms': transforms_config,
+            'transforms_cfg': transforms_config,
         },
     },
     'val_dataloader': {
@@ -76,12 +75,12 @@ config = {
         },
     },
     'test_dataset': {
-        'class': datasets.NYUv2Dataset,
+        'class': data.datasets.NYUv2Dataset,
         'args': {
-            'data_root': "./datasets/datasets/soft_links/NYUD_MT",
+            'data_root': "./data/datasets/soft_links/NYUD_MT",
             'split': "test",
             'indices': None,
-            'transforms': transforms_config,
+            'transforms_cfg': transforms_config,
         },
     },
     'test_dataloader': {
@@ -92,56 +91,45 @@ config = {
         },
     },
     'criterion': {
-        'class': criteria.MultiTaskCriterion,
+        'class': criteria.wrappers.MultiTaskCriterion,
         'args': {
             'criterion_configs': {
                 'depth_estimation': {
-                    'class': criteria.DepthEstimationCriterion,
+                    'class': criteria.vision_2d.DepthEstimationCriterion,
                     'args': {},
                 },
                 'normal_estimation': {
-                    'class': criteria.NormalEstimationCriterion,
+                    'class': criteria.vision_2d.NormalEstimationCriterion,
                     'args': {},
                 },
                 'semantic_segmentation': {
-                    'class': criteria.SemanticSegmentationCriterion,
+                    'class': criteria.vision_2d.SemanticSegmentationCriterion,
                     'args': {
-                        'ignore_index': datasets.NYUv2Dataset.IGNORE_INDEX,
+                        'ignore_index': data.datasets.NYUv2Dataset.IGNORE_INDEX,
                     },
                 },
             },
         },
     },
     'metric': {
-        'class': metrics.XMTLMetric,
+        'class': metrics.wrappers.MultiTaskMetric,
         'args': {
             'metric_configs': {
                 'depth_estimation': {
-                    'class': metrics.DepthEstimationMetric,
+                    'class': metrics.vision_2d.DepthEstimationMetric,
                     'args': {},
                 },
                 'normal_estimation': {
-                    'class': metrics.NormalEstimationMetric,
+                    'class': metrics.vision_2d.NormalEstimationMetric,
                     'args': {},
                 },
                 'semantic_segmentation': {
-                    'class': metrics.SemanticSegmentationMetric,
+                    'class': metrics.vision_2d.SemanticSegmentationMetric,
                     'args': {
-                        'num_classes': datasets.NYUv2Dataset.NUM_CLASSES,
-                        'ignore_index': datasets.NYUv2Dataset.IGNORE_INDEX,
+                        'num_classes': data.datasets.NYUv2Dataset.NUM_CLASSES,
+                        'ignore_index': data.datasets.NYUv2Dataset.IGNORE_INDEX,
                     },
                 },
-            },
-            'grad_measures': {
-                'grad_conflicts': grad_measures.grad_conflicts,
-                'grad_dominance': grad_measures.grad_dominance,
-                'grad_stability': grad_measures.grad_stability,
-                'feature_ent': grad_measures.feature_ent,
-                'task_ent_cos_sim': grad_measures.task_ent_cos_sim,
-                'task_ent_sym_cross_entropy': grad_measures.task_ent_sym_cross_entropy,
-                'task_ent_sym_Kullback_Leibler_divergence': grad_measures.task_ent_sym_Kullback_Leibler_divergence,
-                'task_ent_Jensen_Shannon_divergence': grad_measures.task_ent_Jensen_Shannon_divergence,
-                'task_ent_earth_mover_distance': grad_measures.task_ent_earth_mover_distance,
             },
         },
     },
