6e6206c251b34f17e603ed9bd3316f9c06b43112
Daniel Mao <danielmao2019@gmail.com>
2024-10-17 17:09:16 -0400

    [Utils][Ops] Implement buffer_equal

--- utils/ops/dict_as_tensor.py
+++ utils/ops/dict_as_tensor.py
@@ -1,42 +1,27 @@
-from typing import List, Dict, Union, Callable, Any
+"""This module implements multiple utility functions to support the idea of treating a Python dictionary
+as an any immutable object indexed tensor, of any data type. For now, we name dictionaries under this
+view as 'buffer'. A buffer could be tuple, list, dict, numpy.ndarray, and torch.Tensor.
+"""
+from typing import List, Dict, Union, Any
 import numpy
 import torch
 
 
-def apply_tensor_op(
-    func: Callable[[torch.Tensor], torch.Tensor],
-    inputs: Union[tuple, list, dict, torch.Tensor, float],
-) -> dict:
-    if type(inputs) == torch.Tensor:
-        return func(inputs)
-    elif type(inputs) == tuple:
-        return tuple(apply_tensor_op(func=func, inputs=tuple_elem) for tuple_elem in inputs)
-    elif type(inputs) == list:
-        return list(apply_tensor_op(func=func, inputs=list_elem) for list_elem in inputs)
-    elif type(inputs) == dict:
-        return {key: apply_tensor_op(func=func, inputs=inputs[key]) for key in inputs.keys()}
+def buffer_equal(buffer, other) -> bool:
+    result = True
+    result = result and type(buffer) == type(other)
+    if type(buffer) in [tuple, list]:
+        result = result and len(buffer) == len(other)
+        result = result and all([buffer_equal(buffer[idx], other[idx]) for idx in range(len(buffer))])
+    elif type(buffer) == dict:
+        result = result and set(buffer.keys()) == set(other.keys())
+        result = result and all([buffer_equal(buffer[key], other[key]) for key in buffer])
+    elif type(buffer) == numpy.ndarray:
+        result = result and numpy.array_equal(buffer, other)
+    elif type(buffer) == torch.Tensor:
+        result = result and torch.equal(buffer, other)
     else:
-        return inputs
-
-
-def apply_pairwise(
-    lot: List[torch.Tensor],
-    func: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
-    symmetric: bool = False,
-) -> torch.Tensor:
-    # input checks
-    assert type(lot) == list, f"{type(lot)=}"
-    # compute result
-    dim = len(lot)
-    result = torch.zeros(size=(dim, dim), dtype=torch.float32, device=torch.device('cuda'))
-    for i in range(dim):
-        loop = range(i, dim) if symmetric else range(dim)
-        for j in loop:
-            val = func(lot[i], lot[j])
-            assert val.numel() == 1, f"{val.numel()}"
-            result[i, j] = val
-            if symmetric:
-                result[j, i] = val
+        result = result and buffer == other
     return result
 
 
