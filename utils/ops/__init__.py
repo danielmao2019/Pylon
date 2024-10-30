018fb0e874a84ed0f925ec9919aaeabda6b3ac6c
Daniel Mao <danielmao2019@gmail.com>
2024-10-29 23:48:00 -0400

    [Utils][Ops] Implement more buffer operations

--- utils/ops/__init__.py
+++ utils/ops/__init__.py
@@ -2,8 +2,13 @@
 UTILS.OPS API
 """
 from utils.ops.apply import apply_tensor_op, apply_pairwise
-from utils.ops.dict_as_tensor import buffer_equal, buffer_add, buffer_scalar_mul, buffer_mean
-from utils.ops.dict_as_tensor import transpose_buffer
+from utils.ops.dict_as_tensor import (
+    buffer_equal,
+    buffer_add, buffer_scalar_mul, buffer_sub,
+    buffer_mul, buffer_rec, buffer_div,
+    buffer_mean,
+    transpose_buffer,
+)
 
 
 __all__ = (
@@ -12,6 +17,10 @@ __all__ = (
     'buffer_equal',
     'buffer_add',
     'buffer_scalar_mul',
+    'buffer_sub',
+    'buffer_mul',
+    'buffer_rec',
+    'buffer_div',
     'buffer_mean',
     'transpose_buffer',
 )
