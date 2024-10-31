94092c31058a99ab939c801720360d966c7d1a30
Daniel Mao <danielmao2019@gmail.com>
2024-10-31 12:05:28 -0400

    [Metrics][Vision-2D] Add basic test case for NormalEstimationMetric

---
+++ metrics/vision_2d/test_normal_estimation_metric.py
@@ -0,0 +1,17 @@
+import pytest
+from .normal_estimation_metric import NormalEstimationMetric
+import torch
+
+
+@pytest.mark.parametrize("output, label, expected", [
+    (
+        torch.tensor([[[[1]], [[0]], [[0]]]], dtype=torch.float32),
+        torch.tensor([[[[1]], [[1]], [[0]]]], dtype=torch.float32),
+        torch.tensor(45, dtype=torch.float32),
+    )
+])
+def test_normal_estimation_metric(output, label, expected) -> None:
+    metric = NormalEstimationMetric()
+    score = metric(y_pred=output, y_true=label)
+    assert type(score) == dict and set(score.keys()) == set(['angle']), f"{score=}"
+    assert torch.equal(score['angle'], expected)
