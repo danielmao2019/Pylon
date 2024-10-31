cae37954726880452827f0a5f0a1c17038633c4f
Daniel Mao <danielmao2019@gmail.com>
2024-10-31 19:50:15 -0400

    [Criteria][Wrappers] Rename file pytorch_criterion_wrapper.py

--- criteria/wrappers/__init__.py
+++ criteria/wrappers/__init__.py
@@ -2,7 +2,7 @@
 CRITERIA.WRAPPERS API
 """
 from criteria.wrappers.single_task_criterion import SingleTaskCriterion
-from criteria.wrappers.pytorch_criteria_wrapper import PyTorchCriterionWrapper
+from criteria.wrappers.pytorch_criterion_wrapper import PyTorchCriterionWrapper
 from criteria.wrappers.auxiliary_outputs_criterion import AuxiliaryOutputsCriterion
 from criteria.wrappers.multi_task_criterion import MultiTaskCriterion
 
