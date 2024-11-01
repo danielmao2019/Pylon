f5816ef916752fcad5e51a2c94212b458debfee4
Daniel Mao <danielmao2019@gmail.com>
2024-11-01 10:41:37 -0400

    [Optimizers][NashMTL] Implement numerical stability considerations for Nash MTL

--- optimizers/gradient_manipulation/nashmtl.py
+++ optimizers/gradient_manipulation/nashmtl.py
@@ -7,6 +7,9 @@ from ._base_ import GradientManipulationBaseOptimizer
 import utils
 
 
+NUMERICAL_STABILITY = 1.0e-05
+
+
 class NashMTLOptimizer(GradientManipulationBaseOptimizer):
     __doc__ = r"""Paper: Multi-Task Learning as a Bargaining Game (https://arxiv.org/abs/2202.01017)
     """
@@ -20,30 +23,31 @@ class NashMTLOptimizer(GradientManipulationBaseOptimizer):
 
     def _init_problem_(self):
         # define variables
-        self.alpha_variable = cp.Variable(shape=(self.num_tasks,), nonneg=True)
+        self.alpha_var = cp.Variable(shape=(self.num_tasks,), nonneg=True)
         self.alpha = cp.Parameter(shape=(self.num_tasks,), value=numpy.ones(self.num_tasks, dtype=numpy.float32))
         self.gtg = cp.Parameter(shape=(self.num_tasks, self.num_tasks), value=numpy.eye(self.num_tasks))
         # define constraints
-        beta_variable = self.gtg @ self.alpha_variable
-        phi_variable = cp.log(self.alpha_variable) + cp.log(beta_variable)
+        beta_var = self.gtg @ self.alpha_var
+        phi_var = cp.log(self.alpha_var) + cp.log(beta_var)
         constraints = []
         for i in range(self.num_tasks):
-            constraints.append(phi_variable[i] >= 0)
-            constraints.append(self.alpha_variable[i] >= 0)
+            constraints.append(self.alpha_var[i] >= NUMERICAL_STABILITY)
+            constraints.append(beta_var >= NUMERICAL_STABILITY)
+            constraints.append(phi_var[i] >= 0)
         # define objective
         beta = self.gtg @ self.alpha
         phi = cp.sum(cp.log(self.alpha) + cp.log(beta))
         gradient = 1 / self.alpha + (1 / beta) @ self.gtg
-        phi_linear = phi + gradient @ (self.alpha_variable - self.alpha)
-        obj = cp.Minimize(cp.sum(beta_variable) + phi_linear)
+        phi_linear = phi + gradient @ (self.alpha_var - self.alpha)
+        obj = cp.Minimize(cp.sum(beta_var) + phi_linear)
         # define problem
         self.problem = cp.Problem(obj, constraints)
 
     def _stopping_criterion_(self):
         return (
-            (self.alpha_variable.value is None)
-            or (numpy.linalg.norm(self.gtg.value @ self.alpha_variable.value - 1 / (self.alpha_variable.value + 1e-10)) < 1e-3)
-            or (numpy.linalg.norm(self.alpha_variable.value - self.alpha.value) < 1e-6)
+            (self.alpha_var.value is None)
+            or (numpy.linalg.norm(self.gtg.value @ self.alpha_var.value - 1 / (self.alpha_var.value + 1e-10)) < 1e-3)
+            or (numpy.linalg.norm(self.alpha_var.value - self.alpha.value) < 1e-6)
         )
 
     def solve_optimization(self):
@@ -52,13 +56,13 @@ class NashMTLOptimizer(GradientManipulationBaseOptimizer):
             try:
                 self.problem.solve(solver=cp.ECOS, warm_start=True, max_iters=100)
             except:
-                self.alpha_variable.value = self.alpha.value
+                self.alpha_var.value = self.alpha.value
             if self._stopping_criterion_():
                 break
         # accept proposal
-        assert self.alpha_variable.value is not None
-        assert numpy.all(self.alpha_variable.value >= 0)
-        self.alpha.value = self.alpha_variable.value
+        assert self.alpha_var.value is not None
+        assert numpy.all(self.alpha_var.value >= 0)
+        self.alpha.value = self.alpha_var.value
 
     def _gradient_manipulation_(
         self,
