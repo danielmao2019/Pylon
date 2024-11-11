c9ba1cb953c004db2c116976b9a7e7a521c66b47
Daniel Mao <danielmao2019@gmail.com>
2024-11-11 16:16:37 -0500

    [Utils][Progress] Re-implement epoch finish logic in utils.progress module

---
+++ utils/progress.py
@@ -0,0 +1,38 @@
+from typing import List
+import os
+import json
+import torch
+
+
+def _check_file_loadable(filepath: str) -> bool:
+    assert os.path.isfile(filepath)
+    assert filepath.endswith(".json") or filepath.endswith(".pt")
+    result: bool = True
+    if filepath.endswith(".json"):
+        try:
+            with open(filepath, mode='r') as f:
+                _ = json.load(f)
+        except:
+            result = False
+    elif filepath.endswith(".pt"):
+        try:
+            _ = torch.load(filepath)
+        except:
+            result = False
+    else:
+        assert 0
+    return result
+
+
+def check_epoch_finished(epoch_dir: str, expected_files: List[str]) -> bool:
+    r"""Three criteria:
+        1. File exists.
+        2. File non-empty.
+        3. File load-able.
+    """
+    return all([
+        os.path.isfile(os.path.join(epoch_dir, filename)) and
+        os.path.getsize(os.path.join(epoch_dir, filename)) > 0 and
+        _check_file_loadable(os.path.join(epoch_dir, filename))
+        for filename in expected_files
+    ])
