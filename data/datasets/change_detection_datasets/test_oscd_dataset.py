5141a4d566f39f7ff287929f8f89ba2b333ff700
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-01-11 13:08:11 -0500

    [Data][Datasets] Implement OSCD dataset class (#14)

---
+++ data/datasets/change_detection_datasets/test_oscd_dataset.py
@@ -0,0 +1,62 @@
+import pytest
+from .oscd_dataset import OSCDDataset
+import os
+import torch
+import utils
+
+
+@pytest.mark.parametrize("dataset", [
+    (OSCDDataset(data_root="./data/datasets/soft_links/OSCD", split='train')),
+    (OSCDDataset(data_root="./data/datasets/soft_links/OSCD", split='test')),
+    (OSCDDataset(data_root="./data/datasets/soft_links/OSCD", split='train', bands=['B01', 'B02'])),
+    (OSCDDataset(data_root="./data/datasets/soft_links/OSCD", split='test', bands=['B01', 'B02'])),
+    (OSCDDataset(data_root="./data/datasets/soft_links/OSCD", split='train', bands=['B04', 'B03', 'B02'])),
+    (OSCDDataset(data_root="./data/datasets/soft_links/OSCD", split='train', bands=['B08', 'B8A'])),
+])
+def test_oscd(dataset: torch.utils.data.Dataset) -> None:
+    assert isinstance(dataset, torch.utils.data.Dataset)
+    for idx in range(len(dataset)):
+        datapoint = dataset[idx]
+        assert type(datapoint) == dict
+        assert set(datapoint.keys()) == set(['inputs', 'labels', 'meta_info'])
+        # inspect inputs
+        inputs = datapoint['inputs']
+        assert type(inputs) == dict
+        assert set(inputs.keys()) == set(OSCDDataset.INPUT_NAMES)
+        img_1 = inputs['img_1']
+        img_2 = inputs['img_2']
+        assert type(img_1) == torch.Tensor and img_1.ndim == 3 and img_1.dtype == torch.float32
+        assert type(img_2) == torch.Tensor and img_2.ndim == 3 and img_2.dtype == torch.float32
+        assert img_1.shape == img_2.shape, f"{img_1.shape=}, {img_2.shape=}"
+        # inspect labels
+        labels = datapoint['labels']
+        assert type(labels) == dict
+        assert set(labels.keys()) == set(OSCDDataset.LABEL_NAMES)
+        change_map = labels['change_map']
+        assert set(torch.unique(change_map).tolist()) == set([0, 1]), f"{torch.unique(change_map)=}"
+        # sanity check for consistency between different modalities
+        # TODO: Enable the following assertions
+        # for input_idx in [1, 2]:
+        #     tif_input = utils.io.load_image(filepaths=list(filter(
+        #         lambda x: os.path.splitext(os.path.basename(x))[0].split('_')[-1] in ['B04', 'B03', 'B02'],
+        #         dataset.annotations[idx]['inputs'][f'tif_input_{input_idx}_filepaths'],
+        #     ))[::-1], dtype=torch.float32, sub=None, div=None)
+        #     lower = torch.quantile(tif_input, 0.02)
+        #     upper = torch.quantile(tif_input, 0.98)
+        #     tif_input = (tif_input - lower) / (upper - lower)
+        #     tif_input = torch.clamp(tif_input, min=0, max=1)
+        #     tif_input = (tif_input * 255).to(torch.uint8)
+        #     png_input = utils.io.load_image(
+        #         filepath=dataset.annotations[idx]['inputs'][f'png_input_{input_idx}_filepath'],
+        #         dtype=torch.uint8, sub=None, div=None,
+        #     )
+        #     assert torch.equal(tif_input, png_input)
+        # tif_label = utils.io.load_image(
+        #     filepaths=dataset.annotations[idx]['labels']['tif_label_filepaths'],
+        #     dtype=torch.int64, sub=1, div=None,
+        # )
+        # png_label = (torch.mean(utils.io.load_image(
+        #     filepath=dataset.annotations[idx]['labels']['png_label_filepath'],
+        #     dtype=torch.float32, sub=None, div=None,
+        # )[:3, :, :], dim=0, keepdim=True) > 0.5).to(torch.int64)
+        # assert torch.equal(tif_label, png_label)
