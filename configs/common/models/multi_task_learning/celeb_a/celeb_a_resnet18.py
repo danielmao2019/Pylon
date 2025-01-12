e0c300d7ddc911b625a3b8326f61ccf025d4dd08
Daniel Mao <danielmao2019@gmail.com>
2025-01-12 12:39:39 -0500

    Restructure Multi-Task Learning code

--- configs/common/models/multi_task_learning/celeb_a/celeb_a_resnet18.py
+++ configs/common/models/multi_task_learning/celeb_a/celeb_a_resnet18.py
@@ -2,7 +2,7 @@ import models
 
 
 model_config_all_tasks = {
-    'class': models.CelebA_ResNet18,
+    'class': models.multi_task_learning.CelebA_ResNet18,
     'args': {
         'return_shared_rep': True,
     },
--- configs/common/models/multi_task_learning/city_scapes_f/pspnet_resnet50.py
+++ configs/common/models/multi_task_learning/city_scapes_f/pspnet_resnet50.py
@@ -3,7 +3,7 @@ import models
 
 
 model_config_depth_estimation = {
-    'class': models.NYUD_MT_PSPNet,
+    'class': models.multi_task_learning.CityScapes_PSPNet,
     'args': {
         'backbone': models.backbones.resnet50(weights='DEFAULT'),
         'in_channels': 2048,
@@ -13,36 +13,36 @@ model_config_depth_estimation = {
     },
 }
 
-model_config_normal_estimation = {
-    'class': models.NYUD_MT_PSPNet,
+model_config_semantic_segmentation = {
+    'class': models.multi_task_learning.CityScapes_PSPNet,
     'args': {
         'backbone': models.backbones.resnet50(weights='DEFAULT'),
         'in_channels': 2048,
-        'tasks': set(['normal_estimation']),
+        'tasks': set(['semantic_segmentation']),
+        'num_classes': data.datasets.CityScapesDataset.NUM_CLASSES_F,
         'return_shared_rep': False,
         'use_attention': False,
     },
 }
 
-model_config_semantic_segmentation = {
-    'class': models.NYUD_MT_PSPNet,
+model_config_instance_segmentation = {
+    'class': models.multi_task_learning.CityScapes_PSPNet,
     'args': {
         'backbone': models.backbones.resnet50(weights='DEFAULT'),
         'in_channels': 2048,
-        'tasks': set(['semantic_segmentation']),
-        'num_classes': data.datasets.NYUv2Dataset.NUM_CLASSES_F,
+        'tasks': set(['instance_segmentation']),
         'return_shared_rep': False,
         'use_attention': False,
     },
 }
 
 model_config_all_tasks = {
-    'class': models.NYUD_MT_PSPNet,
+    'class': models.multi_task_learning.CityScapes_PSPNet,
     'args': {
         'backbone': models.backbones.resnet50(weights='DEFAULT'),
         'in_channels': 2048,
-        'tasks': set(["depth_estimation", "normal_estimation", "semantic_segmentation"]),
-        'num_classes': data.datasets.NYUv2Dataset.NUM_CLASSES_F,
+        'tasks': set(["depth_estimation", "semantic_segmentation", "instance_segmentation"]),
+        'num_classes': data.datasets.CityScapesDataset.NUM_CLASSES_F,
         'return_shared_rep': True,
         'use_attention': False,
     },
