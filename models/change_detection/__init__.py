1a585867c9729d647984c269cb84946abea14094
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-01-14 17:15:07 -0500

    [Models] Implement ChangeStar model (#28)

--- models/change_detection/__init__.py
+++ models/change_detection/__init__.py
@@ -2,8 +2,10 @@
 MODELS.CHANGE_DETECTION API
 """
 from models.change_detection.fully_convolutional_siamese_networks import FullyConvolutionalSiameseNetwork
+from models.change_detection.change_star import ChangeStar
 
 
 __all__ = (
     'FullyConvolutionalSiameseNetwork',
+    'ChangeStar',
 )
