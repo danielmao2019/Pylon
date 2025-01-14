e39e41dab96150fd4c0ee79a391e60f451338930
Daniel Mao <danielmao2019@gmail.com>
2025-01-13 21:49:57 -0500

    f

--- data/collators/__init__.py
+++ data/collators/__init__.py
@@ -2,8 +2,10 @@
 DATA.COLLATORS API
 """
 from data.collators.base_collator import BaseCollator
+from data.collators.change_star_collator import ChangeStarCollator
 
 
 __all__ = (
     'BaseCollator',
+    'ChangeStarCollator',
 )
