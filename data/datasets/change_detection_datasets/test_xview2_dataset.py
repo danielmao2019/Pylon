1146dedc7a61278c4af26c68592fae6e208436a3
Daniel Mao <danielmao2019@gmail.com>
2025-01-14 22:07:15 -0500

    f

--- data/datasets/change_detection_datasets/test_xview2_dataset.py
+++ data/datasets/change_detection_datasets/test_xview2_dataset.py
@@ -10,5 +10,15 @@ import torch
 ])
 def test_xview2(dataset: torch.utils.data.Dataset) -> None:
     assert isinstance(dataset, torch.utils.data.Dataset)
-    for idx in range(min(len(dataset), 100)):
+    for idx in range(len(dataset)):
         datapoint = dataset[idx]
+        # inspect inputs
+        inputs = datapoint['inputs']
+        assert set(inputs.keys()) == set(xView2Dataset.INPUT_NAMES)
+        assert 0 <= inputs['img_1'].min() <= inputs['img_1'].max() <= 1
+        assert 0 <= inputs['img_2'].min() <= inputs['img_2'].max() <= 1
+        # inspect labels
+        labels = datapoint['labels']
+        assert set(labels.keys()) == set(xView2Dataset.LABEL_NAMES)
+        assert set(torch.unique(labels['lbl_1']).tolist()).issubset(set([0, 1, 2, 3, 4])), f"{torch.unique(labels['lbl_1'])=}"
+        assert set(torch.unique(labels['lbl_2']).tolist()).issubset(set([0, 1, 2, 3, 4])), f"{torch.unique(labels['lbl_2'])=}"
