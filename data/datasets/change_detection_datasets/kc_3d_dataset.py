f6ab841e46b81a2d171bf0c3309c6c09d474c38f
Daniel Mao <danielmao2019@gmail.com>
2025-01-14 21:23:22 -0500

    f

--- data/datasets/change_detection_datasets/kc_3d_dataset.py
+++ data/datasets/change_detection_datasets/kc_3d_dataset.py
@@ -1,14 +1,14 @@
 from typing import Tuple, Dict, Any, Optional
+from data.datasets import BaseDataset
 import os
 import pickle
 import numpy
 import torch
-from torchvision.ops import masks_to_boxes
-from einops import rearrange
+import torchvision
 import utils
 
 
-class KC3DDataset:
+class KC3DDataset(BaseDataset):
     __doc__ = r"""
     Reference:
         * https://github.com/ragavsachdeva/CYWS-3D/blob/master/kc3d.py
@@ -30,12 +30,15 @@ class KC3DDataset:
 
     SPLIT_OPTIONS = ['train', 'val', 'test']
     DATASET_SIZE = None  # Define if exact sizes for splits are known
-    INPUT_NAMES = ['img_1', 'img_2']
+    INPUT_NAMES = [
+        'img_1', 'img_2', 'depth_1', 'depth_2',
+        'intrinsics1', 'intrinsics2', 'position1', 'position2', 'rotation1', 'rotation2',
+    ]
     LABEL_NAMES = ['bbox_1', 'bbox_2']
     SHA1SUM = None  # Define if checksum validation is required
 
-    def __init__(self, use_ground_truth_registration: Optional[bool] = True) -> None:
-        super(KC3DDataset, self).__init__()
+    def __init__(self, use_ground_truth_registration: Optional[bool] = True, **kwargs) -> None:
+        super(KC3DDataset, self).__init__(**kwargs)
         assert isinstance(use_ground_truth_registration, bool)
         self.use_ground_truth_registration = use_ground_truth_registration
 
@@ -52,14 +55,21 @@ class KC3DDataset:
         
         # Load annotations for the specified split
         self.annotations = dataset_splits[split]
+        # sanity check
+        for ann in self.annotations:
+            assert os.path.isfile(os.path.join(self.data_root, ann['image1']))
+            assert os.path.isfile(os.path.join(self.data_root, ann['image2']))
+            assert os.path.isfile(os.path.join(self.data_root, ann['mask1']))
+            assert os.path.isfile(os.path.join(self.data_root, ann['mask2']))
 
     def get_target_bboxes_from_mask(self, mask_as_tensor):
         """
         Converts a mask tensor to bounding boxes.
         """
-        if len(mask_as_tensor.shape) == 2:
-            mask_as_tensor = rearrange(mask_as_tensor, "h w -> 1 h w")
-        bboxes = masks_to_boxes(mask_as_tensor)
+        assert mask_as_tensor.ndim == 2, f"{mask_as_tensor.shape=}"
+        mask_as_tensor = mask_as_tensor.unsqueeze(0)
+        assert mask_as_tensor.ndim == 3 and mask_as_tensor.shape[0] == 1, f"{mask_as_tensor.shape=}"
+        bboxes = torchvision.ops.masks_to_boxes(mask_as_tensor)
         return bboxes
 
     def _load_datapoint(self, idx: int) -> Tuple[
@@ -79,21 +89,21 @@ class KC3DDataset:
         """
         # Load RGB images
         img_1 = utils.io.load_image(
-            filepath=self.annotation[idx]['image1'],
+            filepath=os.path.join(self.data_root, self.annotations[idx]['image1']),
             dtype=torch.float32, sub=None, div=255.0,
         )
         img_2 = utils.io.load_image(
-            filepath=self.annotation[idx]['image2'],
+            filepath=os.path.join(self.data_root, self.annotations[idx]['image2']),
             dtype=torch.float32, sub=None, div=255.0,
         )
 
         # Load depth maps
         depth_1 = utils.io.load_image(
-            filepaths=[self.annotation[idx]['depth1']],
+            filepaths=[os.path.join(self.data_root, self.annotations[idx]['depth1'])],
             dtype=torch.float32, sub=None, div=None,
         )
         depth_2 = utils.io.load_image(
-            filepaths=[self.annotation[idx]['depth2']],
+            filepaths=[os.path.join(self.data_root, self.annotations[idx]['depth2'])],
             dtype=torch.float32, sub=None, div=None,
         )
 
@@ -134,11 +144,11 @@ class KC3DDataset:
         """
         # Load masks
         mask_1 = utils.io.load_image(
-            filepath=os.path.join(self.data_root, self.annotation[idx]["mask1"]),
+            filepath=os.path.join(self.data_root, self.annotations[idx]["mask1"]),
             dtype=torch.int64, sub=None, div=None,
         )
         mask_2 = utils.io.load_image(
-            filepath=os.path.join(self.data_root, self.annotation[idx]["mask2"]),
+            filepath=os.path.join(self.data_root, self.annotations[idx]["mask2"]),
             dtype=torch.int64, sub=None, div=None,
         )
 
