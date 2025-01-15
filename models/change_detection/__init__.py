d43929c04b8bf9f9b97ed8193faf10211f88f4bc
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-01-15 11:17:53 -0500

    [Models] Implement CYWS-3D model (#34)

--- models/change_detection/__init__.py
+++ models/change_detection/__init__.py
@@ -2,10 +2,12 @@
 MODELS.CHANGE_DETECTION API
 """
 from models.change_detection.fully_convolutional_siamese_networks import FullyConvolutionalSiameseNetwork
-from models.change_detection.change_star import ChangeStar
+from models.change_detection.change_star.change_star import ChangeStar
+from models.change_detection.cyws_3d.cyws_3d import CYWS3D
 
 
 __all__ = (
     'FullyConvolutionalSiameseNetwork',
     'ChangeStar',
+    'CYWS3D',
 )
