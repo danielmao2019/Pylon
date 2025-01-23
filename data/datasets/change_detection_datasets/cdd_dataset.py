56964d8f2c9a0bb090e8b7f6bec17fb0a569f5a4
Y29lin <y29lin@uwaterloo.ca>
2025-01-23 16:29:17 -0500

    f

---
+++ data/datasets/change_detection_datasets/cdd_dataset.py
@@ -0,0 +1,29 @@
+import os
+import torch
+from data.datasets import BaseDataset
+import utils
+
+class CDDDataset(BaseDataset):
+    __doc__ = r"""
+    References:
+        * https://github.com/ServiceNow/seasonal-contrast/blob/main/datasets/oscd_dataset.py
+        * https://github.com/granularai/fabric/blob/igarss2019/utils/dataloaders.py
+        * https://github.com/NIX369/UNet_LSTM/blob/master/custom.py
+        * https://github.com/mpapadomanolaki/UNetLSTM/blob/master/custom.py
+        * https://github.com/WennyXY/DINO-MC/blob/main/data_process/oscd_dataset.py
+
+    Download:
+        * https://drive.google.com/file/d/1GX656JqqOyBi_Ef0w65kDGVto-nHrNs9/edit
+        ```bash
+        mkdir <data-root>
+        cd <data-root>
+        # <download the zip files from the link above>
+        unrar x ChangeDetectionDataset.rar
+        # create softlink
+        ln -s <data_root_path> <Pylon_path>/data/datasets/soft_links/CDD
+        # verify softlink status
+        stat <Pylon_path>/data/datasets/soft_links/CDD
+        ```
+    Used in:
+
+    """
