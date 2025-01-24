2de500e08552346f23c57daaacb5b0c320bfbba4
Daniel Mao <danielmao2019@gmail.com>
2025-01-23 21:11:54 -0500

    [Data][Transforms] Implement Randomize class

--- data/transforms/__init__.py
+++ data/transforms/__init__.py
@@ -3,6 +3,7 @@ DATA.TRANSFORMS API
 """
 from data.transforms.base_transform import BaseTransform
 from data.transforms.compose import Compose
+from data.transforms.randomize import Randomize
 from data.transforms import normalize
 from data.transforms import resize
 
@@ -10,6 +11,7 @@ from data.transforms import resize
 __all__ = (
     'BaseTransform',
     'Compose',
+    'Randomize',
     'normalize',
     'resize',
 )
---
+++ data/transforms/randomize.py
@@ -0,0 +1,21 @@
+from typing import List, Callable, Union
+import random
+import torch
+from data.transforms import BaseTransform, Compose
+
+
+class Randomize(BaseTransform):
+
+    def __init__(self, transform: Callable, p: float) -> None:
+        assert callable(transform), f"{type(callable)=}"
+        assert not isinstance(transform, BaseTransform) and not isinstance(transform, Compose), f"{type(callable)=}"
+        self.transform = transform
+        assert type(p) in [int, float], f"{type(p)=}"
+        assert 0 <= p <= 1, f"{p=}"
+        self.p = p
+
+    def __call__(self, *args) -> Union[torch.Tensor, List[torch.Tensor]]:
+        if random.uniform(0, 1) < self.p:
+            return self.transform(*args)
+        else:
+            return list(args)
