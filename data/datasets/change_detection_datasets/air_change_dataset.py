a1f6efb370dc163df1f1d4ab80d44c2f207e9244
Daniel Mao <danielmao2019@gmail.com>
2025-01-24 15:19:42 -0500

    f

--- data/datasets/change_detection_datasets/air_change_dataset.py
+++ data/datasets/change_detection_datasets/air_change_dataset.py
@@ -4,12 +4,16 @@ from data.datasets import BaseDataset
 class AirChangeDataset(BaseDataset):
     __doc__ = r"""
     Download:
-    ```bash
-        wget http://mplab.sztaki.hu/~bcsaba/test/SZTAKI_AirChange_Benchmark.zip
-        unzip SZTAKI_AirChange_Benchmark.zip
-        mv SZTAKI_AirChange_Benchmark AirChange
-        rm SZTAKI_AirChange_Benchmark.zip
-    ```
+        ```bash
+            wget http://mplab.sztaki.hu/~bcsaba/test/SZTAKI_AirChange_Benchmark.zip
+            unzip SZTAKI_AirChange_Benchmark.zip
+            mv SZTAKI_AirChange_Benchmark AirChange
+            rm SZTAKI_AirChange_Benchmark.zip
+        ```
+
+    Used in:
+        * Change Detection Based on Deep Siamese Convolutional Network for Optical Aerial Images
+        * Fully Convolutional Siamese Networks for Change Detection
     """
 
     def _init_annotations(self) -> None:
