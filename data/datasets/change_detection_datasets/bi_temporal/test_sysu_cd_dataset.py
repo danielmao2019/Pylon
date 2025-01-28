403cf98f3a2c040799120ae5dca1f26d0c8c3263
Daniel Mao <danielmao2019@gmail.com>
2025-01-28 18:51:36 -0500

    f

--- data/datasets/change_detection_datasets/bi_temporal/test_sysu_cd_dataset.py
+++ data/datasets/change_detection_datasets/bi_temporal/test_sysu_cd_dataset.py
@@ -24,9 +24,6 @@ def test_sysu_cd(dataset: torch.utils.data.Dataset) -> None:
         assert type(img_1) == torch.Tensor and img_1.ndim == 3 and img_1.dtype == torch.float32
         assert type(img_2) == torch.Tensor and img_2.ndim == 3 and img_2.dtype == torch.float32
         assert img_1.shape == img_2.shape, f"{img_1.shape=}, {img_2.shape=}"
-        if dataset.bands is None:
-            for input_idx, x in enumerate([img_1, img_2]):
-                assert 0 <= x.min() <= x.max() <= 1, f"{input_idx=}, {x.min()=}, {x.max()=}"
         # inspect labels
         labels = datapoint['labels']
         assert type(labels) == dict
