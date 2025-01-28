b4824af3384952f8aa27cbf2b1f2afa67b69e47a
Y29lin <y29lin@uwaterloo.ca>
2025-01-28 14:54:37 -0500

    f

--- models/change_detection/snunet/up.py
+++ models/change_detection/snunet/up.py
@@ -1,16 +1,15 @@
-import torch.nn as nn
 import torch
 
-class up(nn.Module):
+class up(torch.nn.Module):
     def __init__(self, in_ch, bilinear=False):
         super(up, self).__init__()
 
         if bilinear:
-            self.up = nn.Upsample(scale_factor=2,
+            self.up = torch.nn.Upsample(scale_factor=2,
                                   mode='bilinear',
                                   align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_ch, in_ch, 2, stride=2)
+            self.up = torch.nn.ConvTranspose2d(in_ch, in_ch, 2, stride=2)
 
     def forward(self, x):
 
