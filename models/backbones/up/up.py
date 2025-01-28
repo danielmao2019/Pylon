1b83c591533783508283b36c552311c7f1815d1a
Y29lin <y29lin@uwaterloo.ca>
2025-01-28 14:27:54 -0500

    f

---
+++ models/backbones/up/up.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch
+
+class up(nn.Module):
+    def __init__(self, in_ch, bilinear=False):
+        super(up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2,
+                                  mode='bilinear',
+                                  align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch, in_ch, 2, stride=2)
+
+    def forward(self, x):
+
+        x = self.up(x)
+        return x
