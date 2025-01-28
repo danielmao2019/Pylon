d06abc39c79967804e9a791185df40a393c7a0cc
Daniel Mao <danielmao2019@gmail.com>
2025-01-27 21:18:59 -0500

    f

--- data/datasets/change_detection_datasets/single_temporal/i3pe_dataset.py
+++ data/datasets/change_detection_datasets/single_temporal/i3pe_dataset.py
@@ -1,6 +1,6 @@
 from typing import Tuple, Dict, Any
 import random
-import numpy
+import numpy as np
 import torch
 from sklearn.cluster import DBSCAN
 from skimage.segmentation import slic
@@ -12,11 +12,18 @@ class I3PEDataset(BaseSyntheticDataset):
     INPUT_NAMES = ['img_1', 'img_2']
     LABEL_NAMES = ['change_map']
     n_segments = 1000
-    eps = 7
+    eps = 7.0
     min_samples = 10
-    scale_factor = [16, 32, 64, 128]
+    scale_factors = [16, 32, 64, 128]
 
-    def __init__(self, exchange_ratio, **kwargs) -> None:
+    def __init__(self, exchange_ratio: float, **kwargs: Any) -> None:
+        """
+        Initialize the I3PEDataset.
+
+        Args:
+            exchange_ratio (float): Ratio of patches to exchange.
+            **kwargs (Any): Additional arguments passed to the BaseSyntheticDataset.
+        """
         self.exchange_ratio = exchange_ratio
         super(I3PEDataset, self).__init__(**kwargs)
 
@@ -24,84 +31,111 @@ class I3PEDataset(BaseSyntheticDataset):
         Dict[str, torch.Tensor], Dict[str, torch.Tensor], Dict[str, Any],
     ]:
         exchange_type_seed = random.random()
-        patch_sz = random.choice(self.scale_factor)
+        patch_size = random.choice(self.scale_factors)
 
-        if exchange_type_seed < 0.5:  # Performing Intra-Image Patch Exchange Method
-            pre_img = self.dataset[idx]['inputs']['image']
-            class_label = self._get_clustering(pre_img)
-            post_img, label = self.intRA_image_patch_exchange(pre_img, class_label, patch_sz=patch_sz)
-
-        else:  # Performing Inter-Image Patch Exchange Method
+        if exchange_type_seed < 0.5:
+            pre_image = self.dataset[idx]['inputs']['image']
+            class_labels = self._perform_clustering(pre_image)
+            post_image, label = self._intra_image_patch_exchange(pre_image, class_labels, patch_size)
+        else:
             img_1 = self.dataset[idx]['inputs']['image']
             img_2 = self.dataset[random.choice(range(len(self.dataset)))]['inputs']['image']
-            img_1_objects = self._get_objects(img_1)
-            img_2_objects = self._get_objects(img_2)
-            post_img, label = self.intER_image_patch_exchange(
-                img_1, img_2, img_1_objects, img_2_objects, patch_sz=patch_sz,
-            )
+            objects_1 = self._segment_objects(img_1)
+            objects_2 = self._segment_objects(img_2)
+            post_image, label = self._inter_image_patch_exchange(img_1, img_2, objects_1, objects_2, patch_size)
 
-        return pre_img, post_img, label
+        return pre_image, post_image, label
 
-    def _get_objects(self, image: torch.Tensor) -> numpy.ndarray:
+    def _segment_objects(self, image: torch.Tensor) -> np.ndarray:
+        """
+        Perform object segmentation using SLIC.
+
+        Args:
+            image (torch.Tensor): Input image tensor.
+
+        Returns:
+            np.ndarray: Object segmentation map.
+        """
         return slic(image.numpy(), n_segments=self.n_segments, start_label=0)
 
-    def _get_clustering(self, image: torch.Tensor) -> numpy.ndarray:
-        objects = self._get_objects(image)
-        obj_num = numpy.max(objects)
-        feat_vect = numpy.zeros(size=(obj_num + 1, 6))
-        for obj_idx in range(0, obj_num + 1):
-            feat_vect[obj_idx] = numpy.concatenate([
-                numpy.mean(image[objects == obj_idx], axis=0),
-                numpy.std(image[objects == obj_idx], axis=0),
-            ], axis=0)
-        clustered_labels = DBSCAN(
-            eps=self.eps, min_samples=self.min_samples, leaf_size=50, n_jobs=12, metric='euclidean'
-        ).fit(feat_vect).labels_
-        clustered_map = numpy.zeros(size=image.shape[0:2])
-        for obj_idx in range(obj_num + 1):
-            clustered_map[objects == obj_idx] = clustered_labels[obj_idx]
-        clustered_map = clustered_map + 1
+    def _perform_clustering(self, image: torch.Tensor) -> np.ndarray:
+        """
+        Perform clustering on image segments using DBSCAN.
+
+        Args:
+            image (torch.Tensor): Input image tensor.
+
+        Returns:
+            np.ndarray: Clustered labels map.
+        """
+        segments = self._segment_objects(image)
+        num_segments = np.max(segments)
+
+        features = np.zeros((num_segments + 1, 6))
+        for segment_idx in range(num_segments + 1):
+            segment_mask = segments == segment_idx
+            features[segment_idx] = np.concatenate([
+                np.mean(image[segment_mask], axis=0),
+                np.std(image[segment_mask], axis=0)
+            ])
+
+        clustering = DBSCAN(eps=self.eps, min_samples=self.min_samples, metric='euclidean').fit(features)
+        cluster_labels = clustering.labels_
+
+        clustered_map = np.zeros(image.shape[:2], dtype=int)
+        for segment_idx in range(num_segments + 1):
+            clustered_map[segments == segment_idx] = cluster_labels[segment_idx] + 1
+
         return clustered_map
 
-    def intRA_image_patch_exchange(self, img: torch.Tensor, class_label: numpy.ndarray, patch_sz):
-        patch_num_in_raw = img.shape[0] // patch_sz
-        patch_idx = numpy.arange(patch_num_in_raw ** 2)
-        numpy.random.shuffle(patch_idx)
+    def _intra_image_patch_exchange(self, image: torch.Tensor, labels: np.ndarray, patch_size: int) -> Tuple[torch.Tensor, np.ndarray]:
+        """
+        Perform intra-image patch exchange.
 
-        exchange_patch_num = int((patch_num_in_raw ** 2) * self.exchange_ratio)
+        Args:
+            image (torch.Tensor): Input image tensor.
+            labels (np.ndarray): Clustering labels map.
+            patch_size (int): Size of patches to exchange.
 
-        exchange_img = img.copy()
-        change_label = numpy.zeros(img.shape[0:2]).astype(numpy.uint8)
+        Returns:
+            Tuple[torch.Tensor, np.ndarray]: Modified image and change label map.
+        """
+        num_patches = image.shape[0] // patch_size
+        patch_indices = np.arange(num_patches ** 2)
+        np.random.shuffle(patch_indices)
 
-        for i in range(0, exchange_patch_num, 2):
-            first_patch_idx = numpy.unravel_index(patch_idx[i], (patch_num_in_raw, patch_num_in_raw))
-            second_patch_idx = numpy.unravel_index(patch_idx[i + 1], (patch_num_in_raw, patch_num_in_raw))
+        num_exchanges = int(num_patches ** 2 * self.exchange_ratio)
+        exchanged_image = image.clone()
+        change_map = np.zeros(image.shape[:2], dtype=np.uint8)
 
-            first_patch = img[patch_sz * first_patch_idx[0]: patch_sz * (first_patch_idx[0] + 1),
-                          patch_sz * first_patch_idx[1]: patch_sz * (first_patch_idx[1] + 1)]
-            second_patch = img[patch_sz * second_patch_idx[0]: patch_sz * (second_patch_idx[0] + 1),
-                           patch_sz * second_patch_idx[1]: patch_sz * (second_patch_idx[1] + 1)]
+        for i in range(0, num_exchanges, 2):
+            idx_1 = np.unravel_index(patch_indices[i], (num_patches, num_patches))
+            idx_2 = np.unravel_index(patch_indices[i + 1], (num_patches, num_patches))
 
-            temp = first_patch.copy()
-            exchange_img[patch_sz * first_patch_idx[0]: patch_sz * (first_patch_idx[0] + 1),
-            patch_sz * first_patch_idx[1]: patch_sz * (first_patch_idx[1] + 1)] = second_patch
-            exchange_img[patch_sz * second_patch_idx[0]: patch_sz * (second_patch_idx[0] + 1),
-            patch_sz * second_patch_idx[1]: patch_sz * (second_patch_idx[1] + 1)] = temp
+            patch_1 = image[patch_size * idx_1[0]:patch_size * (idx_1[0] + 1),
+                            patch_size * idx_1[1]:patch_size * (idx_1[1] + 1)]
 
-            incons_label = \
-                (class_label[patch_sz * first_patch_idx[0]: patch_sz * (first_patch_idx[0] + 1),
-                 patch_sz * first_patch_idx[1]: patch_sz * (first_patch_idx[1] + 1)] !=
-                 class_label[patch_sz * second_patch_idx[0]: patch_sz * (second_patch_idx[0] + 1),
-                 patch_sz * second_patch_idx[1]: patch_sz * (second_patch_idx[1] + 1)]).astype(numpy.uint8)
+            patch_2 = image[patch_size * idx_2[0]:patch_size * (idx_2[0] + 1),
+                            patch_size * idx_2[1]:patch_size * (idx_2[1] + 1)]
 
-            #
-            change_label[patch_sz * first_patch_idx[0]: patch_sz * (first_patch_idx[0] + 1),
-            patch_sz * first_patch_idx[1]: patch_sz * (first_patch_idx[1] + 1)] = incons_label
+            exchanged_image[patch_size * idx_1[0]:patch_size * (idx_1[0] + 1),
+                            patch_size * idx_1[1]:patch_size * (idx_1[1] + 1)] = patch_2
 
-            change_label[patch_sz * second_patch_idx[0]: patch_sz * (second_patch_idx[0] + 1),
-            patch_sz * second_patch_idx[1]: patch_sz * (second_patch_idx[1] + 1)] = incons_label
+            exchanged_image[patch_size * idx_2[0]:patch_size * (idx_2[0] + 1),
+                            patch_size * idx_2[1]:patch_size * (idx_2[1] + 1)] = patch_1
 
-        return exchange_img, change_label
+            inconsistency = (labels[patch_size * idx_1[0]:patch_size * (idx_1[0] + 1),
+                                  patch_size * idx_1[1]:patch_size * (idx_1[1] + 1)] !=
+                             labels[patch_size * idx_2[0]:patch_size * (idx_2[0] + 1),
+                                  patch_size * idx_2[1]:patch_size * (idx_2[1] + 1)]).astype(np.uint8)
+
+            change_map[patch_size * idx_1[0]:patch_size * (idx_1[0] + 1),
+                       patch_size * idx_1[1]:patch_size * (idx_1[1] + 1)] = inconsistency
+
+            change_map[patch_size * idx_2[0]:patch_size * (idx_2[0] + 1),
+                       patch_size * idx_2[1]:patch_size * (idx_2[1] + 1)] = inconsistency
+
+        return exchanged_image, change_map
 
     def intER_image_patch_exchange(self, img_1: torch.Tensor, img_2: torch.Tensor, object_1: numpy.ndarray, object_2: numpy.ndarray, patch_sz):
         concat_img = numpy.concatenate([img_1, img_2], axis=1)
