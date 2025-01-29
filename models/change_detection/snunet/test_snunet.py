5b08a53ec4cfe0ef4701232d064201a6d8f0e755
Y29lin <y29lin@uwaterloo.ca>
2025-01-29 17:44:12 -0500

    [Model][ChangeDetection] Implement SNUNet-cd model

--- models/change_detection/snunet/test_snunet.py
+++ models/change_detection/snunet/test_snunet.py
@@ -8,4 +8,5 @@ def test_snunet() -> None:
         'img_1': torch.zeros(size=(16, 3, 32, 32)),
         'img_2': torch.zeros(size=(16, 3, 32, 32)),
     }
-    model(inputs)
+    output = model(inputs)
+    assert output.shape == torch.Size([16, 2, 32, 32]), f'{output.shape=}'
