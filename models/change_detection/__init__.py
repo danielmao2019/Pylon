d7a5a0328237f90633f65240ae544c3abae0a2cc
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-01-29 14:35:36 -0500

    [Models][ChangeDetection] Implement I3PE model (#65)

--- models/change_detection/__init__.py
+++ models/change_detection/__init__.py
@@ -3,11 +3,13 @@ MODELS.CHANGE_DETECTION API
 """
 from models.change_detection.fully_convolutional_siamese_networks import FullyConvolutionalSiameseNetwork
 from models.change_detection.change_star.change_star import ChangeStar
+from models.change_detection.i3pe.i3pe_model import I3PEModel
 # from models.change_detection.cyws_3d.cyws_3d import CYWS3D
 
 
 __all__ = (
     'FullyConvolutionalSiameseNetwork',
     'ChangeStar',
+    'I3PEModel',
     # 'CYWS3D',
 )
