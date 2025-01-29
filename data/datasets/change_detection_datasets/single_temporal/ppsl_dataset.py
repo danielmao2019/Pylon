802ae06aa83c65da031472ca92ea4412a9c87ac3
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-01-28 22:57:00 -0500

    [Data][Datasets] Implement PPSL dataset class (#62)

---
+++ data/datasets/change_detection_datasets/single_temporal/ppsl_dataset.py
@@ -0,0 +1,64 @@
+from typing import Tuple, Dict, Any
+from data.datasets import BaseSyntheticDataset
+import random
+import torch
+import torchvision
+
+
+class PPSLDataset(BaseSyntheticDataset):
+    __doc__ = r"""
+    References:
+        * https://github.com/SGao1997/PPSL_MGFDNet/blob/main/dataset_half_bz24.py#L19
+    """
+
+    INPUT_NAMES = ['img_1', 'img_2']
+    LABEL_NAMES = ['change_map']
+
+    def __init__(self, **kwargs) -> None:
+        super(PPSLDataset, self).__init__(**kwargs)
+        self.colorjit = torchvision.transforms.ColorJitter(brightness=0.7, contrast=0.7, saturation=0.7, hue=0.2)
+        self.affine = torchvision.transforms.RandomAffine(degrees=(-5, 5), scale=(1, 1.02), translate=(0.02, 0.02), shear=(-5, 5))
+
+    def _load_datapoint(self, idx: int) -> Tuple[
+        Dict[str, torch.Tensor], Dict[str, torch.Tensor], Dict[str, Any],
+    ]:
+        # Fetch the primary datapoint
+        img_1 = self.source[idx]['inputs']['image']
+        label_1 = self.source[idx]['labels']['semantic_map']
+
+        # Apply color jitter to the first image
+        img_1 = self.colorjit(img_1)
+
+        # Select a random second datapoint
+        idx_2 = random.choice(range(len(self.source)))
+        img_2 = self.source[idx_2]['inputs']['image']
+        label_2 = self.source[idx_2]['labels']['semantic_map']
+
+        # Apply affine transformation to the second image
+        img_2 = self.affine(img_2)
+
+        # Create the patch from the top half of the first image and label
+        patch_img = img_1[:, 256:512, :]  # Extract patch from the first image
+        patch_label = label_1[256:512, :]  # Extract patch from the first label
+
+        # Apply the patch to the second image and label
+        img_2_patched = img_2.clone()  # Ensure we don't modify the original tensor
+        label_2_patched = label_2.clone()
+
+        img_2_patched[:, 256:512, :] = patch_img
+        label_2_patched[256:512, :] = patch_label
+
+        # Calculate the change map (logical XOR of labels)
+        change_map = (label_1 != label_2_patched).type(torch.int64)
+
+        # Prepare the inputs and labels
+        inputs = {
+            'img_1': img_1,
+            'img_2': img_2_patched,
+        }
+        labels = {
+            'change_map': change_map,
+        }
+        meta_info = {}
+
+        return inputs, labels, meta_info
--- data/datasets/change_detection_datasets/single_temporal/test_ppsl_dataset.py
+++ data/datasets/change_detection_datasets/single_temporal/test_ppsl_dataset.py
@@ -1,19 +1,19 @@
-from .i3pe_dataset import I3PEDataset
+from .ppsl_dataset import PPSLDataset
 import torch
-from data.datasets import Bi2SingleTemporal, SYSU_CD_Dataset
+from data.datasets import WHU_BD_Dataset
 
 
-def test_i3pe_dataset() -> None:
-    source = Bi2SingleTemporal(SYSU_CD_Dataset(data_root="./data/datasets/soft_links/SYSU-CD", split='train'))
-    dataset = I3PEDataset(source=source, dataset_size=len(source), exchange_ratio=0.1)
-    for idx in range(min(len(dataset), 100)):
+def test_ppsl_dataset() -> None:
+    source = WHU_BD_Dataset(data_root="./data/datasets/soft_links/WHU-BD", split='train')
+    dataset = PPSLDataset(source=source, dataset_size=len(source))
+    for idx in range(len(dataset)):
         datapoint = dataset[idx]
         assert type(datapoint) == dict
         assert set(datapoint.keys()) == set(['inputs', 'labels', 'meta_info'])
         # inspect inputs
         inputs = datapoint['inputs']
         assert type(inputs) == dict
-        assert set(inputs.keys()) == set(I3PEDataset.INPUT_NAMES)
+        assert set(inputs.keys()) == set(PPSLDataset.INPUT_NAMES)
         img_1 = inputs['img_1']
         img_2 = inputs['img_2']
         assert type(img_1) == torch.Tensor and img_1.ndim == 3 and img_1.dtype == torch.float32
@@ -22,7 +22,7 @@ def test_i3pe_dataset() -> None:
         # inspect labels
         labels = datapoint['labels']
         assert type(labels) == dict
-        assert set(labels.keys()) == set(I3PEDataset.LABEL_NAMES)
+        assert set(labels.keys()) == set(PPSLDataset.LABEL_NAMES)
         change_map = labels['change_map']
         assert type(change_map) == torch.Tensor and change_map.ndim == 2 and change_map.dtype == torch.int64
         assert set(torch.unique(change_map).tolist()).issubset({0, 1}), f"{torch.unique(change_map)=}"
