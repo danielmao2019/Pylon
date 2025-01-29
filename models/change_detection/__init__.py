b3fc4859c84414508b6511db6fd35ea86e46a1e9
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-01-29 14:47:33 -0500

    [Models][ChangeDetection] Implement PPSL model class (#66)

--- models/change_detection/__init__.py
+++ models/change_detection/__init__.py
@@ -4,6 +4,7 @@ MODELS.CHANGE_DETECTION API
 from models.change_detection.fully_convolutional_siamese_networks import FullyConvolutionalSiameseNetwork
 from models.change_detection.change_star.change_star import ChangeStar
 from models.change_detection.i3pe.i3pe_model import I3PEModel
+from models.change_detection.ppsl_model import PPSLModel
 # from models.change_detection.cyws_3d.cyws_3d import CYWS3D
 
 
@@ -11,5 +12,6 @@ __all__ = (
     'FullyConvolutionalSiameseNetwork',
     'ChangeStar',
     'I3PEModel',
+    'PPSLModel',
     # 'CYWS3D',
 )
