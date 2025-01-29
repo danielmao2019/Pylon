a6ff81aa671040e85ee247f771c58533fd2095d4
Daniel Mao <danielmao2019@gmail.com>
2025-01-29 14:33:50 -0500

    f

--- models/change_detection/i3pe/test_i3pe_model.py
+++ models/change_detection/i3pe/test_i3pe_model.py
@@ -4,6 +4,6 @@ import torch
 
 def test_i3pe_model() -> None:
     model = I3PEModel().to('cuda')
-    img_1 = torch.zeros(size=(3, 224, 224), device='cuda')
-    img_2 = torch.zeros(size=(3, 224, 224), device='cuda')
+    img_1 = torch.zeros(size=(1, 3, 224, 224), device='cuda')
+    img_2 = torch.zeros(size=(1, 3, 224, 224), device='cuda')
     _ = model(img_1, img_2)
