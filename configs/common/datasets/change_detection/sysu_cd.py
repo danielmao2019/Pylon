6aad3d023e5bc1f495e5ca4d506038db20ef289d
Daniel Mao <danielmao2019@gmail.com>
2025-01-29 20:16:03 -0500

    f

--- configs/common/datasets/change_detection/sysu_cd.py
+++ configs/common/datasets/change_detection/sysu_cd.py
@@ -74,7 +74,7 @@ config = {
         },
     },
     'criterion': {
-        'class': criteria.vision_2d.SymmetricChangeDetectionCriterion,
+        'class': criteria.vision_2d.SemanticSegmentationCriterion,
         'args': {
             'class_weights': tuple((
                 data.datasets.SYSU_CD_Dataset.NUM_CLASSES*(1/torch.tensor(data.datasets.SYSU_CD_Dataset.CLASS_DIST['train']))/torch.sum(1/torch.tensor(data.datasets.SYSU_CD_Dataset.CLASS_DIST['train']))
