c92d4900475f07fd5539ddd00fdcc6b95d19735d
Daniel Mao <danielmao2019@gmail.com>
2025-01-29 20:17:59 -0500

    f

--- criteria/vision_2d/symmetric_change_detection_criterion.py
+++ criteria/vision_2d/symmetric_change_detection_criterion.py
@@ -5,10 +5,10 @@ import torch
 
 class SymmetricChangeDetectionCriterion(SemanticSegmentationCriterion):
 
-    def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: torch.Tensor) -> torch.Tensor:
+    def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
         assert type(y_pred) == dict and set(y_pred.keys()) == {'change_map_12', 'change_map_21'}
-        assert type(y_true) == torch.Tensor
-        loss_12 = self.criterion(y_pred['change_map_12'], y_true)
-        loss_21 = self.criterion(y_pred['change_map_21'], y_true)
+        assert type(y_true) == dict and set(y_true.keys()) == {'change_map'}
+        loss_12 = self.criterion(y_pred['change_map_12'], y_true['change_map'])
+        loss_21 = self.criterion(y_pred['change_map_21'], y_true['change_map'])
         total_loss = (loss_12 + loss_21) / 2
         return total_loss
