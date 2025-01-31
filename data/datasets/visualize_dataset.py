bd50a5c9bc590e5927d3e0e9f79d55eabe362bf8
Daniel Mao <danielmao2019@gmail.com>
2025-01-31 17:23:09 -0500

    f

--- data/datasets/visualize_dataset.py
+++ data/datasets/visualize_dataset.py
@@ -5,15 +5,14 @@ import data
 
 
 if __name__ == "__main__":
-    # source_dataset = data.datasets.Bi2SingleTemporal(
-    #     source=data.datasets.SYSU_CD_Dataset(data_root="./soft_links/SYSU-CD", split="train"),
-    # )
-    # dataset = data.datasets.I3PEDataset(
-    #     source=source_dataset,
-    #     dataset_size=len(source_dataset),
-    #     exchange_ratio=0.75,
-    # )
-    dataset = data.datasets.SYSU_CD_Dataset(data_root="./soft_links/SYSU-CD", split="train")
+    source_dataset = data.datasets.Bi2SingleTemporal(
+        source=data.datasets.SYSU_CD_Dataset(data_root="./soft_links/SYSU-CD", split="train"),
+    )
+    dataset = data.datasets.I3PEDataset(
+        source=source_dataset,
+        dataset_size=len(source_dataset),
+        exchange_ratio=0.75,
+    )
     output_dir = f"./visualization/{dataset.__class__.__name__}"
     if os.path.isdir(output_dir):
         os.system(f"rm -rf {output_dir}")
