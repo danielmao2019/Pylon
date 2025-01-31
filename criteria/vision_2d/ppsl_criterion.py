40dce9710f9d57291504a6702ef679fa0ad4db95
Daniel Mao <danielmao2019@gmail.com>
2025-01-31 15:13:46 -0500

    f

--- criteria/vision_2d/ppsl_criterion.py
+++ criteria/vision_2d/ppsl_criterion.py
@@ -19,10 +19,10 @@ class PPSLCriterion(SemanticSegmentationCriterion):
     def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
         assert type(y_pred) == dict and set(y_pred.keys()) == {'change_map', 'semantic_map', 'metrics'}
         assert type(y_true) == dict and set(y_true.keys()) == {'change_map', 'semantic_map'}
-        change_loss = self.change_criterion(inputs=y_pred['change_map'], targets=y_true['change_map'])
+        change_loss = self.change_criterion(input=y_pred['change_map'], target=y_true['change_map'])
         semantic_loss = self.semantic_criterion(
-            inputs=torch.nn.functional.interpolate(y_pred['semantic_map'], y_true['semantic_map'].shape[-2:], mode='nearest'),
-            targets=y_true['semantic_map'],
+            input=torch.nn.functional.interpolate(y_pred['semantic_map'], y_true['semantic_map'].shape[-2:], mode='nearest'),
+            target=y_true['semantic_map'],
         )
         metrics_losses = [self._metric_criterion(metric, y_true['change_map']) for metric in y_pred['metrics']]
         total_loss = change_loss + semantic_loss + sum(metrics_losses) / len(metrics_losses)
