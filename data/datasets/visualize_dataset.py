3ec5e5c2627482acf0f2482a95071a429d3f6fb6
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-01 17:11:07 -0500

    [Data][Datasets] Add visualization methods (#75)

---
+++ data/datasets/visualize_dataset.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("../../")
+import os
+import data
+
+
+if __name__ == "__main__":
+    source_dataset = data.datasets.Bi2SingleTemporal(
+        source=data.datasets.SYSU_CD_Dataset(data_root="./soft_links/SYSU-CD", split="train"),
+    )
+    dataset = data.datasets.I3PEDataset(
+        source=source_dataset,
+        dataset_size=len(source_dataset),
+        exchange_ratio=0.1,
+    )
+    output_dir = f"./visualizations/{dataset.__class__.__name__}"
+    if os.path.isdir(output_dir):
+        os.system(f"rm -rf {output_dir}")
+    os.makedirs(output_dir, exist_ok=True)
+    dataset.visualize(output_dir)
