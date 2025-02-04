4f2772445e16f2b8df72a781b4c8c9e257ba34b6
Daniel Mao <danielmao2019@gmail.com>
2025-02-03 20:20:34 -0500

    f

--- runners/__init__.py
+++ runners/__init__.py
@@ -4,10 +4,12 @@ RUNNERS API
 from runners.base_trainer import BaseTrainer
 from runners.supervised_single_task_trainer import SupervisedSingleTaskTrainer
 from runners.supervised_multi_task_trainer import SupervisedMultiTaskTrainer
+from runners import gan_trainers
 
 
 __all__ = (
     'BaseTrainer',
     'SupervisedSingleTaskTrainer',
     'SupervisedMultiTaskTrainer',
+    'gan_trainers',
 )
