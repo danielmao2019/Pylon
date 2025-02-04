9d81d3fdc4e637bb1ffe36741b66c5e7160b9bb1
Daniel Mao <danielmao2019@gmail.com>
2025-02-03 19:52:57 -0500

    f

--- runners/gan_trainers/gan_trainer.py
+++ runners/gan_trainers/gan_trainer.py
@@ -7,6 +7,19 @@ import utils
 
 class GANTrainer(BaseTrainer):
 
+    def __init__(self, *args, **kwargs) -> None:
+        super(GANTrainer, self).__init__(*args, **kwargs)
+        # sanity checks
+        assert hasattr(self, 'model')
+        assert hasattr(self.model, 'generator')
+        assert hasattr(self.model, 'discriminator')
+        assert hasattr(self, 'optimizer')
+        assert hasattr(self.optimizer, 'G_optimizer')
+        assert hasattr(self.optimizer, 'D_optimizer')
+        assert hasattr(self, 'scheduler')
+        assert hasattr(self.scheduler, 'G_scheduler')
+        assert hasattr(self.scheduler, 'D_scheduler')
+
     def _train_step_(self, dp: Dict[str, Dict[str, Any]]) -> None:
         # init time
         start_time = time.time()
@@ -14,16 +27,16 @@ class GANTrainer(BaseTrainer):
         image = dp['labels']['image']
         fake_tensor = torch.zeros_like(image, requires_grad=False)
         real_tensor = torch.ones_like(image, requires_grad=False)
-        gen_image = self.model.netG(dp['inputs'])
+        gen_image = self.model.generator(dp['inputs'])
         # update generator
-        G_loss = self.criterion(self.model.netD(gen_image), real_tensor)
+        G_loss = self.criterion(self.model.discriminator(gen_image), real_tensor)
         self.optimizer.G_optimizer.zero_grad()
         G_loss.backward()
         self.optimizer.G_optimizer.step()
         # update discriminator
         D_loss = (
-            self.criterion(self.model.netD(image), real_tensor) +
-            self.criterion(self.model.netD(gen_image), fake_tensor)
+            self.criterion(self.model.discriminator(image), real_tensor) +
+            self.criterion(self.model.discriminator(gen_image), fake_tensor)
         ) / 2
         self.optimizer.D_optimizer.zero_grad()
         D_loss.backward()
