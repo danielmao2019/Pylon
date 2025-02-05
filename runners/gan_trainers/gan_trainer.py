1dd4bdea250715abd1170bcc0f405eabe4f4f82e
Daniel Mao <danielmao2019@gmail.com>
2025-02-05 12:34:03 -0500

    f

--- runners/gan_trainers/gan_trainer.py
+++ runners/gan_trainers/gan_trainer.py
@@ -47,8 +47,8 @@ class GANTrainer(BaseTrainer):
         start_time = time.time()
         # do computation
         image = dp['labels']['image']
-        fake_tensor = torch.zeros(size=(image.size(0),), requires_grad=False)
-        real_tensor = torch.ones(size=(image.size(0),), requires_grad=False)
+        fake_tensor = torch.zeros(size=(image.size(0),), device=image.device, requires_grad=False)
+        real_tensor = torch.ones(size=(image.size(0),), device=image.device, requires_grad=False)
         gen_image = self.model.generator(dp['inputs'])
         # update generator
         G_loss = self.criterion(self.model.discriminator(gen_image), real_tensor)
