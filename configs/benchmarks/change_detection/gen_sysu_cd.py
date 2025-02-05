d8f62af76e9f3e7a308b0603abe272e714122f90
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-04 21:52:25 -0500

    [Configs] Restructure MTL configs (#87)

--- configs/benchmarks/change_detection/gen_sysu_cd.py
+++ configs/benchmarks/change_detection/gen_sysu_cd.py
@@ -35,7 +35,7 @@ def main() -> None:
     # add optimizer config
     config += f"# optimizer config\n"
     config += f"from configs.common.optimizers.single_task_optimizer import single_task_optimizer_config as optimizer_config\n"
-    config += f"from configs.common.optimizers._core_ import adam_optimizer_config\n"
+    config += f"from configs.common.optimizers.standard import adam_optimizer_config\n"
     config += f"optimizer_config['args']['optimizer_config'] = adam_optimizer_config\n"
     config += f"config['optimizer'] = optimizer_config\n"
     config += '\n'
