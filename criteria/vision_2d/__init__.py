80e857e0323995cb4ad77ab9f6ee4f01f6c83c3d
Y29lin <y29lin@uwaterloo.ca>
2025-02-06 00:54:18 -0500

    f

--- criteria/vision_2d/__init__.py
+++ criteria/vision_2d/__init__.py
@@ -7,8 +7,8 @@ from criteria.vision_2d.spatial_cross_entropy_criterion import SpatialCrossEntro
 from criteria.vision_2d.semantic_segmentation_criterion import SemanticSegmentationCriterion
 from criteria.vision_2d.instance_segmentation_criterion import InstanceSegmentationCriterion
 # Change Detection Specific
-from criteria.vision_2d.symmetric_change_detection_criterion import SymmetricChangeDetectionCriterion
-from criteria.vision_2d.change_star_criterion import ChangeStarCriterion
+from criteria.vision_2d.change_detection.symmetric_change_detection_criterion import SymmetricChangeDetectionCriterion
+from criteria.vision_2d.change_detection.change_star_criterion import ChangeStarCriterion
 from criteria.vision_2d.change_detection.ppsl_criterion import PPSLCriterion
 from criteria.vision_2d.dice_loss import DiceLoss
 
