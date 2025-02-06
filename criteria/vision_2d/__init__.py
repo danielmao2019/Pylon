23d3c54053ed7844b75e02263ddc71558074b716
Y29lin <y29lin@uwaterloo.ca>
2025-02-06 00:51:23 -0500

    [Criteria][Vision_2D] Impl dice loss

--- criteria/vision_2d/__init__.py
+++ criteria/vision_2d/__init__.py
@@ -6,10 +6,9 @@ from criteria.vision_2d.normal_estimation_criterion import NormalEstimationCrite
 from criteria.vision_2d.spatial_cross_entropy_criterion import SpatialCrossEntropyCriterion
 from criteria.vision_2d.semantic_segmentation_criterion import SemanticSegmentationCriterion
 from criteria.vision_2d.instance_segmentation_criterion import InstanceSegmentationCriterion
-# Change Detection Specific
-from criteria.vision_2d.change_detection.symmetric_change_detection_criterion import SymmetricChangeDetectionCriterion
-from criteria.vision_2d.change_detection.change_star_criterion import ChangeStarCriterion
-from criteria.vision_2d.change_detection.ppsl_criterion import PPSLCriterion
+from criteria.vision_2d.symmetric_change_detection_criterion import SymmetricChangeDetectionCriterion
+from criteria.vision_2d.change_star_criterion import ChangeStarCriterion
+from criteria.vision_2d.dice_loss import DiceLoss
 
 
 __all__ = (
@@ -18,8 +17,7 @@ __all__ = (
     'SpatialCrossEntropyCriterion',
     'SemanticSegmentationCriterion',
     'InstanceSegmentationCriterion',
-    # Change Detection Specifig
     'SymmetricChangeDetectionCriterion',
     'ChangeStarCriterion',
-    'PPSLCriterion',
+    'DiceLoss',
 )
---
+++ criteria/vision_2d/dice_loss.py
@@ -0,0 +1,45 @@
+from typing import Optional
+import torch
+
+class DiceLoss(torch.nn.Module):
+    """Dice loss, need one hot encode input
+    Args:
+        eps: added to the denominator for numerical stability.
+    Reference:
+        * https://github.com/likyoo/Siam-NestedUNet/blob/master/utils/metrics.py
+    """
+    def __init__(self, eps: Optional[float] = 1e-7):
+        super(DiceLoss, self).__init__()
+        self.eps = eps
+
+    def __call__(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        """Computes the Sørensen–Dice loss.
+        Note that PyTorch optimizers minimize a loss. In this
+        case, we would like to maximize the dice loss so we
+        return the negated dice loss.
+        Args:
+            y_true: a tensor of shape [B, H, W].
+            y_pred: a tensor of shape [B, C, H, W]. Corresponds to
+                the raw output or y_pred of the model.
+        Returns:
+            dice_loss: the Sørensen–Dice loss.
+        """
+        assert isinstance(y_pred, torch.Tensor), "y_pred must be a PyTorch tensor."
+        assert isinstance(y_true, torch.Tensor), "y_true must be a PyTorch tensor."
+        assert y_pred.ndimension() == 4, "y_pred must have shape [B, C, H, W]."
+        assert y_true.ndimension() == 3, "y_true must have shape [B, H, W]."
+        #assert y_pred.shape == y_true.shape
+        #onehot encoding
+        num_classes = y_pred.shape[1]
+
+        #use onehot in scikit
+
+        true_1_hot = torch.eye(num_classes)[y_true]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        
+        #probas = torch.nn.functional.softmax(y_pred, dim=1)
+
+        dims = (y_pred.shape[0],) + tuple(range(2, y_true.ndimension()))# check dim here
+        intersection = torch.sum(y_pred * true_1_hot, dims)
+        cardinality = torch.sum(y_pred + true_1_hot, dims)
+        return (2*intersection/cardinality).mean()
