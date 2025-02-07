85d45b3195118fac6ccfb01bc3484953f6f6b026
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-06 22:23:43 -0500

    [Configs][Benchmarks][ChangeDetection] Configure multi val dataset evaluation (#98)

--- configs/common/datasets/change_detection/val/change_star_v1_xview2.py
+++ configs/common/datasets/change_detection/val/change_star_v1_xview2.py
@@ -2,6 +2,11 @@ import data.collators.change_star_collator
 import torch
 import data
 import metrics
+from .air_change import config as air_change_cfg
+from .cdd import config as cdd_cfg
+from .levir_cd import config as levir_cd_cfg
+from .oscd import config as oscd_cfg
+from .sysu_cd import config as sysu_cd_cfg
 
 
 transforms_config = {
@@ -16,7 +21,7 @@ transforms_config = {
     },
 }
 
-collate_fn_cfg = {
+change_star_v1_collate_fn_cfg = {
     'class': data.collators.ChangeStarCollator,
     'args': {
         'method': "eval",
@@ -24,38 +29,29 @@ collate_fn_cfg = {
 }
 
 config = {
-    'val_dataset': {
+    'val_datasets': [{
         'class': data.datasets.xView2Dataset,
         'args': {
             'data_root': "./data/datasets/soft_links/xView2",
             'split': "test",
             'transforms_cfg': transforms_config,
         },
-    },
-    'val_dataloader': {
+    }] + list(map(lambda x: x['val_dataset'], [air_change_cfg, cdd_cfg, levir_cd_cfg, oscd_cfg, sysu_cd_cfg])),
+    'val_dataloaders': [{
         'class': torch.utils.data.DataLoader,
         'args': {
             'batch_size': 1,
             'num_workers': 4,
-            'collate_fn': collate_fn_cfg,
+            'collate_fn': change_star_v1_collate_fn_cfg,
         },
-    },
-    'test_dataset': {
-        'class': data.datasets.xView2Dataset,
-        'args': {
-            'data_root': "./data/datasets/soft_links/xView2",
-            'split': "hold",
-            'transforms_cfg': transforms_config,
-        },
-    },
-    'test_dataloader': {
+    }] + list(map(lambda x: {
         'class': torch.utils.data.DataLoader,
         'args': {
             'batch_size': 1,
             'num_workers': 4,
-            'collate_fn': collate_fn_cfg,
+            'collate_fn': x['val_dataloader']['args']['collate_fn'],
         },
-    },
+    }, [air_change_cfg, cdd_cfg, levir_cd_cfg, oscd_cfg, sysu_cd_cfg])),
     'metric': {
         'class': metrics.vision_2d.ChangeStarMetric,
         'args': {},
