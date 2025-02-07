43afd85e52bf9ee03a6e5a4a5656665a808f9f43
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-07 14:01:43 -0500

    [Utils][Builders] Implement scheduler builder (#101)

--- utils/builders/__init__.py
+++ utils/builders/__init__.py
@@ -2,8 +2,10 @@
 UTILS.BUILDERS API
 """
 from utils.builders.builder import build_from_config
+from utils.builders.scheduler_builder import build_scheduler
 
 
 __all__ = (
     'build_from_config',
+    'build_scheduler',
 )
