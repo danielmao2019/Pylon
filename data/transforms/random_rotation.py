aa0f01f5e0948da85b51e58d13a16b32f0125514
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-07 14:27:00 -0500

    [Data][Transforms] Support rotation of 2D maps (#103)
    In addition,
    1. Renamed degrees to angle to align with torchvision API.
    2. Changed angle type annotation from int to Union[int, float].

--- data/transforms/random_rotation.py
+++ data/transforms/random_rotation.py
@@ -3,17 +3,22 @@ import random
 import torch
 import torchvision.transforms.functional as TF
 from data.transforms import BaseTransform
+from data.transforms import Rotation
 
 
 class RandomRotation(BaseTransform):
 
-    def __init__(self, choices: Optional[List[int]] = None, range: Optional[Tuple[int, int]] = None) -> None:
+    def __init__(
+        self,
+        choices: Optional[List[Union[int, float]]] = None,
+        range: Optional[Tuple[Union[int, float], Union[int, float]]] = None,
+    ) -> None:
         """
         Initializes a RandomRotation transform where the rotation angle is randomly chosen.
 
         Args:
-            choices (Optional[List[int]]): List of discrete degrees to sample from.
-            range (Optional[Tuple[int, int]]): Min/max degrees to sample from, left inclusive.
+            choices (Optional[List[Union[int, float]]]): List of discrete angle to sample from.
+            range (Optional[Tuple[Union[int, float], Union[int, float]]]): Min/max angle to sample from, inclusive.
 
         Raises:
             ValueError: If neither or both `choices` and `range` are provided.
@@ -22,11 +27,11 @@ class RandomRotation(BaseTransform):
             raise ValueError("Exactly one of `choices` or `range` must be provided, but got both None or both set.")
 
         if choices is not None:
-            if not (isinstance(choices, list) and all(isinstance(d, int) for d in choices)):
+            if not (isinstance(choices, list) and all(isinstance(d, (int, float)) for d in choices)):
                 raise ValueError("`choices` must be a list of integers.")
 
         if range is not None:
-            if not (isinstance(range, tuple) and len(range) == 2 and all(isinstance(d, int) for d in range) and range[0] < range[1]):
+            if not (isinstance(range, tuple) and len(range) == 2 and all(isinstance(d, (int, float)) for d in range) and range[0] < range[1]):
                 raise ValueError("`range` must be a tuple of two integers (min, max) with min < max.")
 
         self.choices = choices
@@ -34,10 +39,11 @@ class RandomRotation(BaseTransform):
 
     def __call__(self, *args) -> Union[torch.Tensor, List[torch.Tensor]]:
         if self.choices is not None:
-            degrees = random.choice(self.choices)
+            angle = random.choice(self.choices)
         else:
-            degrees = random.randint(self.range[0], self.range[1] - 1)  # Right exclusive range
-        transform = lambda x: TF.rotate(x, degrees)
+            angle = random.randint(self.range[0], self.range[1] - 1)  # Right exclusive range
+        angle = float(angle)
+        transform = Rotation(angle)
         result = [transform(arg) for arg in args]
         if len(result) == 1:
             result = result[0]
