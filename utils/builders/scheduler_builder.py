db721d7b282bf2f2f6b4e3eb6d7a262b26b0dfb8
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-07 14:01:43 -0500

    [Utils][Builders] Implement scheduler builder (#101)

---
+++ utils/builders/scheduler_builder.py
@@ -0,0 +1,25 @@
+import torch
+import schedulers
+from runners import BaseTrainer
+from utils.builders import build_from_config
+
+
+def build_scheduler(trainer: BaseTrainer, cfg: dict) -> torch.optim.lr_scheduler._LRScheduler:
+    assert isinstance(cfg, dict) and set(cfg.keys()) == {'class', 'args'}
+    cfg['args']['optimizer'] = trainer.optimizer.optimizer
+    if cfg['class'] == torch.optim.lr_scheduler.LambdaLR:
+        assert set(cfg['args'].keys()).issubset({'optimizer', 'lr_lambda'}), f"{cfg['args'].keys()=}"
+        lr_lambda_cfg = cfg['args']['lr_lambda']
+        if lr_lambda_cfg['class'] == schedulers.lr_lambdas.ConstantLambda:
+            pass
+        elif lr_lambda_cfg['class'] == schedulers.lr_lambdas.WarmupLambda:
+            if lr_lambda_cfg['args'].get('steps', None) is None:
+                lr_lambda_cfg['args']['steps'] = len(trainer.train_dataloader)
+        else:
+            raise NotImplementedError
+    elif cfg['class'] == torch.optim.lr_scheduler.PolynomialLR:
+        assert set(cfg['args'].keys()).issubset({'optimizer', 'total_iters', 'power'}), f"{cfg['args'].keys()=}"
+        cfg['args']['total_iters'] = len(trainer.train_dataloader) * trainer.tot_epochs
+    else:
+        raise NotImplementedError
+    return build_from_config(cfg)
