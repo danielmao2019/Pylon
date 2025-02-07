011e91ff52d3530a0dc14a68f91716ac48141818
Daniel Mao <danielmao2019@gmail.com>
2025-02-06 19:39:42 -0500

    f

--- configs/benchmarks/change_detection/gen_ppsl.py
+++ configs/benchmarks/change_detection/gen_ppsl.py
@@ -50,8 +50,8 @@ def main() -> None:
     os.makedirs(os.path.join("./configs", relpath), exist_ok=True)
     for idx, seeded_config in enumerate(seeded_configs):
         seeded_config += f"# work dir\n"
-        seeded_config += f"config['work_dir'] = \"" + os.path.join("./logs", relpath, f"ppsl_whu_bd_levir_cd_run_{idx}") + "\"\n"
-        with open(os.path.join("./configs", relpath, f"ppsl_whu_bd_levir_cd_run_{idx}.py"), mode='w') as f:
+        seeded_config += f"config['work_dir'] = \"" + os.path.join("./logs", relpath, f"whu_bd_levir_cd_run_{idx}") + "\"\n"
+        with open(os.path.join("./configs", relpath, f"whu_bd_levir_cd_run_{idx}.py"), mode='w') as f:
             f.write(seeded_config)
 
 
