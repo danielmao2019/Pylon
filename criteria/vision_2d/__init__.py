95d550e56ecf4dbc6111fdab6e501728d301dc8b
Daniel Mao <danielmao2019@gmail.com>
2025-02-10 15:38:29 -0500

    f

--- criteria/vision_2d/__init__.py
+++ criteria/vision_2d/__init__.py
@@ -6,11 +6,11 @@ from criteria.vision_2d.normal_estimation_criterion import NormalEstimationCrite
 from criteria.vision_2d.spatial_cross_entropy_criterion import SpatialCrossEntropyCriterion
 from criteria.vision_2d.semantic_segmentation_criterion import SemanticSegmentationCriterion
 from criteria.vision_2d.instance_segmentation_criterion import InstanceSegmentationCriterion
+from criteria.vision_2d.dice_loss import DiceLoss
 # Change Detection Specific
 from criteria.vision_2d.change_detection.symmetric_change_detection_criterion import SymmetricChangeDetectionCriterion
 from criteria.vision_2d.change_detection.change_star_criterion import ChangeStarCriterion
 from criteria.vision_2d.change_detection.ppsl_criterion import PPSLCriterion
-from criteria.vision_2d.dice_loss import DiceLoss
 
 
 __all__ = (
@@ -19,9 +19,9 @@ __all__ = (
     'SpatialCrossEntropyCriterion',
     'SemanticSegmentationCriterion',
     'InstanceSegmentationCriterion',
+    'DiceLoss',
     # Change Detection Specific
     'SymmetricChangeDetectionCriterion',
     'ChangeStarCriterion',
     'PPSLCriterion',
-    'DiceLoss',
 )
