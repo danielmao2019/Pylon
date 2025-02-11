a5eac82cce905cd50d85ea8eb1146ae21a93938c
Daniel Mao <danielmao2019@gmail.com>
2025-02-11 15:55:01 -0500

    copy code from online github

---
+++ models/change_detection/change_former/models/change_former_v3.py
@@ -0,0 +1,28 @@
+from functools import partial
+import torch
+from models.change_detection.change_former.modules.tenc import Tenc
+from models.change_detection.change_former.modules.tdec_v2 import TDecV2
+
+
+class ChangeFormerV3(torch.nn.Module):
+
+    def __init__(self, input_nc=3, output_nc=2, decoder_softmax=False):
+        super(ChangeFormerV3, self).__init__()
+        #Transformer Encoder
+        self.Tenc   = Tenc( patch_size=16, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 4, 8], 
+                            mlp_ratios=[4, 4, 4, 4], qkv_bias=True, norm_layer=partial(torch.nn.LayerNorm, eps=1e-6), 
+                            depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1], drop_rate=0.0, drop_path_rate=0.1)
+        
+        #Transformer Decoder
+        self.TDec   = TDecV2( input_transform='multiple_select', in_index=[0, 1, 2, 3], align_corners=True, 
+                            in_channels = [64, 128, 320, 512], embedding_dim= 64, output_nc=output_nc, 
+                            decoder_softmax = decoder_softmax, feature_strides=[4, 8, 16, 32])
+
+    def forward(self, x1, x2):
+
+        fx1 = self.Tenc(x1)
+        fx2 = self.Tenc(x2)
+
+        cp = self.TDec(fx1, fx2)
+
+        return cp
---
+++ models/change_detection/change_former/models/change_former_v4.py
@@ -0,0 +1,35 @@
+import torch
+from models.change_detection.change_former.modules.encoder_transformer_x2 import EncoderTransformer_x2
+from models.change_detection.change_former.modules.decoder_transformer_x2 import DecoderTransformer_x2
+
+
+class ChangeFormerV4(torch.nn.Module):
+
+    def __init__(self, input_nc=3, output_nc=2, decoder_softmax=False):
+        super(ChangeFormerV4, self).__init__()
+        #Transformer Encoder
+        self.embed_dims = [32, 64, 128, 320, 512]
+        self.depths     = [3, 3, 4, 12, 3] #[3, 3, 6, 18, 3]
+        self.embedding_dim = 256
+
+        self.Tenc_x2 = EncoderTransformer_x2(img_size=256, patch_size=3, in_chans=input_nc, num_classes=output_nc, embed_dims=self.embed_dims,
+                 num_heads=[2, 2, 4, 8, 16], mlp_ratios=[2, 2, 2, 2, 2], qkv_bias=False, qk_scale=None, drop_rate=0.,
+                 attn_drop_rate=0., drop_path_rate=0., norm_layer=torch.nn.LayerNorm,
+                 depths=self.depths, sr_ratios=[8, 4, 2, 1, 1])
+        
+        #Transformer Decoder
+        self.TDec_x2 = DecoderTransformer_x2(input_transform='multiple_select', in_index=[0, 1, 2, 3, 4], align_corners=True, 
+                    in_channels = self.embed_dims, embedding_dim= 256, output_nc=output_nc, 
+                    decoder_softmax = decoder_softmax, feature_strides=[2, 4, 8, 16, 32])
+
+    def forward(self, x1, x2):
+
+        [fx1, fx2] = [self.Tenc_x2(x1), self.Tenc_x2(x2)]
+
+        cp = self.TDec_x2(fx1, fx2)
+
+        # # Save to mat
+        # save_to_mat(x1, x2, fx1, fx2, cp, "ChangeFormerV4")
+
+        # exit()
+        return cp
