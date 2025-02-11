8282d2871e36f17ba79c885232d70502f9face4a
Daniel Mao <danielmao2019@gmail.com>
2025-02-11 17:00:49 -0500

    f

---
+++ configs/common/models/change_detection/change_former.py
@@ -0,0 +1,32 @@
+import models
+
+
+change_former_v1_config = {
+    'class': models.change_detection.ChangeFormerV1,
+    'args': {},
+}
+
+change_former_v2_config = {
+    'class': models.change_detection.ChangeFormerV2,
+    'args': {},
+}
+
+change_former_v3_config = {
+    'class': models.change_detection.ChangeFormerV3,
+    'args': {},
+}
+
+change_former_v4_config = {
+    'class': models.change_detection.ChangeFormerV4,
+    'args': {},
+}
+
+change_former_v5_config = {
+    'class': models.change_detection.ChangeFormerV5,
+    'args': {},
+}
+
+change_former_v6_config = {
+    'class': models.change_detection.ChangeFormerV6,
+    'args': {},
+}
