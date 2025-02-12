e37b5b14f1131b20160202035115960385cd96ef
Daniel Mao <danielmao2019@gmail.com>
2025-02-11 20:50:43 -0500

    f

--- models/change_detection/change_former/models/change_former_v4.py
+++ models/change_detection/change_former/models/change_former_v4.py
@@ -6,14 +6,14 @@ from models.change_detection.change_former.modules.decoder_transformer_x2 import
 
 class ChangeFormerV4(torch.nn.Module):
 
-    def __init__(self, img_size: int, input_nc=3, output_nc=2):
+    def __init__(self, input_nc=3, output_nc=2):
         super(ChangeFormerV4, self).__init__()
         #Transformer Encoder
         self.embed_dims = [32, 64, 128, 320, 512]
         self.depths     = [3, 3, 4, 12, 3] #[3, 3, 6, 18, 3]
         self.embedding_dim = 256
 
-        self.Tenc_x2 = EncoderTransformer_x2(img_size=img_size, patch_size=3, in_chans=input_nc, num_classes=output_nc, embed_dims=self.embed_dims,
+        self.Tenc_x2 = EncoderTransformer_x2(img_size=256, patch_size=3, in_chans=input_nc, num_classes=output_nc, embed_dims=self.embed_dims,
                  num_heads=[2, 2, 4, 8, 16], mlp_ratios=[2, 2, 2, 2, 2], qkv_bias=False, qk_scale=None, drop_rate=0.,
                  attn_drop_rate=0., drop_path_rate=0., norm_layer=torch.nn.LayerNorm,
                  depths=self.depths, sr_ratios=[8, 4, 2, 1, 1])
