bb1b9bfd55d23656eb33fef1b1164ad1f127d401
Y29lin <y29lin@uwaterloo.ca>
2025-02-12 18:17:24 -0500

    f

--- configs/common/models/change_detection/snunet-cd.py
+++ configs/common/models/change_detection/snunet-cd.py
@@ -1,7 +1,8 @@
 import models
+import models.change_detection.snunet
 
 
 model_config = {
-    'class': models.change_detection.snunet,
+    'class': models.change_detection.SNUNet_ECAM,
     'args': {},
 }
