15c3e07ab441bd162475c0b77ddf4a757e5da373
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-13 16:09:17 -0500

    [Criterion][Vision2D][ChangeDetection] Implement FTNCriterion class (#115)

---
+++ criteria/vision_2d/change_detection/ftn_criterion.py
@@ -0,0 +1,28 @@
+from typing import Tuple, Dict, Optional
+import torch
+from criteria.wrappers import SingleTaskCriterion, AuxiliaryOutputsCriterion
+from criteria.vision_2d import SemanticSegmentationCriterion, IoULoss, SSIMLoss
+
+
+class FTNCriterion(SingleTaskCriterion):
+
+    def __init__(self, num_classes: Optional[int] = 2) -> None:
+        super(FTNCriterion, self).__init__()
+        self.ce_criterion = AuxiliaryOutputsCriterion(SemanticSegmentationCriterion())
+        self.ssim_criterion = AuxiliaryOutputsCriterion(SSIMLoss(channels=num_classes))
+        self.iou_criterion = AuxiliaryOutputsCriterion(IoULoss())
+
+    def __call__(self, y_pred: Tuple[torch.Tensor], y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
+        # input checks
+        assert isinstance(y_pred, tuple) and len(y_pred) == 4, f"{type(y_pred)=}, {len(y_pred)=}"
+        assert all(isinstance(x, torch.Tensor) for x in y_pred)
+        assert isinstance(y_true, dict) and set(y_true.keys()) == {'change_map'}
+        # compute losses
+        ce_loss = self.ce_criterion(y_pred, y_true['change_map'])
+        ssim_loss = self.ssim_criterion(y_pred, y_true['change_map'])
+        iou_loss = self.iou_criterion(y_pred, y_true['change_map'])
+        total_loss = ce_loss + ssim_loss + iou_loss
+        assert total_loss.ndim == 0, f"{total_loss.shape=}"
+        # log loss
+        self.buffer.append(total_loss.detach().cpu())
+        return total_loss
