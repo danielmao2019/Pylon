c9f961aa0d8552342ef40ae906bada6075d01c44
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-13 16:09:17 -0500

    [Criterion][Vision2D][ChangeDetection] Implement FTNCriterion class (#115)

--- criteria/vision_2d/__init__.py
+++ criteria/vision_2d/__init__.py
@@ -14,6 +14,7 @@ from criteria.vision_2d.semantic_map.ssim_loss import SSIMLoss
 # Change Detection Specific
 from criteria.vision_2d.change_detection.symmetric_change_detection_criterion import SymmetricChangeDetectionCriterion
 from criteria.vision_2d.change_detection.change_former_criterion import ChangeFormerCriterion
+from criteria.vision_2d.change_detection.ftn_criterion import FTNCriterion
 from criteria.vision_2d.change_detection.change_star_criterion import ChangeStarCriterion
 from criteria.vision_2d.change_detection.ppsl_criterion import PPSLCriterion
 
@@ -32,6 +33,7 @@ __all__ = (
     # Change Detection Specific
     'SymmetricChangeDetectionCriterion',
     'ChangeFormerCriterion',
+    'FTNCriterion',
     'ChangeStarCriterion',
     'PPSLCriterion',
 )
