1b56dcee734193525c096d9f7a4e65f401b2bcbe
Daniel Mao <danielmao2019@gmail.com>
2025-02-13 22:12:21 -0500

    f

---
+++ models/change_detection/csa_cdgan/models.py
@@ -0,0 +1,13 @@
+import torch
+import utils
+
+
+class CSA_CDGAN(torch.nn.Module):
+
+    def __init__(self, generator_cfg: dict, discriminator_cfg: dict) -> None:
+        super(CSA_CDGAN, self).__init__()
+        self.generator = utils.builders.build_from_config(generator_cfg)
+        self.discriminator = utils.builders.build_from_config(discriminator_cfg)
+
+    def forward(self):
+        raise NotImplementedError
