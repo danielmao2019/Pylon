4202de2a8dcc752c14eeb06f21177ad4dc5f97cd
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-15 13:54:00 -0500

    [Models][ChangeDetection] Debug FTN eval stage (#118)

--- configs/benchmarks/change_detection/air_change/FTN_run_2.py
+++ configs/benchmarks/change_detection/air_change/FTN_run_2.py
@@ -57,9 +57,9 @@ from configs.common.datasets.change_detection.val.air_change import config as va
 config.update(val_dataset_config)
 
 # model config
-import models
 from configs.common.models.change_detection.ftn import model_config
 config['model'] = model_config
+
 import criteria
 config['criterion'] = {'class': criteria.vision_2d.FTNCriterion, 'args': {}}
 
