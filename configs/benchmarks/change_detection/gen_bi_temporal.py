48a274d9324e235e457dc444126061f7aa4ce3c6
Daniel Mao <danielmao2019@gmail.com>
2025-02-15 15:46:33 -0500

    f

--- configs/benchmarks/change_detection/gen_bi_temporal.py
+++ configs/benchmarks/change_detection/gen_bi_temporal.py
@@ -37,7 +37,7 @@ def main(dataset: str, model: str) -> None:
         config += f"config['model']['args']['arch'] = \"{model}\"\n"
         config += f"config['model']['args']['in_channels'] = {6 if model == 'FC-EF' else 3}\n"
         config += '\n'
-    elif model == "SNUNet_ECAM":
+    elif model == "SNUNet-ECAM":
         config += f"from configs.common.models.change_detection.snunet import model_config\n"
         config += f"config['model'] = model_config\n"
         config += f"import criteria\n"
@@ -64,7 +64,7 @@ def main(dataset: str, model: str) -> None:
         config += f"import criteria\n"
         config += f"config['criterion'] = {{'class': criteria.vision_2d.change_detection.FTNCriterion, 'args': {{}}}}\n"
         config += '\n'
-    elif model == "CSA_CDGAN":
+    elif model == "CSA-CDGAN":
         config += f"from configs.common.models.change_detection.csa_cdgan import model_config\n"
         config += f"config['model'] = model_config\n"
         config += '\n'
@@ -92,9 +92,10 @@ if __name__ == "__main__":
     for dataset, model in itertools.product(
         ['air_change', 'cdd', 'levir_cd', 'oscd', 'sysu_cd'],
         [
-            'FC-EF', 'FC-Siam-conc', 'FC-Siam-diff', 'SNUNet_ECAM',
+            'FC-EF', 'FC-Siam-conc', 'FC-Siam-diff', 'SNUNet-ECAM',
             'ChangeFormerV1', 'ChangeFormerV2', 'ChangeFormerV3', 'ChangeFormerV4', 'ChangeFormerV5', 'ChangeFormerV6',
             'FTN',
+            'CSA-CDGAN',
         ],
     ):
         main(dataset, model)
