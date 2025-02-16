72cb669e8fcb7415fcf74074cbde24178d56cc9d
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-15 19:57:03 -0500

    [Models][ChangeDetection] Implement CSA-CDGAN model, criterion, trainer, and configs (#119)

--- criteria/vision_2d/change_detection/__init__.py
+++ criteria/vision_2d/change_detection/__init__.py
@@ -1,2 +1,21 @@
-"""Intentionally blank.
 """
+CRITERIA.VISION_2D.CHANGE_DETECTION API
+"""
+from criteria.vision_2d.change_detection.symmetric_change_detection_criterion import SymmetricChangeDetectionCriterion
+from criteria.vision_2d.change_detection.snunet_criterion import SNUNetCDCriterion
+from criteria.vision_2d.change_detection.change_former_criterion import ChangeFormerCriterion
+from criteria.vision_2d.change_detection.ftn_criterion import FTNCriterion
+from criteria.vision_2d.change_detection.csa_cdgan_criterion import CSA_CDGAN_Criterion
+from criteria.vision_2d.change_detection.change_star_criterion import ChangeStarCriterion
+from criteria.vision_2d.change_detection.ppsl_criterion import PPSLCriterion
+
+
+__all__ = (
+    'SymmetricChangeDetectionCriterion',
+    'SNUNetCDCriterion',
+    'ChangeFormerCriterion',
+    'FTNCriterion',
+    'CSA_CDGAN_Criterion',
+    'ChangeStarCriterion',
+    'PPSLCriterion',
+)
---
+++ criteria/vision_2d/change_detection/csa_cdgan_criterion.py
@@ -0,0 +1,51 @@
+from typing import Dict
+import torch
+from criteria.wrappers import SingleTaskCriterion, MultiTaskCriterion
+
+
+class CSA_CDGAN_GeneratorCriterion(SingleTaskCriterion):
+
+    def __init__(self) -> None:
+        super(CSA_CDGAN_GeneratorCriterion, self).__init__()
+        self.l_bce = torch.nn.BCELoss()
+        self.l_con = torch.nn.L1Loss()
+
+    def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        err_g_total = self.l_con(torch.nn.functional.softmax(y_pred['gen_image'], dim=1), y_true['change_map'])
+        assert err_g_total.ndim == 0, f"{err_g_total.shape=}"
+        # log loss
+        self.buffer.append(err_g_total.detach().cpu())
+        return err_g_total
+
+
+class CSA_CDGAN_DiscriminatorCriterion(SingleTaskCriterion):
+
+    def __init__(self) -> None:
+        super(CSA_CDGAN_DiscriminatorCriterion, self).__init__()
+        self.l_bce = torch.nn.BCELoss()
+
+    def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        err_d_real = self.l_bce(y_pred['pred_real'], y_true['real_label'])
+        err_d_fake = self.l_bce(y_pred['pred_fake'], y_true['fake_label'])
+        err_d_total = (err_d_real + err_d_fake) * 0.5
+        assert err_d_total.ndim == 0, f"{err_d_total.shape=}"
+        # log loss
+        self.buffer.append(err_d_total.detach().cpu())
+        return err_d_total
+
+
+class CSA_CDGAN_Criterion(MultiTaskCriterion, torch.nn.Module):
+
+    def __init__(self) -> None:
+        self.task_criteria = {
+            'generator': CSA_CDGAN_GeneratorCriterion(),
+            'discriminator': CSA_CDGAN_DiscriminatorCriterion(),
+        }
+        self.task_names = set(self.task_criteria.keys())
+        self.reset_buffer()
+
+    def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        raise NotImplementedError
+
+    def to(self, *args, **kwargs) -> None:
+        return self
