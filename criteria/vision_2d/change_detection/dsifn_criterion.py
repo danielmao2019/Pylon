377ce84d5b6cb175607caca1d89fdbefef7d1074
Daniel Mao <danielmao2019@gmail.com>
2025-02-18 20:28:21 -0500

    f

--- criteria/vision_2d/change_detection/dsifn_criterion.py
+++ criteria/vision_2d/change_detection/dsifn_criterion.py
@@ -8,7 +8,7 @@ class DSIFNCriterion(SingleTaskCriterion):
 
     def __init__(self) -> None:
         super(DSIFNCriterion, self).__init__()
-        self.bce_loss = AuxiliaryOutputsCriterion(torch.nn.BCELoss())
+        self.bce_loss = AuxiliaryOutputsCriterion(torch.nn.BCEWithLogitsLoss())
         self.dice_loss = AuxiliaryOutputsCriterion(DiceLoss())
 
     def __call__(self, y_pred: Tuple[torch.Tensor, ...], y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
@@ -16,7 +16,6 @@ class DSIFNCriterion(SingleTaskCriterion):
         assert all(isinstance(x, torch.Tensor) for x in y_pred)
         assert isinstance(y_true, dict)
         assert set(y_true.keys()) == {'change_map'}
-        y_pred = list(map(lambda x: torch.sigmoid(x), y_pred))
         y_true = y_true['change_map'].unsqueeze(1)
         bce_loss = self.bce_loss(y_pred, y_true)
         dice_loss = self.dice_loss(y_pred, y_true)
