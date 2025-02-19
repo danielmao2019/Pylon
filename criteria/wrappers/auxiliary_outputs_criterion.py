08e012b1714859620c4fd823b83512f23b2cf0f3
Daniel Mao <danielmao2019@gmail.com>
2025-02-18 20:08:14 -0500

    f

--- criteria/wrappers/auxiliary_outputs_criterion.py
+++ criteria/wrappers/auxiliary_outputs_criterion.py
@@ -1,4 +1,4 @@
-from typing import Sequence, Dict, Union
+from typing import Sequence, Dict, Union, Optional
 import torch
 from .single_task_criterion import SingleTaskCriterion
 from utils.builders import build_from_config
@@ -8,9 +8,13 @@ class AuxiliaryOutputsCriterion(SingleTaskCriterion):
     __doc__ = r"""Compare multiple predictions to the same ground truth and sum all losses.
     """
 
-    def __init__(self, criterion_cfg: dict) -> None:
+    REDUCTION_OPTIONS = ['sum', 'mean']
+
+    def __init__(self, criterion_cfg: dict, reduction: Optional[str] = 'sum') -> None:
         super(AuxiliaryOutputsCriterion, self).__init__()
         self.criterion = build_from_config(config=criterion_cfg)
+        assert reduction in self.REDUCTION_OPTIONS
+        self.reduction = reduction
 
     def __call__(
         self,
@@ -23,9 +27,13 @@ class AuxiliaryOutputsCriterion(SingleTaskCriterion):
         assert type(y_pred) in [tuple, list], f"{type(y_pred)=}"
         assert all(type(elem) == torch.Tensor for elem in y_pred)
         # compute losses
-        loss: torch.Tensor = sum([
+        losses: torch.Tensor = torch.stack([
             self.criterion(y_pred=each_y_pred, y_true=y_true) for each_y_pred in y_pred
-        ])
+        ], dim=0)
+        if self.reduction == 'sum':
+            loss = losses.sum()
+        else:
+            loss = losses.mean()
         assert loss.ndim == 0, f"{loss.shape=}"
         # log loss
         self.buffer.append(loss.detach().cpu())
