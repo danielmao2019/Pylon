e6b1c9c007e5e98b923bcb779b58bbaf65fcbb7a
Y29lin <y29lin@uwaterloo.ca>
2025-02-20 16:40:41 -0500

    f

--- configs/benchmarks/change_detection/gen_bi_temporal.py
+++ configs/benchmarks/change_detection/gen_bi_temporal.py
@@ -105,6 +105,16 @@ def main(dataset: str, model: str) -> None:
         config += f"from runners.gan_trainers import CSA_CDGAN_Trainer\n"
         config += f"config['runner'] = CSA_CDGAN_Trainer\n"
         config += '\n'
+    elif model == "LWGANet":
+        config += f"from configs.common.models.change_detection.csa_cdgan import model_config\n"
+        config += f"config['model'] = model_config\n"
+        config += '\n'
+        config += f"# criterion config\n"
+        config += f"import criteria\n"
+        config += f"config['criterion'] = {{'class': criteria.vision_2d.change_detection.LWGANetCriterion, 'args': {{}}}}\n"
+        config += '\n'
+        config += f'# setup distributed computing'
+        config += f'if not dist.is_initialized():dist.init_process_group(backend={'nccl'},init_method={'tcp://127.0.0.1:23456'},rank=0,world_size=1)'
     else:
         raise NotImplementedError
     # add seeds
