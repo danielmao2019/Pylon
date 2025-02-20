87d996eafd2d813099aa29fe41af03a8ed28bc69
Y29lin <y29lin@uwaterloo.ca>
2025-02-20 00:53:38 -0500

    f

---
+++ criteria/vision_2d/change_detection/lwganet_criterion.py
@@ -0,0 +1,23 @@
+from typing import Dict
+import torch
+import criteria
+from criteria.wrappers import SingleTaskCriterion
+from criteria.vision_2d import DiceLoss
+
+
+class LWGANetCriterion(SingleTaskCriterion):
+
+    def __init__(self) -> None:
+        super(LWGANetCriterion, self).__init__()
+        # unbounded
+        self.bce_criterion = torch.nn.functional.binary_cross_entropy_with_logits()
+        self.dice_criterion = DiceLoss()
+
+    def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """Override parent class __call__ method.
+        """
+        assert set(y_true.keys()) == {'change_map'}
+        bce_loss += (self.bce_criterion(y_pred=mask, y_true=y_true['change_map']) for mask in y_pred.values()) 
+        dice_loss += (self.dice_criterion(y_pred=mask, y_true=y_true['change_map']) for mask in y_pred.values()) 
+        
+        return bce_loss + 4 - dice_loss
