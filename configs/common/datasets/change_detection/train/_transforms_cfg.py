694b3e592c9471d0ece53dd6c6320529dd6526cd
Daniel Mao <danielmao2019@gmail.com>
2025-02-21 17:36:11 -0500

    f

--- configs/common/datasets/change_detection/train/_transforms_cfg.py
+++ configs/common/datasets/change_detection/train/_transforms_cfg.py
@@ -1,17 +1,26 @@
-from typing import Tuple
+from typing import Tuple, Optional
 import torchvision
 import data
 
 
-def transforms_cfg(size: Tuple[int, int]) -> dict:
+def transforms_cfg(size: Tuple[int, int], first: Optional[str] = "RandomCrop") -> dict:
+    if first == "RandomCrop":
+        first_transform = [(
+            data.transforms.RandomCrop(size=size),
+            [('inputs', 'img_1'), ('inputs', 'img_2'), ('labels', 'change_map')],
+        )]
+    else:
+        first_transform = [(
+            data.transforms.resize.ResizeMaps(size=size, interpolation=torchvision.transforms.InterpolationMode.BILINEAR),
+            [('inputs', 'img_1'), ('inputs', 'img_2')],
+        ), (
+            data.transforms.resize.ResizeMaps(size=size, interpolation=torchvision.transforms.InterpolationMode.NEAREST),
+            [('labels', 'change_map')],
+        )]
     return {
         'class': data.transforms.Compose,
         'args': {
-            'transforms': [
-                (
-                    data.transforms.RandomCrop(size=size),
-                    [('inputs', 'img_1'), ('inputs', 'img_2'), ('labels', 'change_map')],
-                ),
+            'transforms': first_transform + [
                 (
                     data.transforms.RandomRotation(choices=[0, 90, 180, 270]),
                     [('inputs', 'img_1'), ('inputs', 'img_2'), ('labels', 'change_map')],
