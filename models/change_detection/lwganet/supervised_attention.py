0476e52811fd34b5e5cd1c3e1b2adb79bc5e9b3c
Y29lin <y29lin@uwaterloo.ca>
2025-02-21 01:04:36 -0500

    f

--- models/change_detection/lwganet/supervised_attention.py
+++ models/change_detection/lwganet/supervised_attention.py
@@ -5,9 +5,9 @@ class SupervisedAttentionModule(torch.nn.Module):
         super(SupervisedAttentionModule, self).__init__()
         self.mid_d = mid_d
         # fusion
-        self.cls = torch.nn.Conv2d(self.mid_d, 1, kernel_size=1)
+        self.cls = torch.nn.Conv2d(self.mid_d, 2, kernel_size=1)
         self.conv_context = torch.nn.Sequential(
-            torch.nn.Conv2d(2, self.mid_d, kernel_size=1),
+            torch.nn.Conv2d(4, self.mid_d, kernel_size=1),
             torch.nn.BatchNorm2d(self.mid_d),
             torch.nn.ReLU(inplace=True)
         )
