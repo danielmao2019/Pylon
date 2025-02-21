f1e27bbf3189737d97ec5206a3fde44514568d34
Y29lin <y29lin@uwaterloo.ca>
2025-02-21 01:02:15 -0500

    f

--- criteria/vision_2d/change_detection/lwganet_criterion.py
+++ criteria/vision_2d/change_detection/lwganet_criterion.py
@@ -10,14 +10,25 @@ class LWGANetCriterion(SingleTaskCriterion):
     def __init__(self) -> None:
         super(LWGANetCriterion, self).__init__()
         # unbounded
-        self.bce_criterion = torch.nn.functional.binary_cross_entropy_with_logits()
+        self.ce_criterion = torch.nn.CrossEntropyLoss()
         self.dice_criterion = DiceLoss()
 
     def __call__(self, y_pred: Dict[str, torch.Tensor], y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
         """Override parent class __call__ method.
         """
         assert set(y_true.keys()) == {'change_map'}
-        bce_loss += (self.bce_criterion(y_pred=mask, y_true=y_true['change_map']) for mask in y_pred.values()) 
-        dice_loss += (self.dice_criterion(y_pred=mask, y_true=y_true['change_map']) for mask in y_pred.values()) 
+
+        # Compute the binary cross entropy loss for each mask and sum them up
+        ce_loss = sum( self.ce_criterion(mask, y_true['change_map'])
+            for mask in y_pred.values())
+    
+        # Compute the dice loss for each mask and sum them up
+        dice_loss = sum(self.dice_criterion(y_pred=mask, y_true=y_true['change_map'])
+            for mask in y_pred.values())
         
-        return bce_loss + dice_loss
+        total_loss = ce_loss + dice_loss
+        
+        assert total_loss.numel() == 1, f"{total_loss.shape=}"
+        self.buffer.append(total_loss.detach().cpu())
+        
+        return total_loss
