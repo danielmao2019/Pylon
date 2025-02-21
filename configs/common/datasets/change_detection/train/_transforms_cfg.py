cbf5ee766e1c090f8d71dc5831b696e895fc3ca1
Daniel Mao <danielmao2019@gmail.com>
2025-02-21 17:52:57 -0500

    f

--- configs/common/datasets/change_detection/train/_transforms_cfg.py
+++ configs/common/datasets/change_detection/train/_transforms_cfg.py
@@ -3,21 +3,29 @@ import torchvision
 import data
 
 
-def transforms_cfg(size: Tuple[int, int], first: Optional[str] = "RandomCrop") -> dict:
-    assert first in {"RandomCrop", "ResizeMaps"}
+def transforms_cfg(
+    first: Optional[str] = "RandomCrop",
+    size: Optional[Tuple[int, int]] = (224, 224),
+    resize: Optional[Tuple[int, int]] = None,
+) -> dict:
     if first == "RandomCrop":
         first_transform = [(
-            data.transforms.crop.RandomCrop(size=size),
-            [('inputs', 'img_1'), ('inputs', 'img_2'), ('labels', 'change_map')],
-        )]
-    else:
-        first_transform = [(
-            data.transforms.resize.ResizeMaps(size=size, interpolation=torchvision.transforms.InterpolationMode.BILINEAR),
+            data.transforms.crop.RandomCrop(size=size, resize=resize, interpolation='bilinear'),
             [('inputs', 'img_1'), ('inputs', 'img_2')],
         ), (
-            data.transforms.resize.ResizeMaps(size=size, interpolation=torchvision.transforms.InterpolationMode.NEAREST),
+            data.transforms.crop.RandomCrop(size=size, resize=resize, interpolation='nearest'),
             [('labels', 'change_map')],
         )]
+    elif first == "ResizeMaps":
+        first_transform = [(
+            data.transforms.resize.ResizeMaps(size=size, interpolation='bilinear'),
+            [('inputs', 'img_1'), ('inputs', 'img_2')],
+        ), (
+            data.transforms.resize.ResizeMaps(size=size, interpolation='nearest'),
+            [('labels', 'change_map')],
+        )]
+    else:
+        raise NotImplementedError
     return {
         'class': data.transforms.Compose,
         'args': {
