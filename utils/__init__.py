c0325aca2bfd096506991422d78f48c3ffc9260f
Daniel Mao <danielmao2019@gmail.com>
2025-02-22 13:17:27 -0500

    f

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,38 +1,34 @@
 """
 UTILS API
 """
+from utils import automation
 from utils import builders
 from utils import conversions
 from utils import input_checks
 from utils import logging
 from utils import models
 from utils import ops
-from utils import paper
 
-from utils.automation import configs
 from utils import determinism
 from utils import gradients
 from utils import io
-from utils.automation import progress
 
 from utils import object_detection
 from utils import semantic_segmentation
 
 
 __all__ = (
+    'automation',
     'builders',
     'conversions',
     'input_checks',
     'logging',
     'models',
     'ops',
-    'paper',
 
-    'configs',
     'determinism',
     'gradients',
     'io',
-    'progress',
 
     'object_detection',
     'semantic_segmentation',
