1265b56d00306f27f3e7ba6035918b4c44561806
Daniel Mao <danielmao2019@gmail.com>
2025-02-22 13:11:40 -0500

    f

--- utils/automation/__init__.py
+++ utils/automation/__init__.py
@@ -2,4 +2,12 @@
 UTILS.AUTOMATION API
 """
 from utils.automation import configs
+from utils.automation import progress
 from utils.automation import tables
+
+
+__all__ = (
+    'configs',
+    'progress',
+    'tables',
+)
