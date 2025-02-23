12c414bba4d25a63581d262d89381dd4e0210780
Y29lin <y29lin@uwaterloo.ca>
2025-02-22 19:47:48 -0500

    f

---
+++ models/change_detection/bit_cd/model.py
@@ -0,0 +1,419 @@
+import torch
+from einops import rearrange
+from models.backbones.resnet.resnet import resnet18, resnet34, resnet50
+
+class Residual(torch.nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+        
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+
+
+class Residual2(torch.nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+        
+    def forward(self, x, x2, **kwargs):
+        return self.fn(x, x2, **kwargs) + x
+
+
+
+class PreNorm(torch.nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = torch.nn.LayerNorm(dim)
+        self.fn = fn
+        
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+    
+
+
+class PreNorm2(torch.nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = torch.nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, x2, **kwargs):
+        return self.fn(self.norm(x), self.norm(x2), **kwargs)
+
+
+
+class Cross_Attention(torch.nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0., softmax=True):
+        super().__init__()
+        inner_dim = dim_head * heads
+        self.heads = heads
+        self.scale = dim ** -0.5
+
+        self.softmax = softmax
+        self.to_q = torch.nn.Linear(dim, inner_dim, bias=False)
+        self.to_k = torch.nn.Linear(dim, inner_dim, bias=False)
+        self.to_v = torch.nn.Linear(dim, inner_dim, bias=False)
+
+        self.to_out = torch.nn.Sequential(
+            torch.nn.Linear(inner_dim, dim),
+            torch.nn.Dropout(dropout)
+        )
+
+    def forward(self, x, m, mask = None):
+
+        b, n, _, h = *x.shape, self.heads
+        q = self.to_q(x)
+        k = self.to_k(m)
+        v = self.to_v(m)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), [q,k,v])
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+        mask_value = -torch.finfo(dots.dtype).max
+
+        if mask is not None:
+            mask = torch.nn.functional.pad(mask.flatten(1), (1, 0), value = True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, mask_value)
+            del mask
+
+        if self.softmax:
+            attn = dots.softmax(dim=-1)
+        else:
+            attn = dots
+        # attn = dots
+        # vis_tmp(dots)
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        # vis_tmp2(out)
+
+        return out
+
+
+
+class Attention(torch.nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        self.heads = heads
+        self.scale = dim ** -0.5
+
+        self.to_qkv = torch.nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = torch.nn.Sequential(
+            torch.nn.Linear(inner_dim, dim),
+            torch.nn.Dropout(dropout)
+        )
+
+    def forward(self, x, mask = None):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+        mask_value = -torch.finfo(dots.dtype).max
+
+        if mask is not None:
+            mask = torch.nn.functional.pad(mask.flatten(1), (1, 0), value = True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, mask_value)
+            del mask
+
+        attn = dots.softmax(dim=-1)
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        return out
+
+
+
+class FeedForward(torch.nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(dim, hidden_dim),
+            torch.nn.GELU(),
+            torch.nn.Dropout(dropout),
+            torch.nn.Linear(hidden_dim, dim),
+            torch.nn.Dropout(dropout)
+        )
+        
+    def forward(self, x):
+        return self.net(x)
+
+
+
+class Transformer(torch.nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout):
+        super().__init__()
+        self.layers = torch.nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(torch.nn.ModuleList([
+                Residual(PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout))),
+                Residual(PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout)))
+            ]))
+            
+    def forward(self, x, mask = None):
+        for attn, ff in self.layers:
+            x = attn(x, mask = mask)
+            x = ff(x)
+        return x
+
+
+
+class TransformerDecoder(torch.nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout, softmax=True):
+        super().__init__()
+        self.layers = torch.nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(torch.nn.ModuleList([
+                Residual2(PreNorm2(dim, Cross_Attention(dim, heads = heads,
+                                                        dim_head = dim_head, dropout = dropout,
+                                                        softmax=softmax))),
+                Residual(PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout)))
+            ]))
+            
+    def forward(self, x, m, mask = None):
+        """target(query), memory"""
+        for attn, ff in self.layers:
+            x = attn(x, m, mask = mask)
+            x = ff(x)
+        return x
+
+
+
+class TwoLayerConv2d(torch.nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        super().__init__(torch.nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size,
+                            padding=kernel_size // 2, stride=1, bias=False),
+                         torch.nn.BatchNorm2d(in_channels),
+                         torch.nn.ReLU(),
+                         torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                            padding=kernel_size // 2, stride=1)
+                         )
+
+
+
+class ResNet(torch.nn.Module):
+    def __init__(self, input_nc, output_nc,
+                 resnet_stages_num=5, backbone='resnet18',
+                 output_sigmoid=False, if_upsample_2x=True):
+        """
+        In the constructor we instantiate two nn.Linear modules and assign them as
+        member variables.
+        """
+        super(ResNet, self).__init__()
+        expand = 1
+        if backbone == 'resnet18':
+            self.resnet = resnet18(pretrained=True,
+                                        replace_stride_with_dilation=[False,True,True])
+        elif backbone == 'resnet34':
+            self.resnet = resnet34(pretrained=True,
+                                        replace_stride_with_dilation=[False,True,True])
+        elif backbone == 'resnet50':
+            self.resnet = resnet50(pretrained=True,
+                                        replace_stride_with_dilation=[False,True,True])
+            expand = 4
+        else:
+            raise NotImplementedError
+        self.relu = torch.nn.ReLU()
+        self.upsamplex2 = torch.nn.Upsample(scale_factor=2)
+        self.upsamplex4 = torch.nn.Upsample(scale_factor=4, mode='bilinear')
+
+        self.classifier = TwoLayerConv2d(in_channels=32, out_channels=output_nc)
+
+        self.resnet_stages_num = resnet_stages_num
+
+        self.if_upsample_2x = if_upsample_2x
+        if self.resnet_stages_num == 5:
+            layers = 512 * expand
+        elif self.resnet_stages_num == 4:
+            layers = 256 * expand
+        elif self.resnet_stages_num == 3:
+            layers = 128 * expand
+        else:
+            raise NotImplementedError
+        self.conv_pred = torch.nn.Conv2d(layers, 32, kernel_size=3, padding=1)
+
+        self.output_sigmoid = output_sigmoid
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x1, x2):
+        x1 = self.forward_single(x1)
+        x2 = self.forward_single(x2)
+        x = torch.abs(x1 - x2)
+        if not self.if_upsample_2x:
+            x = self.upsamplex2(x)
+        x = self.upsamplex4(x)
+        x = self.classifier(x)
+
+        if self.output_sigmoid:
+            x = self.sigmoid(x)
+        return x
+
+    def forward_single(self, x):
+        # resnet layers
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+        x = self.resnet.maxpool(x)
+
+        x_4 = self.resnet.layer1(x) # 1/4, in=64, out=64
+        x_8 = self.resnet.layer2(x_4) # 1/8, in=64, out=128
+
+        if self.resnet_stages_num > 3:
+            x_8 = self.resnet.layer3(x_8) # 1/8, in=128, out=256
+
+        if self.resnet_stages_num == 5:
+            x_8 = self.resnet.layer4(x_8) # 1/32, in=256, out=512
+        elif self.resnet_stages_num > 5:
+            raise NotImplementedError
+
+        if self.if_upsample_2x:
+            x = self.upsamplex2(x_8)
+        else:
+            x = x_8
+        # output layers
+        x = self.conv_pred(x)
+        return x
+
+
+
+class BASE_Transformer(ResNet):
+    """
+    Resnet of 8 downsampling + BIT + bitemporal feature Differencing + a small CNN
+    """
+    def __init__(self, input_nc, output_nc, with_pos, resnet_stages_num=5,
+                 token_len=4, token_trans=True,
+                 enc_depth=1, dec_depth=1,
+                 dim_head=64, decoder_dim_head=64,
+                 tokenizer=True, if_upsample_2x=True,
+                 pool_mode='max', pool_size=2,
+                 backbone='resnet18',
+                 decoder_softmax=True, with_decoder_pos=None,
+                 with_decoder=True):
+        super(BASE_Transformer, self).__init__(input_nc, output_nc,backbone=backbone,
+                                             resnet_stages_num=resnet_stages_num,
+                                               if_upsample_2x=if_upsample_2x,
+                                               )
+        self.token_len = token_len
+        self.conv_a = torch.nn.Conv2d(32, self.token_len, kernel_size=1,
+                                padding=0, bias=False)
+        self.tokenizer = tokenizer
+        if not self.tokenizer:
+            #  if not use tokenzier，then downsample the feature map into a certain size
+            self.pooling_size = pool_size
+            self.pool_mode = pool_mode
+            self.token_len = self.pooling_size * self.pooling_size
+
+        self.token_trans = token_trans
+        self.with_decoder = with_decoder
+        dim = 32
+        mlp_dim = 2*dim
+
+        self.with_pos = with_pos
+        if with_pos is 'learned':
+            self.pos_embedding = torch.nn.Parameter(torch.randn(1, self.token_len*2, 32))
+        decoder_pos_size = 256//4
+        self.with_decoder_pos = with_decoder_pos
+        if self.with_decoder_pos == 'learned':
+            self.pos_embedding_decoder = torch.nn.Parameter(torch.randn(1, 32,
+                                                                 decoder_pos_size,
+                                                                 decoder_pos_size))
+        self.enc_depth = enc_depth
+        self.dec_depth = dec_depth
+        self.dim_head = dim_head
+        self.decoder_dim_head = decoder_dim_head
+        self.transformer = Transformer(dim=dim, depth=self.enc_depth, heads=8,
+                                       dim_head=self.dim_head,
+                                       mlp_dim=mlp_dim, dropout=0)
+        self.transformer_decoder = TransformerDecoder(dim=dim, depth=self.dec_depth,
+                            heads=8, dim_head=self.decoder_dim_head, mlp_dim=mlp_dim, dropout=0,
+                                                      softmax=decoder_softmax)
+
+    def _forward_semantic_tokens(self, x):
+        b, c, h, w = x.shape
+        spatial_attention = self.conv_a(x)
+        spatial_attention = spatial_attention.view([b, self.token_len, -1]).contiguous()
+        spatial_attention = torch.softmax(spatial_attention, dim=-1)
+        x = x.view([b, c, -1]).contiguous()
+        tokens = torch.einsum('bln,bcn->blc', spatial_attention, x)
+
+        return tokens
+
+    def _forward_reshape_tokens(self, x):
+        # b,c,h,w = x.shape
+        if self.pool_mode is 'max':
+            x = torch.nn.functional.adaptive_max_pool2d(x, [self.pooling_size, self.pooling_size])
+        elif self.pool_mode is 'ave':
+            x = torch.nn.functional.adaptive_avg_pool2d(x, [self.pooling_size, self.pooling_size])
+        else:
+            x = x
+        tokens = rearrange(x, 'b c h w -> b (h w) c')
+        return tokens
+
+    def _forward_transformer(self, x):
+        if self.with_pos:
+            x += self.pos_embedding
+        x = self.transformer(x)
+        return x
+
+    def _forward_transformer_decoder(self, x, m):
+        b, c, h, w = x.shape
+        if self.with_decoder_pos == 'fix':
+            x = x + self.pos_embedding_decoder
+        elif self.with_decoder_pos == 'learned':
+            x = x + self.pos_embedding_decoder
+        x = rearrange(x, 'b c h w -> b (h w) c')
+        x = self.transformer_decoder(x, m)
+        x = rearrange(x, 'b (h w) c -> b c h w', h=h)
+        return x
+
+    def _forward_simple_decoder(self, x, m):
+        b, c, h, w = x.shape
+        b, l, c = m.shape
+        m = m.expand([h,w,b,l,c])
+        m = rearrange(m, 'h w b l c -> l b c h w')
+        m = m.sum(0)
+        x = x + m
+        return x
+
+    def forward(self, x1, x2):
+        # forward backbone resnet
+        x1 = self.forward_single(x1)
+        x2 = self.forward_single(x2)
+
+        #  forward tokenzier
+        if self.tokenizer:
+            token1 = self._forward_semantic_tokens(x1)
+            token2 = self._forward_semantic_tokens(x2)
+        else:
+            token1 = self._forward_reshape_tokens(x1)
+            token2 = self._forward_reshape_tokens(x2)
+        # forward transformer encoder
+        if self.token_trans:
+            self.tokens_ = torch.cat([token1, token2], dim=1)
+            self.tokens = self._forward_transformer(self.tokens_)
+            token1, token2 = self.tokens.chunk(2, dim=1)
+        # forward transformer decoder
+        if self.with_decoder:
+            x1 = self._forward_transformer_decoder(x1, token1)
+            x2 = self._forward_transformer_decoder(x2, token2)
+        else:
+            x1 = self._forward_simple_decoder(x1, token1)
+            x2 = self._forward_simple_decoder(x2, token2)
+        # feature differencing
+        x = torch.abs(x1 - x2)
+        if not self.if_upsample_2x:
+            x = self.upsamplex2(x)
+        x = self.upsamplex4(x)
+        # forward small cnn
+        x = self.classifier(x)
+        if self.output_sigmoid:
+            x = self.sigmoid(x)
+        return x
