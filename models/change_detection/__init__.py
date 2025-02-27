b1e1ada22d9b64c42096a0eacb7fb8637ffc2554
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-26 22:15:21 -0500

    [Models][ChangeDetection] Implement Changer model and configs (#133)

--- models/change_detection/__init__.py
+++ models/change_detection/__init__.py
@@ -6,6 +6,7 @@ from models.change_detection.fc_siam.fully_convolutional_siamese_networks import
 from models.change_detection.snunet.snunet import SNUNet_ECAM
 from models.change_detection.dsifn.dsifn_model import DSIFN
 from models.change_detection.tiny_cd.tiny_cd_model import TinyCD
+from models.change_detection import changer
 from models.change_detection.change_former.models.change_former_v1 import ChangeFormerV1
 from models.change_detection.change_former.models.change_former_v2 import ChangeFormerV2
 from models.change_detection.change_former.models.change_former_v3 import ChangeFormerV3
@@ -30,6 +31,7 @@ __all__ = (
     'SNUNet_ECAM',
     'DSIFN',
     'TinyCD',
+    'changer',
     'ChangeFormerV1',
     'ChangeFormerV2',
     'ChangeFormerV3',
