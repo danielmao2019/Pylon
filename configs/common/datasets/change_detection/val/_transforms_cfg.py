94130875d30a443f18fda0fdcde289cda52b461d
Daniel Mao <danielmao2019@gmail.com>
2025-02-28 14:03:26 -0500

    f

--- configs/common/datasets/change_detection/val/_transforms_cfg.py
+++ configs/common/datasets/change_detection/val/_transforms_cfg.py
@@ -8,26 +8,20 @@ def transforms_cfg(
     resize: Optional[Tuple[int, int]] = None,
 ) -> dict:
     if first == "RandomCrop":
-        first_transform = [(
-            data.transforms.crop.RandomCrop(size=size, resize=resize, interpolation='bilinear'),
-            [('inputs', 'img_1'), ('inputs', 'img_2')],
-        ), (
-            data.transforms.crop.RandomCrop(size=size, resize=resize, interpolation='nearest'),
-            [('labels', 'change_map')],
-        )]
+        first_transform = (
+            data.transforms.crop.RandomCrop(size=size, resize=resize, interpolation=None),
+            [('inputs', 'img_1'), ('inputs', 'img_2'), ('labels', 'change_map')],
+        )
     elif first == "ResizeMaps":
-        first_transform = [(
-            data.transforms.resize.ResizeMaps(size=size, interpolation='bilinear'),
-            [('inputs', 'img_1'), ('inputs', 'img_2')],
-        ), (
-            data.transforms.resize.ResizeMaps(size=size, interpolation='nearest'),
-            [('labels', 'change_map')],
-        )]
+        first_transform = (
+            data.transforms.resize.ResizeMaps(size=size, interpolation=None),
+            [('inputs', 'img_1'), ('inputs', 'img_2'), ('labels', 'change_map')],
+        )
     else:
         raise NotImplementedError
     return {
         'class': data.transforms.Compose,
         'args': {
-            'transforms': first_transform,
+            'transforms': [first_transform],
         },
     }
