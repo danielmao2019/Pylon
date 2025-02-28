e9fa75c932d9e700aae507fe0b58650d8bfe1772
Daniel Mao <danielmao2019@gmail.com>
2025-02-27 19:40:19 -0500

    f

--- models/change_detection/change_next/modules/change_next.py
+++ models/change_detection/change_next/modules/change_next.py
@@ -3,6 +3,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from timm.models.layers import to_2tuple
+from models.change_detection.change_former.modules.dwconv import DWConv
+
+
+class Mlp(nn.Module):
+    def __init__(self,
+                 in_features,
+                 hidden_features=None,
+                 out_features=None,
+                 act_layer=nn.GELU,
+                 drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)
+        self.dwconv = DWConv(hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+
+        x = self.dwconv(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+
+        return x
 
 
 def resize(input,
