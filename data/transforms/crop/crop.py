438e4c6576bc629d0b18b55ead93ce0498f29385
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-02-28 14:23:20 -0500

    [Data][Transforms] Implement dynamic selection of interpolation mode and debug data config (#136)

--- data/transforms/crop/crop.py
+++ data/transforms/crop/crop.py
@@ -1,6 +1,5 @@
 from typing import Tuple, Optional
 import torch
-import torchvision
 import data
 from data.transforms import BaseTransform
 
@@ -12,7 +11,7 @@ class Crop(BaseTransform):
         loc: Tuple[int, int],
         size: Tuple[int, int],
         resize: Optional[Tuple[int, int]] = None,
-        interpolation: Optional[str] = 'bilinear',
+        interpolation: Optional[str] = None,
     ) -> None:
         """
         Initializes the Crop transform with a location and resolution.
@@ -21,7 +20,7 @@ class Crop(BaseTransform):
             loc (Tuple[int, int]): The (x, y) starting location of the crop.
             size (Tuple[int, int]): The (width, height) resolution of the crop.
             resize (Optional[Tuple[int, int]]): Target size to resize the cropped tensor. Default is None.
-            interpolation (Optional[str]): Interpolation mode for resizing. Default is 'bilinear'.
+            interpolation (Optional[str]): Interpolation mode for resizing. Default is None.
 
         Raises:
             ValueError: If loc or size are not valid tuples of two positive integers.
@@ -33,9 +32,10 @@ class Crop(BaseTransform):
 
         self.loc = loc
         self.size = size
-
-        if resize is not None:
-            self.resize_op = data.transforms.resize.ResizeMaps(size=resize, interpolation=interpolation)
+        if resize:
+            self.resize_op = data.transforms.resize.ResizeMaps(
+                size=resize, interpolation=interpolation,
+            )
         else:
             self.resize_op = None
 
@@ -67,7 +67,7 @@ class Crop(BaseTransform):
         tensor = tensor[..., y_start:y_end, x_start:x_end]
 
         # Perform resizing if applicable
-        if self.resize_op is not None:
+        if self.resize_op:
             tensor = self.resize_op(tensor)
 
         return tensor
