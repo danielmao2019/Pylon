943ee5d7a5a49436b9b01e5aa2d825b0b217e043
Daniel Mao <danielmao2019@gmail.com>
2025-02-27 19:40:56 -0500

    f

--- models/change_detection/change_next/modules/change_next.py
+++ models/change_detection/change_next/modules/change_next.py
@@ -2,7 +2,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from timm.models.layers import to_2tuple
+from timm.models.layers import to_2tuple, DropPath
 from models.change_detection.change_former.modules.dwconv import DWConv
 
 
