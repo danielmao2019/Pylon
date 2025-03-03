8375670456329cd91399aabae96633efbd5460ba
Daniel Mao <danielmao2019@gmail.com>
2025-03-03 17:36:25 -0500

    f

--- configs/reproduce/change_detection/templates/fc_siam_oscd.py
+++ configs/reproduce/change_detection/templates/fc_siam_oscd.py
@@ -52,14 +52,6 @@ config = {
                             data.transforms.Randomize(transform=data.transforms.Flip(axis=-2), p=0.5),
                             [('inputs', 'img_1'), ('inputs', 'img_2'), ('labels', 'change_map')],
                         ),
-                        (
-                            data.transforms.Randomize(torchvision.transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5), p=0.5),
-                            ('inputs', 'img_1'),
-                        ),
-                        (
-                            data.transforms.Randomize(torchvision.transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5), p=0.5),
-                            ('inputs', 'img_2'),
-                        ),
                     ],
                 },
             },
