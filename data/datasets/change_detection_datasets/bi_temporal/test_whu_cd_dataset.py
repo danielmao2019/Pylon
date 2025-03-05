df5d8c92b5ceef85844c21a5dfa6eeca547d2965
Y29lin <y29lin@uwaterloo.ca>
2025-03-05 16:45:26 -0500

    f

--- data/datasets/change_detection_datasets/bi_temporal/test_whu_cd_dataset.py
+++ data/datasets/change_detection_datasets/bi_temporal/test_whu_cd_dataset.py
@@ -20,15 +20,15 @@ def test_whu_cd(dataset: torch.utils.data.Dataset) -> None:
         assert set(inputs.keys()) == set(WHU_CD_Dataset.INPUT_NAMES)
         img_1 = inputs['img_1']
         img_2 = inputs['img_2']
-        assert type(img_1) == torch.Tensor and img_1.ndim == 2 and img_1.dtype == torch.float32
-        assert type(img_2) == torch.Tensor and img_2.ndim == 2 and img_2.dtype == torch.float32
+        assert type(img_1) == torch.Tensor and img_1.ndim == 3 and img_1.dtype == torch.float32
+        assert type(img_2) == torch.Tensor and img_2.ndim == 3 and img_2.dtype == torch.float32
         assert img_1.shape == img_2.shape, f"{img_1.shape=}, {img_2.shape=}"
         # inspect labels
         labels = datapoint['labels']
         assert type(labels) == dict
         assert set(labels.keys()) == set(WHU_CD_Dataset.LABEL_NAMES)
         change_map = labels['change_map']
-        assert type(change_map) == torch.Tensor and change_map.ndim == 2 and change_map.dtype == torch.int64
+        assert type(change_map) == torch.Tensor and change_map.ndim == 3 and change_map.dtype == torch.int64
         assert set(torch.unique(change_map).tolist()).issubset(set([0, 1])), f"{torch.unique(change_map)=}"
         for cls in range(dataset.NUM_CLASSES):
             class_dist[cls] += torch.sum(change_map == cls)
