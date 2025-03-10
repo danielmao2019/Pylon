95c4da96c1be7742eec4a352256209b1c0177eaa
billylin609 <linyuchen830@gmail.com>
2025-03-10 15:48:27 -0400

    f

--- configs/common/models/change_detection/lwganet.py
+++ configs/common/models/change_detection/lwganet.py
@@ -1,10 +1,17 @@
 import models
-import models.change_detection.lwganet
+import torch
 
 
 model_config = {
     'class': models.change_detection.BaseNet_LWGANet_L2,
     'args': {
         'preptrained_path': '/pub7/yuchen/Pylon/models/change_detection/lwganet/lwganet_l2_e296.pth',
+    },
+    'class': torch.distributed.init_process_group,
+    'args': {
+        'backend' = 'nccl',
+        'init_method' = 'tcp://127.0.0.1:23456',
+        'rank' = '0',
+        'world_size' = '1',
     },
 }
