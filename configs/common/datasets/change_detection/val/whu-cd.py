2efa2285163c9fd3e5206ce1492a42affb97e3d9
billylin609 <linyuchen830@gmail.com>
2025-03-10 15:22:02 -0400

    fg

---
+++ configs/common/datasets/change_detection/val/whu-cd.py
@@ -0,0 +1,57 @@
+import torch
+import data
+import metrics
+from ._transforms_cfg import transforms_cfg
+
+
+collate_fn_cfg = {
+    'class': data.collators.BaseCollator,
+    'args': {
+        'collators': {
+            'meta_info': {
+                'image_resolution': torch.Tensor,
+            },
+        },
+    },
+}
+
+config = {
+    'val_dataset': {
+        'class': data.datasets.WHU_CD_Dataset,
+        'args': {
+            'data_root': "./data/datasets/soft_links/WHU-CD",
+            'split': "val",
+            'transforms_cfg': transforms_cfg(size=(224, 224)),
+        },
+    },
+    'val_dataloader': {
+        'class': torch.utils.data.DataLoader,
+        'args': {
+            'batch_size': 1,
+            'num_workers': 4,
+            'collate_fn': collate_fn_cfg,
+        },
+    },
+    'test_dataset': {
+        'class': data.datasets.WHU_CD_Dataset,
+        'args': {
+            'data_root': "./data/datasets/soft_links/WHU-CD",
+            'split': "test",
+            'transforms_cfg': transforms_cfg,
+        },
+    },
+    'test_dataloader': {
+        'class': torch.utils.data.DataLoader,
+        'args': {
+            'batch_size': 1,
+            'num_workers': 4,
+            'collate_fn': collate_fn_cfg,
+        },
+    },
+    'metric': {
+        'class': metrics.vision_2d.SemanticSegmentationMetric,
+        'args': {
+            'num_classes': 2,
+        },
+    },
+}
