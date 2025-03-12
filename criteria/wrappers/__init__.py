baaf288b3bcf9d9c5be8b770eb96cc8d558684d3
Daniel Mao <danielmao2019@gmail.com>
2025-03-11 22:56:11 -0400

    f

--- criteria/wrappers/__init__.py
+++ criteria/wrappers/__init__.py
@@ -3,7 +3,6 @@ CRITERIA.WRAPPERS API
 """
 from criteria.wrappers.single_task_criterion import SingleTaskCriterion
 from criteria.wrappers.pytorch_criterion_wrapper import PyTorchCriterionWrapper
-from criteria.wrappers.spatial_pytorch_criterion_wrapper import SpatialPyTorchCriterionWrapper
 from criteria.wrappers.auxiliary_outputs_criterion import AuxiliaryOutputsCriterion
 from criteria.wrappers.multi_task_criterion import MultiTaskCriterion
 
@@ -11,7 +10,6 @@ from criteria.wrappers.multi_task_criterion import MultiTaskCriterion
 __all__ = (
     'SingleTaskCriterion',
     'PyTorchCriterionWrapper',
-    'SpatialPyTorchCriterionWrapper',
     'AuxiliaryOutputsCriterion',
     'MultiTaskCriterion',
 )
