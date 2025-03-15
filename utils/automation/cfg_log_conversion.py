9ecad7c47a7accfaec10dd4a700ac85de6b0d5cb
Daniel Mao <danielmao2019@gmail.com>
2025-03-15 16:24:00 -0400

    f

--- utils/automation/cfg_log_conversion.py
+++ utils/automation/cfg_log_conversion.py
@@ -1,9 +1,16 @@
 import os
+from pathlib import Path
 
 
 def get_work_dir(config_file: str) -> str:
-    return os.path.join("./logs", os.path.splitext(os.path.relpath(config_file, start="./configs"))[0])
+    config_path = Path(config_file)
+    configs_dir = Path('./configs')
+    rel_path = Path(os.path.splitext(config_path.relative_to(configs_dir))[0])
+    return str(Path('./logs') / rel_path)
 
 
 def get_config(work_dir: str) -> str:
-    return os.path.join("./configs", os.path.relpath(work_dir, "./logs")) + ".py"
+    work_path = Path(work_dir)
+    logs_dir = Path('./logs')
+    rel_path = work_path.relative_to(logs_dir)
+    return str(Path('./configs') / rel_path) + '.py'
