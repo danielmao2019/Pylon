e059b86e51d14234105dcf44b9a1d3208950651b
Daniel Mao <danielmao2019@gmail.com>
2025-03-15 16:29:11 -0400

    f

--- runners/viewer/viewer.py
+++ runners/viewer/viewer.py
@@ -4,7 +4,7 @@ import dash
 from runners.viewer.states.trainer import TrainingState
 from runners.viewer.layout.main import create_main_layout
 from runners.viewer.callbacks.navigation import register_navigation_callbacks
-from utils.automation.cfg_log_conversion import get_work_dir
+from utils.automation.cfg_log_conversion import get_work_dir, get_repo_root
 
 
 class TrainerViewer:
@@ -33,11 +33,12 @@ class TrainerViewer:
             Input:  ./configs/reproduce/change_detection/xxx/config.py
             Output: ./logs/viewer/change_detection/xxx
         """
-        # Get the corresponding work directory
+        # Get the corresponding work directory using repo root
         work_dir = Path(get_work_dir(str(config_path)))
-        # Replace the second directory with 'viewer'
-        parts = work_dir.parts
-        return Path(*parts[:2]) / 'viewer' / Path(*parts[2:])
+        # Get relative path from repo root
+        rel_path = work_dir.relative_to(get_repo_root() / 'logs')
+        # Construct new path with 'viewer' as second directory
+        return get_repo_root() / 'logs' / 'viewer' / rel_path
     
     def run(self, host: str = "localhost", port: int = 8050, debug: bool = True):
         """Start the Dash server."""
