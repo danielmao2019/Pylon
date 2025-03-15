d731e2d7e115ab903d51d862b2d3716e49eabf95
Daniel Mao <danielmao2019@gmail.com>
2025-03-15 16:07:08 -0400

    f

--- runners/viewer/cli.py
+++ runners/viewer/cli.py
@@ -7,7 +7,7 @@ repo_root = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__fi
 if repo_root not in sys.path:
     sys.path.insert(0, repo_root)
 
-from .viewer import TrainerViewer
+from runners.viewer.viewer import TrainerViewer
 
 
 def main():
