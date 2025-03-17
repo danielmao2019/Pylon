6d77df88e0bd3ef8b63f9eb19e20f29bbbe8661c
Daniel Mao <danielmao2019@gmail.com>
2025-03-17 15:27:36 -0400

    f

--- configs/common/models/change_detection/hcgmnet.py
+++ configs/common/models/change_detection/hcgmnet.py
@@ -1,8 +1,10 @@
 from typing import Dict, Any
+import models
+
 
 # Default configuration for HCGMNet
 hcgmnet_config: Dict[str, Any] = {
-    'class': 'models.change_detection.hcgmnet.HCGMNet',
+    'class': models.change_detection.HCGMNet,
     'args': {
         'num_classes': 2,
     }
