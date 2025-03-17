9e58c7e3b47cc40b24b1d83faf921cb9005ca006
Daniel Mao <danielmao2019@gmail.com>
2025-03-17 16:05:12 -0400

    f

--- models/change_detection/__init__.py
+++ models/change_detection/__init__.py
@@ -21,7 +21,7 @@ from models.change_detection.ftn.model import FTN
 from models.change_detection.srcnet.srcnet_model import SRCNet
 from models.change_detection.bifa.bifa_model import BiFA
 from models.change_detection.cdx_former.cdx_former_model import CDXFormer
-from models.change_detection.cdmaskformer import CDMaskFormer
+from models.change_detection.cdmaskformer.models.cdmaskformer import CDMaskFormer
 from models.change_detection import csa_cdgan
 from models.change_detection.change_mamba.st_mamba_bcd import STMambaBCD
 from models.change_detection.siamese_kpconv.siamese_kpconv_model import SiameseKPConv
