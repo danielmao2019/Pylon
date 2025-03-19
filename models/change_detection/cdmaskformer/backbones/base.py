630406c0ce1d35a552d7b1a4cb1dfde628a2089f
Daniel Mao <danielmao2019@gmail.com>
2025-03-19 17:49:33 -0400

    f

--- models/change_detection/cdmaskformer/backbones/base.py
+++ models/change_detection/cdmaskformer/backbones/base.py
@@ -1,18 +1,13 @@
-import torch 
 import torch.nn as nn
-from rscd.models.backbones.seaformer import *
-from rscd.models.backbones.resnet import get_resnet18, get_resnet50_OS32, get_resnet50_OS8
-from rscd.models.backbones.swintransformer import *
+from models.change_detection.cdmaskformer.backbones.seaformer import SeaFormer_L
 
 class Base(nn.Module):
     def __init__(self, name):
         super().__init__()
         if name == 'Seaformer':
             self.backbone = SeaFormer_L(pretrained=True)
-        elif name == 'Resnet18':
-            self.backbone = get_resnet18(pretrained=True)
-        elif name == 'Swin':
-            self.backbone = swin_tiny(True)
+        else:
+            raise ValueError(f"Unsupported backbone: {name}")
 
     def forward(self, xA, xB):
         featuresA = self.backbone(xA)
