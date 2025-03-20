61c58a5129d30f49a289b55cd013f055ef1a51c2
Daniel Mao <danielmao2019@gmail.com>
2025-03-20 17:44:58 -0400

    f

--- models/change_detection/siamese_kpconv_variants/oneconvfusionkpconv.py
+++ models/change_detection/siamese_kpconv_variants/oneconvfusionkpconv.py
@@ -13,49 +13,32 @@ class OneConvFusionKPConv(UnwrappedUnetBasedModel):
     def __init__(self, option, model_type, dataset, modules):
         # Extract parameters from the dataset
         self._num_classes = dataset.num_classes
-        self._use_category = getattr(option, "use_category", False)
-        if self._use_category:
-            if not dataset.class_to_segments:
-                raise ValueError(
-                    "The dataset needs to specify a class_to_segments property when using category information for segmentation"
-                )
-            self._class_to_seg = dataset.class_to_segments
-            self._num_categories = len(self._class_to_seg)
-        else:
-            self._num_categories = 0
+        self._num_categories = 0
 
         # Assemble encoder / decoder
         UnwrappedUnetBasedModel.__init__(self, option, model_type, dataset, modules)
 
         # Build final MLP
         self.last_mlp_opt = option.mlp_cls
-        if self._use_category:
-            self.FC_layer = MultiHeadClassifier(
-                self.last_mlp_opt.nn[0],
-                self._class_to_seg,
-                dropout_proba=self.last_mlp_opt.dropout,
-                bn_momentum=self.last_mlp_opt.bn_momentum,
+        in_feat = self.last_mlp_opt.nn[0] + self._num_categories
+        self.FC_layer = Sequential()
+        for i in range(1, len(self.last_mlp_opt.nn)):
+            self.FC_layer.add_module(
+                str(i),
+                Sequential(
+                    *[
+                        Linear(in_feat, self.last_mlp_opt.nn[i], bias=False),
+                        FastBatchNorm1d(self.last_mlp_opt.nn[i], momentum=self.last_mlp_opt.bn_momentum),
+                        torch.nn.LeakyReLU(0.2),
+                    ]
+                ),
             )
-        else:
-            in_feat = self.last_mlp_opt.nn[0] + self._num_categories
-            self.FC_layer = Sequential()
-            for i in range(1, len(self.last_mlp_opt.nn)):
-                self.FC_layer.add_module(
-                    str(i),
-                    Sequential(
-                        *[
-                            Linear(in_feat, self.last_mlp_opt.nn[i], bias=False),
-                            FastBatchNorm1d(self.last_mlp_opt.nn[i], momentum=self.last_mlp_opt.bn_momentum),
-                            torch.nn.LeakyReLU(0.2),
-                        ]
-                    ),
-                )
-                in_feat = self.last_mlp_opt.nn[i]
+            in_feat = self.last_mlp_opt.nn[i]
 
-            if self.last_mlp_opt.dropout:
-                self.FC_layer.add_module("Dropout", Dropout(p=self.last_mlp_opt.dropout))
+        if self.last_mlp_opt.dropout:
+            self.FC_layer.add_module("Dropout", Dropout(p=self.last_mlp_opt.dropout))
 
-            self.FC_layer.add_module("Class", Lin(in_feat, self._num_classes, bias=False))
+        self.FC_layer.add_module("Class", Lin(in_feat, self._num_classes, bias=False))
 
         self.last_feature = None
         print('total : ' + str(sum(p.numel() for p in self.parameters() if p.requires_grad)))
