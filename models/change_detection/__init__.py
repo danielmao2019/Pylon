9094b51b92f4c71ff93c34900b0150a54bbfbd10
Daniel Mao <danielmao2019@gmail.com>
2025-03-24 18:26:25 -0400

    f

--- models/change_detection/__init__.py
+++ models/change_detection/__init__.py
@@ -7,6 +7,7 @@ from models.change_detection.snunet.snunet import SNUNet_ECAM
 from models.change_detection.dsifn.dsifn_model import DSIFN
 from models.change_detection.tiny_cd.tiny_cd_model import TinyCD
 from models.change_detection.hcgmnet.hcgmnet import HCGMNet
+from models.change_detection.hanet.HANet import HANet
 from models.change_detection import changer
 from models.change_detection.change_former.models.change_former_v1 import ChangeFormerV1
 from models.change_detection.change_former.models.change_former_v2 import ChangeFormerV2
@@ -40,6 +41,7 @@ __all__ = (
     'DSIFN',
     'TinyCD',
     'HCGMNet',
+    'HANet',
     'changer',
     'ChangeFormerV1',
     'ChangeFormerV2',
