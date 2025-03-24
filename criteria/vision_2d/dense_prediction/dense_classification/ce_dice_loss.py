4804c212b5263c04d251335dc133491fe4cee7c2
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-03-24 17:39:35 -0400

    [Criteria][Wrapper] Implement HybridCriterion and FocalDiceLoss (#170)

--- criteria/vision_2d/dense_prediction/dense_classification/ce_dice_loss.py
+++ criteria/vision_2d/dense_prediction/dense_classification/ce_dice_loss.py
@@ -1,10 +1,9 @@
-import torch
-from criteria.wrappers import SingleTaskCriterion
+from criteria.wrappers.hybrid_criterion import HybridCriterion
 from criteria.vision_2d.dense_prediction.dense_classification.semantic_segmentation import SemanticSegmentationCriterion
 from criteria.vision_2d.dense_prediction.dense_classification.dice_loss import DiceLoss
 
 
-class CEDiceLoss(SingleTaskCriterion):
+class CEDiceLoss(HybridCriterion):
     """
     Combined Cross-Entropy and Dice Loss for semantic segmentation tasks.
     
@@ -24,19 +23,23 @@ class CEDiceLoss(SingleTaskCriterion):
         class_weights (Optional[torch.Tensor]): Optional weights for each class
     """
 
-    COMBINE_OPTIONS = {'mean', 'sum'}
-
     def __init__(self, combine='sum', class_weights=None, ignore_value=255) -> None:
-        super(CEDiceLoss, self).__init__()
-        assert combine in self.COMBINE_OPTIONS
-        self.ce_loss = SemanticSegmentationCriterion(reduction='mean', class_weights=class_weights, ignore_value=ignore_value)
-        self.dice_loss = DiceLoss(reduction='mean', class_weights=class_weights, ignore_value=ignore_value)
-        self.combine = combine
-
-    def _compute_loss(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
-        ce_loss = self.ce_loss(y_pred=y_pred, y_true=y_true)
-        dice_loss = self.dice_loss(y_pred=y_pred, y_true=y_true)
-        total_loss = ce_loss + dice_loss
-        if self.combine == 'mean':
-            total_loss = total_loss / 2
-        return total_loss
+        criteria_cfg = [
+            {
+                'class': SemanticSegmentationCriterion,
+                'args': {
+                    'reduction': 'mean',
+                    'class_weights': class_weights,
+                    'ignore_value': ignore_value
+                }
+            },
+            {
+                'class': DiceLoss,
+                'args': {
+                    'reduction': 'mean',
+                    'class_weights': class_weights,
+                    'ignore_value': ignore_value
+                },
+            },
+        ]
+        super(CEDiceLoss, self).__init__(combine=combine, criteria_cfg=criteria_cfg)
