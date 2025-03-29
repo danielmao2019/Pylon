567dac2e21905cca5ce46d34fce3a6be97c56134
Daniel Mao <danielmao2019@gmail.com>
2025-03-29 16:28:50 -0400

    f

--- criteria/vision_2d/change_detection/__init__.py
+++ criteria/vision_2d/change_detection/__init__.py
@@ -5,6 +5,7 @@ from criteria.vision_2d.change_detection.symmetric_change_detection_criterion im
 from criteria.vision_2d.change_detection.snunet_criterion import SNUNetCDCriterion
 from criteria.vision_2d.change_detection.dsifn_criterion import DSIFNCriterion
 from criteria.vision_2d.change_detection.dsfernet_criterion import DsferNetCriterion
+from criteria.vision_2d.change_detection.dsamnet_criterion import DSAMNetCriterion
 from criteria.vision_2d.change_detection.ftn_criterion import FTNCriterion
 from criteria.vision_2d.change_detection.srcnet_criterion import SRCNetCriterion
 from criteria.vision_2d.change_detection.cdmaskformer_criterion.cdmaskformer_criterion import CDMaskFormerCriterion
@@ -13,11 +14,13 @@ from criteria.vision_2d.change_detection.st_mamba_bcd_criterion import STMambaBC
 from criteria.vision_2d.change_detection.change_star_criterion import ChangeStarCriterion
 from criteria.vision_2d.change_detection.ppsl_criterion import PPSLCriterion
 
+
 __all__ = (
     'SymmetricChangeDetectionCriterion',
     'SNUNetCDCriterion',
     'DSIFNCriterion',
     'DsferNetCriterion',
+    'DSAMNetCriterion',
     'FTNCriterion',
     'SRCNetCriterion',
     'CDMaskFormerCriterion',
