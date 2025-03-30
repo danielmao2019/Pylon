babfb5bf107559dfd3216739a212c901a5b52a77
Daniel Mao <danielmao2019@gmail.com>
2025-03-30 14:52:56 -0400

    f

---
+++ configs/common/datasets/point_cloud_registration/val/overlappredator_data_cfg.py
@@ -0,0 +1,48 @@
+import data
+
+
+data_cfg = {
+    'val_dataset': {
+        'class': data.datasets.SynthPCRDataset,
+        'args': {
+            'data_root': './data/datasets/soft_links/ivision-pcr-data',
+            'split': 'val',
+            'rot_mag': 45.0,
+            'trans_mag': 0.5,
+            'voxel_size': 5.0,
+        },
+    },
+    'val_dataloader': {
+        'class': data.dataloaders.OverlapPredatorDataloader,
+        'args': {
+            'batch_size': 1,
+            'num_workers': 4,
+            'config': {
+                'deform_radius': 5.0,
+                'num_layers': 4,
+                'first_subsampling_dl': 0.3,
+                'conv_radius': 4.25,
+                'architecture': [
+                    'simple',
+                    'resnetb',
+                    'resnetb_strided',
+                    'resnetb',
+                    'resnetb',
+                    'resnetb_strided',
+                    'resnetb',
+                    'resnetb',
+                    'resnetb_strided',
+                    'resnetb',
+                    'resnetb',
+                    'nearest_upsample',
+                    'unary',
+                    'nearest_upsample',
+                    'unary',
+                    'nearest_upsample',
+                    'last_unary',
+                ],
+            },
+        },
+    },
+    'metric': None,
+}
