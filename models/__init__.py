d587158dc234f8b1571c8a9d1acc35410a384e7f
Daniel Mao <danielmao2019@gmail.com>
2025-03-29 20:36:43 -0400

    f

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 """
 MODELS API
 """
-from models import backbones
 from models import gans
 from models import multi_task_learning
 from models import change_detection
@@ -9,7 +8,6 @@ from models import point_cloud_registration
 
 
 __all__ = (
-    'backbones',
     'gans',
     'multi_task_learning',
     'change_detection',
