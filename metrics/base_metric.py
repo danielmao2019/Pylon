a1c81ae8805e8b27ec7a3fc0b4ed39ef81562a6a
Daniel Mao <danielmao2019@gmail.com>
2025-04-07 15:43:05 -0400

    f

--- metrics/base_metric.py
+++ metrics/base_metric.py
@@ -1,15 +1,18 @@
 from typing import List, Any, Optional
 from abc import ABC, abstractmethod
+import threading
 
 
 class BaseMetric(ABC):
 
     def __init__(self):
         super(BaseMetric, self).__init__()
+        self._buffer_lock = threading.Lock()
         self.reset_buffer()
 
     def reset_buffer(self):
-        self.buffer: List[Any] = []
+        with self._buffer_lock:
+            self.buffer: List[Any] = []
 
     @abstractmethod
     def __call__(self, y_pred: Any, y_true: Any) -> Any:
@@ -18,3 +21,13 @@ class BaseMetric(ABC):
     @abstractmethod
     def summarize(self, output_path: Optional[str] = None) -> Any:
         raise NotImplementedError("Abstract method BaseMetric.summarize not implemented.")
+        
+    def add_to_buffer(self, item: Any) -> None:
+        """Thread-safe method to add an item to the buffer."""
+        with self._buffer_lock:
+            self.buffer.append(item)
+            
+    def get_buffer(self) -> List[Any]:
+        """Thread-safe method to get a copy of the buffer."""
+        with self._buffer_lock:
+            return self.buffer.copy()
