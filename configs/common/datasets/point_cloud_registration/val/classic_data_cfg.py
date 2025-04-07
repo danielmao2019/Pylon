a26364147da1b415e2870c86a25798520b0f1fe0
Daniel Mao <danielmao2019@gmail.com>
2025-04-07 15:56:33 -0400

    f

--- configs/common/datasets/point_cloud_registration/val/classic_data_cfg.py
+++ configs/common/datasets/point_cloud_registration/val/classic_data_cfg.py
@@ -13,6 +13,7 @@ data_cfg = {
             'trans_mag': 0.5,
             'voxel_size': 10.0,
             'min_points': 256,
+            'device': 'cpu',
         },
     },
     'eval_dataloader': {
