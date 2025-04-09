c02ba3a589f638096177993e28109cb2c4028b73
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-04-09 13:17:05 -0400

    [Data][Transforms] Implement PCRTranslation class (#188)

--- configs/common/datasets/point_cloud_registration/train/real_pcr.py
+++ configs/common/datasets/point_cloud_registration/train/real_pcr.py
@@ -1,9 +1,9 @@
-from data.datasets import RealPCRDataset
+import data
 
 
 data_cfg = {
     'train_dataset': {
-        'class': RealPCRDataset,
+        'class': data.datasets.RealPCRDataset,
         'args': {
             'data_root': './data/datasets/soft_links/ivision-pcr-data',
             'gt_transforms': './data/datasets/soft_links/ivision-pcr-data/gt_transforms.json',
@@ -11,6 +11,17 @@ data_cfg = {
             'voxel_size': 10.0,
             'min_points': 256,
             'max_points': 8192,
+            'transforms_cfg': {
+                'class': data.transforms.Compose,
+                'args': {
+                    'transforms': [
+                        (
+                            data.transforms.vision_3d.PCRTranslation(),
+                            [('inputs', 'src_pc'), ('inputs', 'tgt_pc'), ('labels', 'transform')],
+                        ),
+                    ],
+                },
+            },
         },
     },
 }
