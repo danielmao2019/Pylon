e5974864793e037f1ef6d2e47724c39827d5685d
Daniel Mao <danielmao2019@gmail.com>
2025-04-09 01:20:16 -0400

    f

---
+++ configs/common/datasets/point_cloud_registration/train/geotransformer_real_pcr_data_cfg.py
@@ -0,0 +1,27 @@
+import data
+
+
+data_cfg = {
+    'train_dataset': {
+        'class': data.datasets.RealPCRDataset,
+        'args': {
+            'data_root': './data/datasets/soft_links/ivision-pcr-data',
+            'split': 'train',
+            'gt_transforms': './data/datasets/soft_links/ivision-pcr-data/gt_transforms.json',
+            'voxel_size': 10.0,
+            'min_points': 256,
+            'max_points': 8192,
+        },
+    },
+    'train_dataloader': {
+        'class': data.dataloaders.GeoTransformerDataloader,
+        'args': {
+            'batch_size': 1,
+            'num_workers': 4,
+            'num_stages': 4,
+            'voxel_size': 0.1,
+            'search_radius': 2.5 * 0.1,
+        },
+    },
+    'criterion': None,
+}
