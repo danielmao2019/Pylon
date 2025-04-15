b9ae7829b484432a63bc693c451bccc9421a1363
Daniel Mao <danielmao2019@gmail.com>
2025-04-15 18:33:34 -0400

    f

--- configs/common/datasets/point_cloud_registration/val/classic_real_pcr_data_cfg.py
+++ configs/common/datasets/point_cloud_registration/val/classic_real_pcr_data_cfg.py
@@ -14,7 +14,7 @@ data_cfg = {
             'voxel_size': 10.0,
             'min_points': 256,
             'max_points': 8192,
-            'overlap': 0.4,
+            'overlap': 0.5,
             'device': 'cpu',
             'transforms_cfg': {
                 'class': data.transforms.Compose,
