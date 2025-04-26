8d3f59b30d412b98c2a5e95b2016f423d990936a
Daniel Mao <danielmao2019@gmail.com>
2025-04-26 14:30:16 -0400

    f

--- metrics/vision_3d/inlier_ratio.py
+++ metrics/vision_3d/inlier_ratio.py
@@ -42,10 +42,10 @@ class InlierRatio(PointCloudMetric):
         y_pred, y_true, N, M = self._validate_and_prepare_inputs(y_pred, y_true)
         
         # Compute distance matrix and find nearest neighbors
-        _, min_dist, _ = self._compute_distance_matrix(y_pred, y_true)
+        dist_matrix, min_distances, nearest_indices = self._compute_distance_matrix(y_pred, y_true)
         
         # Count inliers (points within threshold)
-        inlier_mask = (min_dist <= self.threshold)  # (N,)
+        inlier_mask = (min_distances <= self.threshold)  # (N,)
         inlier_ratio = torch.mean(inlier_mask.float())
         
         # Get indices of inlier points
