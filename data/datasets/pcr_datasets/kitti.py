3b245c41c629ab01239ac0a3ed001e61af8ab971
Daniel Mao <danielmao2019@gmail.com>
2025-05-22 20:16:07 -0400

    f

--- data/datasets/pcr_datasets/kitti.py
+++ data/datasets/pcr_datasets/kitti.py
@@ -49,10 +49,13 @@ class KITTIDataset(BaseDataset):
         super(KITTIDataset, self).__init__(**kwargs)
 
     def _init_annotations(self) -> None:
+        self.annotations_cache_root = os.path.join(self.data_root, 'annotations')
         if os.path.isfile(os.path.join(self.annotations_cache_root, f'{self.split}.json')):
             with open(os.path.join(self.annotations_cache_root, f'{self.split}.json'), 'r') as f:
                 self.annotations = json.load(f)
             return
+
+        os.makedirs(self.annotations_cache_root, exist_ok=True)
         self.annotations: List[Dict[str, Any]] = []
         self.seq_pose_cache = {}
         sequences = self.SEQUENCES_SPLIT[self.split]
