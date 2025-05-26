fa579c180022b60fc028e91c39758baa11e994f1
Daniel Mao <danielmao2019@gmail.com>
2025-05-26 14:32:54 -0400

    f

--- utils/point_cloud_ops/select.py
+++ utils/point_cloud_ops/select.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
 import torch
+from utils.input_checks.point_cloud import check_point_cloud
 
 
 class Select:
@@ -7,13 +8,9 @@ class Select:
         self.indices = indices
 
     def __call__(self, pc: Dict[str, Any]) -> Dict[str, Any]:
-        assert isinstance(pc, dict), f"{type(pc)=}"
-        assert pc.keys() >= {'pos'}, f"{pc.keys()=}"
-        assert pc['pos'].ndim == 2 and pc['pos'].shape[1] == 3, f"{pc['pos'].shape=}"
-        assert pc['pos'].dtype == torch.float32, f"{pc['pos'].dtype=}"
+        check_point_cloud(pc)
         result = {}
         for key, val in pc.items():
-            if isinstance(val, torch.Tensor) and val.size(0) == pc['pos'].size(0):
-                result[key] = val[self.indices]
+            result[key] = val[self.indices]
         result['indices'] = self.indices
         return result
