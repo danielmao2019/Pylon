84f0b632a6eb1d7eb8e0124d706993f163ea7ce4
Daniel Mao <danielmao2019@gmail.com>
2025-05-26 15:46:47 -0400

    f

--- data/transforms/vision_3d/__init__.py
+++ data/transforms/vision_3d/__init__.py
@@ -4,6 +4,7 @@ DATA.TRANSFORMS.VISION_3D API
 from data.transforms.vision_3d.scale import Scale
 from data.transforms.vision_3d.downsample import DownSample
 from data.transforms.vision_3d.shuffle import Shuffle
+from data.transforms.vision_3d.clamp import Clamp
 from data.transforms.vision_3d.pcr_translation import PCRTranslation
 from data.transforms.vision_3d.random_rigid_transform import RandomRigidTransform
 
@@ -12,6 +13,7 @@ __all__ = (
     'Scale',
     'DownSample',
     'Shuffle',
+    'Clamp',
     'PCRTranslation',
     'RandomRigidTransform',
 )
