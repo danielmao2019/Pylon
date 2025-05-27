8f24f08cca4a4786aeb3f99fe7bd01dc63fd146c
Daniel Mao <danielmao2019@gmail.com>
2025-05-27 16:59:05 -0400

    f

--- models/point_cloud_registration/__init__.py
+++ models/point_cloud_registration/__init__.py
@@ -4,10 +4,12 @@ MODELS.POINT_CLOUD_REGISTRATION API
 from models.point_cloud_registration import classic
 from models.point_cloud_registration.geotransformer.geotransformer import GeoTransformer
 from models.point_cloud_registration.overlappredator.overlappredator import OverlapPredator
+from models.point_cloud_registration.buffer.buffer import BUFFER
 
 
 __all__ = (
     'classic',
     'GeoTransformer',
     'OverlapPredator',
+    'BUFFER',
 )
