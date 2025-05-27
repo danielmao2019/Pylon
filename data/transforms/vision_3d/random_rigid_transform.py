4c47c8f62106b881d51d9a26b5e25333b5af98c5
Daniel Mao <danielmao2019@gmail.com>
2025-05-27 18:36:38 -0400

    f

--- data/transforms/vision_3d/random_rigid_transform.py
+++ data/transforms/vision_3d/random_rigid_transform.py
@@ -119,7 +119,7 @@ class RandomRigidTransform(BaseTransform):
         # source point cloud to the target point cloud
         random_transform_inv = torch.inverse(random_transform)
         assert torch.equal(random_transform_inv[-1, :], torch.tensor([0, 0, 0, 1], device=random_transform_inv.device))
-        assert torch.allclose(random_transform_inv[:3, :3], random_transform[:3, :3].T)
+        assert torch.allclose(random_transform_inv[:3, :3], random_transform[:3, :3].T), f"{random_transform_inv[:3, :3]=}\n{random_transform[:3, :3].T=}"
         assert torch.allclose(random_transform_inv[:3, 3], -random_transform[:3, :3].T @ random_transform[:3, 3])
 
         new_transform = transform @ random_transform_inv
