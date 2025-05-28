74a885a00676a85f856f44ea5d49a5f8a25013ef
Daniel Mao <danielmao2019@gmail.com>
2025-05-27 20:31:05 -0400

    f

--- runners/pcr_trainers/buffer_trainer.py
+++ runners/pcr_trainers/buffer_trainer.py
@@ -5,7 +5,8 @@ class BufferTrainer(SupervisedSingleTaskTrainer):
     
     def _init_model_(self) -> None:
         super(BufferTrainer, self)._init_model_()
-        freeze_stages = ['Ref', 'Desc', 'Keypt', 'Inlier'].remove(self.config['stage'])
+        freeze_stages = ['Ref', 'Desc', 'Keypt', 'Inlier']
+        freeze_stages.remove(self.config['stage'])
         for stage in freeze_stages:
             for param in getattr(self.model, stage).parameters():
                 param.requires_grad = False
