0285ec913c2202fba0771ba46a52e3a3553b2265
Daniel Mao <danielmao2019@gmail.com>
2025-05-27 20:15:39 -0400

    f

--- criteria/vision_3d/point_cloud_registration/buffer_criteria/ref_stage_criterion.py
+++ criteria/vision_3d/point_cloud_registration/buffer_criteria/ref_stage_criterion.py
@@ -6,6 +6,11 @@ from criteria.wrappers import SingleTaskCriterion
 class BUFFER_RefStageCriterion(SingleTaskCriterion):
 
     def __call__(self, y_pred: Dict[str, Any], y_true: Dict[str, torch.Tensor]) -> torch.Tensor:
+        assert isinstance(y_pred, dict)
+        assert y_pred.keys() == {'src_ref', 'tgt_ref', 'src_s', 'tgt_s'}, f"{y_pred.keys()=}"
+        assert isinstance(y_true, dict)
+        assert y_true.keys() == {'transform'}, f"{y_true.keys()=}"
+
         src_axis, tgt_axis = y_pred['src_ref'], y_pred['tgt_ref']
         gt_trans = y_true['transform']
         src_axis = src_axis @ gt_trans[:3, :3].transpose(-1, -2)
