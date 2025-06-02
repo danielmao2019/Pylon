027ef16c21b91db753d6988e33b92a59cba87200
Daniel Mao <danielmao2019@gmail.com>
2025-06-01 21:51:18 -0400

    f

--- tests/runners/supervised_single_task_trainer/test_compare_pytorch.py
+++ tests/runners/supervised_single_task_trainer/test_compare_pytorch.py
@@ -15,26 +15,32 @@ class Dataset(torch.utils.data.Dataset):
         x = torch.rand(size=(2,), dtype=torch.float32)
         noise = torch.randn(size=(2,), dtype=torch.float32)
         y = gt @ x + noise * 0.001
-        return {'x': x, 'y': y}
+        return x, y
 
     def __len__(self) -> int:
         return 10
 
 
 def run_pytorch() -> None:
-    dataset = Dataset()
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
-    model = torch.nn.Linear(in_features=2, out_features=2)
+    torch.manual_seed(0)
+    train_dataset = Dataset()
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=8)
+    val_dataset = Dataset()
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=8)
     criterion = torch.nn.MSELoss(reduction='mean')
     metric = torch.nn.MSELoss(reduction='mean')
+    model = torch.nn.Linear(in_features=2, out_features=2)
     optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-03)
     work_dir = "./logs/tests/supervised_single_task_trainer/compare_pytorch"
-    for idx in range(100):
+    for idx in range(10):
+        torch.manual_seed(0)
         all_losses = []
         all_scores = []
+        epoch_dir = os.path.join(work_dir, f"epoch_{idx}")
+        os.makedirs(epoch_dir, exist_ok=True)
         # train epoch
         model.train()
-        for datapoint in dataloader:
+        for datapoint in train_dataloader:
             loss = criterion(input=model(datapoint[0]), target=datapoint[1])
             optimizer.zero_grad()
             loss.backward()
@@ -42,11 +48,11 @@ def run_pytorch() -> None:
             all_losses.append(loss.item())
         # save losses
         save_losses = torch.tensor(all_losses)
-        torch.save(save_losses, os.path.join(work_dir, f"epoch_{idx}", "training_losses.pt"))
+        torch.save(save_losses, os.path.join(epoch_dir, "training_losses.pt"))
         # val epoch
         model.eval()
         with torch.no_grad():
-            for datapoint in dataloader:
+            for datapoint in val_dataloader:
                 score = metric(input=model(datapoint[0]), target=datapoint[1])
                 all_scores.append(score.item())
         # save scores
@@ -58,7 +64,7 @@ def run_pytorch() -> None:
                 'score': torch.tensor(all_scores).detach().tolist(),
             },
         }
-        with open(os.path.join(work_dir, f"epoch_{idx}", "validation_scores.pt"), "w") as f:
+        with open(os.path.join(epoch_dir, "validation_scores.pt"), "w") as f:
             json.dump(save_scores, f)
 
 
@@ -66,7 +72,7 @@ def test_compare_pytorch() -> None:
     run_pytorch()
     dir1 = "./logs/examples/linear/"
     dir2 = "./logs/tests/supervised_single_task_trainer/compare_pytorch"
-    for idx in range(100):
+    for idx in range(10):
         losses1 = torch.load(os.path.join(dir1, f"epoch_{idx}", "training_losses.pt"))
         losses2 = torch.load(os.path.join(dir2, f"epoch_{idx}", "training_losses.pt"))
         assert torch.allclose(losses1, losses2), f"{idx} - losses"
