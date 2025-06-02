e513378614d149d301f8a850c1b5c56025ed9478
Daniel Mao <danielmao2019@gmail.com>
2025-06-01 21:43:30 -0400

    f

--- tests/runners/supervised_single_task_trainer/test_compare_pytorch.py
+++ tests/runners/supervised_single_task_trainer/test_compare_pytorch.py
@@ -1,27 +1,73 @@
-def test_compare_pytorch() -> None:
-    dataset = dataset_config['class'](**dataset_config['args'])
+from typing import Tuple
+import torch
+
+
+torch.manual_seed(0)
+gt = torch.rand(size=(2, 2), dtype=torch.float32)
+
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        torch.manual_seed(idx)
+        x = torch.rand(size=(2,), dtype=torch.float32)
+        noise = torch.randn(size=(2,), dtype=torch.float32)
+        y = gt @ x + noise * 0.001
+        return {'x': x, 'y': y}
+
+    def __len__(self) -> int:
+        return 10
+
+
+def run_pytorch() -> None:
+    dataset = Dataset()
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
-    model = torchvision.models.ResNet(torchvision.models.resnet.BasicBlock, [2, 2, 2, 2], num_classes=10)
-    criterion = config['criterion']['class'](**config['criterion']['args'])
-    metric = config['metric']['class'](**config['metric']['args'])
+    model = torch.nn.Linear(in_features=2, out_features=2)
+    criterion = torch.nn.MSELoss(reduction='mean')
+    metric = torch.nn.MSELoss(reduction='mean')
     optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-03)
-    all_accuracies: List[float] = []
-    for _ in range(config['epochs']):
+    work_dir = "./logs/tests/supervised_single_task_trainer/compare_pytorch"
+    for idx in range(100):
+        all_losses = []
+        all_scores = []
         # train epoch
         model.train()
-        for example in dataloader:
-            outputs = model(example['inputs']['image'])
-            loss = criterion(y_pred=outputs, y_true=example['labels']['target'])
+        for datapoint in dataloader:
+            loss = criterion(input=model(datapoint[0]), target=datapoint[1])
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            all_losses.append(loss.item())
+        # save losses
+        save_losses = torch.tensor(all_losses)
+        torch.save(save_losses, os.path.join(work_dir, f"epoch_{idx}", "training_losses.pt"))
         # val epoch
         model.eval()
-        metric.reset_buffer()
-        for example in dataloader:
-            outputs = model(example['inputs']['image'])
-            metric(y_pred=outputs, y_true=example['labels']['target'])
-        all_accuracies.append(metric.summarize()['accuracy'])
-    plt.figure()
-    plt.plot(all_accuracies)
-    plt.savefig(os.path.join(config['work_dir'], "acc_pytorch.png"))
+        with torch.no_grad():
+            for datapoint in dataloader:
+                score = metric(input=model(datapoint[0]), target=datapoint[1])
+                all_scores.append(score.item())
+        # save scores
+        save_scores = {
+            'aggregated': {
+                'score': torch.tensor(all_scores).detach().mean().item(),
+            },
+            'per_datapoint': {
+                'score': torch.tensor(all_scores).detach().tolist(),
+            },
+        }
+        with open(os.path.join(work_dir, f"epoch_{idx}", "validation_scores.pt"), "w") as f:
+            json.dump(save_scores, f)
+
+
+def test_compare_pytorch() -> None:
+    run_pytorch()
+    dir1 = "./logs/examples/linear/"
+    dir2 = "./logs/tests/supervised_single_task_trainer/compare_pytorch"
+    for idx in range(100):
+        losses1 = torch.load(os.path.join(dir1, f"epoch_{idx}", "training_losses.pt"))
+        losses2 = torch.load(os.path.join(dir2, f"epoch_{idx}", "training_losses.pt"))
+        assert torch.allclose(losses1, losses2), f"{idx} - losses"
+        scores1 = json.load(open(os.path.join(dir1, f"epoch_{idx}", "validation_scores.pt")))
+        scores2 = json.load(open(os.path.join(dir2, f"epoch_{idx}", "validation_scores.pt")))
+        assert scores1 == scores2, f"{idx} - scores"
