3788ca553f41213b8c720fbbdd82eed4c5e5468f
Daniel Mao <danielmao2019@gmail.com>
2025-06-05 23:30:08 -0400

    f

--- configs/benchmarks/multi_task_learning/gen_multi_task_learning.py
+++ configs/benchmarks/multi_task_learning/gen_multi_task_learning.py
@@ -101,7 +101,7 @@ def gen_method_configs(dataset_name: str, model_name: str, method_name: str) -> 
     config += '\n'
     # add seeds
     relpath: str = os.path.join("benchmarks", "multi_task_learning", dataset_name, model_name)
-    seeded_configs: List[str] = utils.automation.configs.generate_seeds(
+    seeded_configs: List[str] = generate_seeds(
         template_config=config,
         base_seed=os.path.join(relpath, method_name),
     )
