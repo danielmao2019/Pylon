cf9cf2cc67a8b0041780634ef610909867255b02
Daniel Mao <danielmao2019@gmail.com>
2025-06-08 16:23:20 -0400

    f

--- agents/viewer/backend.py
+++ agents/viewer/backend.py
@@ -10,6 +10,22 @@ def get_progress(
     outdated_days: int,
     servers: List[str],
 ) -> float:
+    """
+    Args:
+        config_files: List of config file paths
+        expected_files: List of expected file patterns
+        epochs: Total number of epochs
+        sleep_time: Time to wait for the status to update
+        outdated_days: Number of days to consider a run outdated
+        servers: List of servers
+    """
+    assert isinstance(config_files, list)
+    assert isinstance(expected_files, list)
+    assert isinstance(epochs, int)
+    assert isinstance(sleep_time, int)
+    assert isinstance(outdated_days, int)
+    assert isinstance(servers, list)
+
     all_run_status = get_all_run_status(config_files, expected_files, epochs, sleep_time, outdated_days, servers)
     all_run_progress = [run.progress for run in all_run_status]
     return round(sum(all_run_progress) / len(all_run_progress), 2)
