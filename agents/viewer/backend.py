f0f266fe9bde20cfc7f0a0bef0229eb60f1e5ce6
Daniel Mao <danielmao2019@gmail.com>
2025-06-08 16:18:59 -0400

    f

--- agents/viewer/backend.py
+++ agents/viewer/backend.py
@@ -1,8 +1,15 @@
 from typing import List
-from utils.automation.run_status import RunStatus
+from utils.automation.run_status import get_all_run_status
 
 
-def get_progress(all_run_status: List[RunStatus]) -> float:
-    """Get the progress of all config files in parallel."""
+def get_progress(
+    config_files: List[str],
+    expected_files: List[str],
+    epochs: int,
+    servers: List[str],
+    sleep_time: int,
+    outdated_days: int,
+) -> float:
+    all_run_status = get_all_run_status(config_files, expected_files, epochs, servers, sleep_time, outdated_days)
     all_run_progress = [run.progress for run in all_run_status]
     return round(sum(all_run_progress) / len(all_run_progress), 2)
