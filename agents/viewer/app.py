042c21f55b44fc7ddd4b705b56e5f5457b4e56e8
Daniel Mao <danielmao2019@gmail.com>
2025-06-08 16:46:25 -0400

    f

--- agents/viewer/app.py
+++ agents/viewer/app.py
@@ -1,12 +1,11 @@
 from typing import Tuple, List, Dict, Optional
 import dash
-from utils.monitor.gpu_status import GPUStatus
-from utils.monitor.gpu_monitor import GPUMonitor
+from agents.base_agent import BaseAgent
 from agents.viewer.layout import create_layout
 from agents.viewer.callback import register_callbacks
 
 
-class AgentsViewerApp:
+class AgentsViewerApp(BaseAgent):
 
     def __init__(
         self,
@@ -18,30 +17,10 @@ class AgentsViewerApp:
         gpu_pool: List[Tuple[str, List[int]]] = [],
         user_names: Dict[str, str] = {},
     ) -> None:
-        self._init_gpu_monitor(gpu_pool)
-        self.servers = [server for server, _ in gpu_pool]
+        super(AgentsViewerApp, self).__init__(config_files, expected_files, epochs, sleep_time, outdated_days, gpu_pool, user_names)
         self.app = dash.Dash(__name__)
         self.app.layout = create_layout(config_files, expected_files, epochs, sleep_time, outdated_days, self.servers, self.gpu_monitor, user_names)
         register_callbacks(self.app, config_files, expected_files, epochs, sleep_time, outdated_days, self.servers, self.gpu_monitor, user_names)
-
-    def _init_gpu_monitor(self, gpu_pool: List[Tuple[str, List[int]]]) -> None:
-        gpus = [
-            GPUStatus(
-                server=server,
-                index=idx,
-                max_memory=0,  # Will be populated by monitor
-                processes=[],
-                window_size=10,
-                memory_window=[],
-                util_window=[],
-                memory_stats={'min': None, 'max': None, 'avg': None},
-                util_stats={'min': None, 'max': None, 'avg': None}
-            )
-            for server, indices in gpu_pool
-            for idx in indices
-        ]
-        self.gpu_monitor = GPUMonitor(gpus)
-        self.gpu_monitor.start()
 
     def run(self, port: Optional[int] = 8050) -> None:
         self.app.run(port=port)
