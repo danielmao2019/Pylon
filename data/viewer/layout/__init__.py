c61ce019d0d371955a109e92717581015a35a93a
Daniel Mao <danielmao2019@gmail.com>
2025-06-08 22:57:20 -0400

    f

--- data/viewer/layout/__init__.py
+++ data/viewer/layout/__init__.py
@@ -3,8 +3,10 @@ DATA.VIEWER.UI API
 """
 
 # Import all UI components
-from data.viewer.layout.display.display_2d import display_2d_datapoint
-from data.viewer.layout.display.display_3d import display_3d_datapoint
+from data.viewer.layout.display.display_2d import display_2dcd_datapoint
+from data.viewer.layout.display.display_3d import display_3dcd_datapoint
+from data.viewer.layout.display.display_pcr import display_pcr_datapoint
+from data.viewer.layout.display.display_semseg import display_semseg_datapoint
 from data.viewer.layout.display.dataset import create_dataset_info_display
 from data.viewer.layout.controls.dataset import create_dataset_selector, create_reload_button
 from data.viewer.layout.controls.navigation import create_navigation_controls
@@ -13,8 +15,10 @@ from data.viewer.layout.app import create_app_layout
 
 
 __all__ = [
-    'display_2d_datapoint',
-    'display_3d_datapoint',
+    'display_2dcd_datapoint',
+    'display_3dcd_datapoint',
+    'display_pcr_datapoint',
+    'display_semseg_datapoint',
     'create_dataset_selector',
     'create_reload_button',
     'create_navigation_controls',
