d0234678ff36dcb60ab849a3b2d49aa9eebe20fa
Daniel Mao <danielmao2019@gmail.com>
2025-06-09 21:08:48 -0400

    [Data][Collators] Debug BUFFER

--- utils/builders/builder.py
+++ utils/builders/builder.py
@@ -4,23 +4,23 @@ import easydict as edict
 import torch
 
 
-def deepcopy_without_params(obj: Any) -> Any:
+def deepcopy_rec(obj: Any) -> Any:
     """A version of deepcopy that preserves PyTorch parameters.
-    
+
     Args:
         obj: The object to copy
-        
+
     Returns:
         A deep copy of the object, but with PyTorch parameters preserved as references
     """
-    if isinstance(obj, torch.nn.Parameter):
+    if isinstance(obj, (torch.nn.Parameter, edict.EasyDict)):
         return obj
-    elif isinstance(obj, list):
-        return [deepcopy_without_params(item) for item in obj]
     elif isinstance(obj, dict):
-        return {key: deepcopy_without_params(value) for key, value in obj.items()}
+        return {key: deepcopy_rec(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [deepcopy_rec(item) for item in obj]
     elif isinstance(obj, tuple):
-        return tuple(deepcopy_without_params(item) for item in obj)
+        return tuple(deepcopy_rec(item) for item in obj)
     else:
         return deepcopy(obj)
 
@@ -36,17 +36,17 @@ def build_from_config(config: Any, **kwargs) -> Any:
         return config
     if isinstance(config, dict) and config.keys() == {'class', 'args'}:
         # Create a deep copy to avoid modifying input, but preserve parameters
-        config_copy = deepcopy_without_params(config)
-        
+        config_copy = deepcopy_rec(config)
+
         # merge args
         assert type(kwargs) == dict, f"{type(kwargs)=}"
         assert set(config_copy.keys()) & set(kwargs.keys()) == set(), f"{config_copy.keys()=}, {kwargs.keys()=}"
         config_copy['args'].update(kwargs)
-        
+
         # build args
         for key in config_copy['args']:
             config_copy['args'][key] = build_from_config(config_copy['args'][key])
-            
+
         # build self
         return config_copy['class'](**config_copy['args'])
     elif isinstance(config, dict):
