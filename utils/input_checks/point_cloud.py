689974743256d1e2d36ada16dac30d8d5802b22f
Daniel Mao <danielmao2019@gmail.com>
2025-06-15 19:37:35 -0400

    f

--- utils/input_checks/point_cloud.py
+++ utils/input_checks/point_cloud.py
@@ -10,7 +10,7 @@ def check_point_cloud(pc: Dict[str, torch.Tensor]) -> None:
     assert 'pos' in pc, f"{pc.keys()=}"
     assert pc['pos'].ndim == 2, f"{pc['pos'].shape=}"
     assert pc['pos'].shape[1] == 3, f"{pc['pos'].shape=}"
-    
+
     length = pc['pos'].shape[0]
     assert all(pc[key].shape[0] == length for key in pc.keys() if key != 'pos'), \
         f"{{{', '.join(f'{k}: {pc[k].shape}' for k in pc.keys())}}}"
