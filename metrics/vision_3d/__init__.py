5f943359dc771149b6ff3669c6dad309257da61d
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2025-06-16 18:53:52 -0400

    [Metrics][Vision3D][PCR] Implement InlierRatio and PointInlierRatio (#235)

--- metrics/vision_3d/__init__.py
+++ metrics/vision_3d/__init__.py
@@ -2,18 +2,14 @@
 METRICS.VISION_3D API
 """
 from metrics.vision_3d.chamfer_distance import ChamferDistance
-from metrics.vision_3d.inlier_ratio import InlierRatio
 from metrics.vision_3d.mae import MAE
-from metrics.vision_3d.registration_recall import RegistrationRecall
 from metrics.vision_3d.rmse import RMSE
 from metrics.vision_3d import point_cloud_registration
 
 
 __all__ = (
     'ChamferDistance',
-    'InlierRatio',
     'MAE',
-    'RegistrationRecall',
     'RMSE',
     'point_cloud_registration',
 )
