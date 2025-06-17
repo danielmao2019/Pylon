a75911a9687e5a9a9ffd780e8296451461237099
Daniel Mao <danielmao2019@gmail.com>
2025-06-17 19:47:26 -0400

    f

--- utils/point_cloud_ops/__init__.py
+++ utils/point_cloud_ops/__init__.py
@@ -6,7 +6,6 @@ from utils.point_cloud_ops import set_ops
 from utils.point_cloud_ops.apply_transform import apply_transform
 from utils.point_cloud_ops.correspondences import get_correspondences
 from utils.point_cloud_ops.grid_sampling import grid_sampling
-from utils.point_cloud_ops.correspondences import get_correspondences
 from utils.point_cloud_ops.select import Select
 from utils.point_cloud_ops.random_select import RandomSelect
 
@@ -17,7 +16,6 @@ __all__ = (
     'apply_transform',
     'get_correspondences',
     'grid_sampling',
-    'get_correspondences',
     'Select',
     'RandomSelect',
 )
