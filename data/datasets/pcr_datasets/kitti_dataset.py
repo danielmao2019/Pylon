647ac7b78e532228bf24fa5037f7545893f942f1
Daniel Mao <danielmao2019@gmail.com>
2025-06-26 20:58:47 -0400

    f

--- data/datasets/pcr_datasets/kitti_dataset.py
+++ data/datasets/pcr_datasets/kitti_dataset.py
@@ -180,7 +180,6 @@ class KITTIDataset(BaseDataset):
             'transform': torch.from_numpy(gt_transform).float(),
         }
         meta_info = {
-            'idx': idx,
             'seq': seq,
             't0': t0,
             't1': t1,
