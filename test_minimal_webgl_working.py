5decf2f7259f8cf1414fd0bc5a9b4188e704eaa7
Daniel Mao <danielmao2019@gmail.com>
2025-07-03 23:07:39 -0400

    f

--- test_minimal_webgl_working.py
+++ test_minimal_webgl_working.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-ABSOLUTE MINIMAL WebGL test - just show some points!
+Baby step 3: WebGL shaders + single red point
 """
 
 import dash
@@ -9,7 +9,7 @@ from dash import html, Input, Output, clientside_callback
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("WebGL Test"),
+    html.H1("WebGL Single Red Point Test"),
     html.Canvas(id='canvas', width=400, height=400, style={'border': '1px solid black'}),
     html.Div(id='output')
 ])
@@ -18,19 +18,50 @@ clientside_callback(
     """
     function() {
         const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
+        const gl = canvas.getContext('webgl');
         
-        // Just draw some colored circles to test if anything works
-        ctx.fillStyle = 'red';
-        ctx.fillRect(50, 50, 50, 50);
+        if (!gl) {
+            return 'WebGL not supported';
+        }
         
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(150, 150, 50, 50);
+        // Simple vertex shader
+        const vertexShaderSource = `
+            void main() {
+                gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+                gl_PointSize = 20.0;
+            }
+        `;
         
-        ctx.fillStyle = 'green';
-        ctx.fillRect(250, 250, 50, 50);
+        // Simple fragment shader
+        const fragmentShaderSource = `
+            precision mediump float;
+            void main() {
+                gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        `;
         
-        return 'Canvas 2D test - should see 3 colored squares';
+        // Create shaders
+        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertexShader, vertexShaderSource);
+        gl.compileShader(vertexShader);
+        
+        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragmentShader, fragmentShaderSource);
+        gl.compileShader(fragmentShader);
+        
+        // Create program
+        const program = gl.createProgram();
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+        
+        // Clear and draw
+        gl.clearColor(0.2, 0.2, 0.2, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.useProgram(program);
+        gl.drawArrays(gl.POINTS, 0, 1);
+        
+        return 'Should see one big red point in center of grey canvas';
     }
     """,
     Output('output', 'children'),
