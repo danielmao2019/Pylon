eadf7c38b5370a7e52dd91a7d512e82de844840e
Daniel Mao <danielmao2019@gmail.com>
2025-07-03 20:58:01 -0400

    f

--- data/viewer/utils/point_cloud/__init__.py
+++ data/viewer/utils/point_cloud/__init__.py
@@ -4,18 +4,16 @@ DATA.VIEWER.UTILS.POINT_CLOUD API
 from .point_cloud import (
     point_cloud_to_numpy,
     prepare_point_cloud_data,
-    create_webgl_point_cloud_component,
     create_point_cloud_figure,
     get_point_cloud_stats,
-    get_webgl_assets
+    register_webgl_callback
 )
 
 
 __all__ = (
     'point_cloud_to_numpy',
     'prepare_point_cloud_data',
-    'create_webgl_point_cloud_component',
     'create_point_cloud_figure',
     'get_point_cloud_stats',
-    'get_webgl_assets'
+    'register_webgl_callback'
 )
