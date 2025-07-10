2eaab0a8ff2a71c00da9d2f7789273daa50c9033
Daniel Mao <danielmao2019@gmail.com>
2025-07-10 15:13:58 -0400

    f

--- runners/eval_viewer/callbacks/update_plots.py
+++ runners/eval_viewer/callbacks/update_plots.py
@@ -1,7 +1,7 @@
 from typing import List, Dict
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import numpy as np
-from dash import Input, Output, dcc
+from dash import Input, Output, State, dcc
 from dash.exceptions import PreventUpdate
 import dash
 import plotly.graph_objects as go
@@ -118,9 +118,13 @@ def register_callbacks(app: dash.Dash, metric_names: List[str], num_datapoints: 
          Output('overlaid-color-bar', 'children')],
         [Input('epoch-slider', 'value'),
          Input('metric-dropdown', 'value'),
+         Input('percentile-slider', 'drag_value'),
          Input('percentile-slider', 'value')]
     )
-    def update_overlaid_score_map(epoch: int, metric_name: str, percentile: float):
+    def update_overlaid_score_map(epoch: int, metric_name: str, percentile_drag: float, percentile_value: float):
+        # Use drag_value if available (while dragging), otherwise use value
+        percentile = percentile_drag if percentile_drag is not None else percentile_value
+        
         if metric_name is None or epoch is None or percentile is None:
             raise PreventUpdate
 
