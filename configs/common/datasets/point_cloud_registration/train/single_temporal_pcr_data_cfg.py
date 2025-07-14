fffefd5f8e79b2d010a308365b07f3e4306cc32b
Daniel Mao <danielmao2019@gmail.com>
2025-07-14 13:39:35 -0400

    f

---
+++ configs/common/datasets/point_cloud_registration/train/single_temporal_pcr_data_cfg.py
@@ -0,0 +1,33 @@
+import data
+
+
+data_cfg = {
+    'train_dataset': {
+        'class': data.datasets.SingleTemporalPCRDataset,
+        'args': {
+            'data_root': './data/datasets/soft_links/ivision-pcr-data',
+            'cache_dirname': 'singletemporal_pcr_cache',
+            'split': 'train',
+            'dataset_size': 5000,  # Total number of synthetic registration pairs to generate
+            'overlap_range': (0.0, 1.0),  # GeoTransformer doesn't use specific overlap ranges
+            'matching_radius': 0.05,  # Radius for correspondence finding
+            'rotation_mag': 45.0,  # GeoTransformer synthetic transform parameters
+            'translation_mag': 0.5,  # GeoTransformer synthetic transform parameters
+            'transforms_cfg': {
+                'class': data.transforms.Compose,
+                'args': {
+                    'transforms': [
+                        # Point sampling transform - moved from dataset initialization
+                        (
+                            {
+                                'class': data.transforms.vision_3d.RandomPointSampling,
+                                'args': {'min_points': 512, 'max_points': 8192},
+                            },
+                            [('inputs', 'src_pc'), ('inputs', 'tgt_pc')],
+                        ),
+                    ],
+                },
+            },
+        },
+    },
+}
