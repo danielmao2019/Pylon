6c40141ca3ac92ae0b255b6b25cd17443e71b3aa
Daniel Mao <danielmao2019@gmail.com>
2025-07-14 13:39:14 -0400

    f

--- configs/common/datasets/point_cloud_registration/train/bi_temporal_pcr_data_cfg.py
+++ configs/common/datasets/point_cloud_registration/train/bi_temporal_pcr_data_cfg.py
@@ -3,12 +3,17 @@ import data
 
 data_cfg = {
     'train_dataset': {
-        'class': data.datasets.RealPCRDataset,
+        'class': data.datasets.BiTemporalPCRDataset,
         'args': {
             'data_root': './data/datasets/soft_links/ivision-pcr-data',
             'gt_transforms_filepath': './data/datasets/soft_links/ivision-pcr-data/gt_transforms.json',
             'cache_dirname': 'real_pcr_cache',
             'split': 'train',
+            'dataset_size': 5000,  # Total number of synthetic registration pairs to generate
+            'overlap_range': (0.0, 1.0),  # GeoTransformer doesn't use specific overlap ranges
+            'matching_radius': 0.05,  # Radius for correspondence finding
+            'rotation_mag': 45.0,  # GeoTransformer synthetic transform parameters
+            'translation_mag': 0.5,  # GeoTransformer synthetic transform parameters
             'voxel_size': 10.0,
             'min_points': 512,
             'max_points': 8192,
