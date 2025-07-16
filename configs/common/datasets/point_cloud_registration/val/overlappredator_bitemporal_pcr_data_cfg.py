c3dd173931235010c73cc7e7b9e1196caca7d0ee
Daniel Mao <danielmao2019@gmail.com>
2025-07-16 14:16:11 -0400

    f

--- configs/common/datasets/point_cloud_registration/val/overlappredator_bitemporal_pcr_data_cfg.py
+++ configs/common/datasets/point_cloud_registration/val/overlappredator_bitemporal_pcr_data_cfg.py
@@ -6,7 +6,7 @@ data_cfg = {
         'class': data.datasets.BiTemporalPCRDataset,
         'args': {
             'data_root': './data/datasets/soft_links/ivision-pcr-data',
-            'gt_transforms_filepath': './data/datasets/soft_links/ivision-pcr-data/gt_transforms.json',
+            'gt_transforms': './data/datasets/soft_links/ivision-pcr-data/gt_transforms.json',
             'cache_filepath': './data/datasets/soft_links/ivision-pcr-data/../bi_temporal_pcr_cache.json',
             'split': 'val',
             'dataset_size': 1000,  # Smaller for validation
