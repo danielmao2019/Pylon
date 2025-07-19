51644c71c7d1288c3f236e311008b462e5adc26b
Daniel Mao <danielmao2019@gmail.com>
2025-07-19 18:52:34 -0400

    f

--- demos/data/transforms/vision_3d/lidar_simulation_crop/webapp/backend/__init__.py
+++ demos/data/transforms/vision_3d/lidar_simulation_crop/webapp/backend/__init__.py
@@ -5,4 +5,5 @@ Backend submodule for LiDAR simulation cropping visualization.
 
 from demos.data.transforms.vision_3d.lidar_simulation_crop.webapp.backend.visualization import LiDARVisualizationBackend
 
+
 __all__ = ['LiDARVisualizationBackend']
