fc2ee3f402ae6c6aaea7ab02c3dccc8e994729d7
Daniel Mao <danielmao2019@gmail.com>
2025-07-26 12:24:47 -0400

    f

--- tests/data/datasets/change_detection_datasets/bi_temporal/levir_cd_dataset/test_levir_cd_dataset_version_dict.py
+++ tests/data/datasets/change_detection_datasets/bi_temporal/levir_cd_dataset/test_levir_cd_dataset_version_dict.py
@@ -1,7 +1,6 @@
 """Test version dict implementation for LevirCdDataset."""
 
 import pytest
-import tempfile
 import os
 from data.datasets.change_detection_datasets.bi_temporal.levir_cd_dataset import LevirCdDataset
 
@@ -27,23 +26,20 @@ def test_levir_cd_dataset_has_version_dict_method():
     assert return_annotation == Dict[str, Any] or str(return_annotation) == 'typing.Dict[str, typing.Any]'
 
 
-def test_levir_cd_dataset_version_dict_functionality(create_dummy_levir_structure):
+def test_levir_cd_dataset_version_dict_functionality(levir_cd_data_root):
     """Test that LevirCdDataset version dict method works correctly."""
     
-    with tempfile.TemporaryDirectory() as temp_dir:
-        create_dummy_levir_structure(temp_dir)
-        
-        dataset = LevirCdDataset(data_root=temp_dir, split='train')
-        version_dict = dataset._get_cache_version_dict()
-        
-        # Should return a dictionary
-        assert isinstance(version_dict, dict)
-        
-        # Should contain class_name
-        assert 'class_name' in version_dict
-        assert version_dict['class_name'] == 'LevirCdDataset'
-        
-        # Should contain base parameters
-        assert 'data_root' in version_dict
-        assert 'split' in version_dict
-        assert version_dict['split'] == 'train'
+    dataset = LevirCdDataset(data_root=levir_cd_data_root, split='train')
+    version_dict = dataset._get_cache_version_dict()
+    
+    # Should return a dictionary
+    assert isinstance(version_dict, dict)
+    
+    # Should contain class_name
+    assert 'class_name' in version_dict
+    assert version_dict['class_name'] == 'LevirCdDataset'
+    
+    # Should contain base parameters
+    assert 'data_root' in version_dict
+    assert 'split' in version_dict
+    assert version_dict['split'] == 'train'
