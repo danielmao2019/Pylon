52ce9405d4872c1a3424c65da4de81b1ff8c0aac
Daniel Mao <danielmao2019@gmail.com>
2025-07-26 17:38:49 -0400

    f

--- tests/data/datasets/change_detection_datasets/bi_temporal/air_change_dataset/test_air_change_dataset_version_dict.py
+++ tests/data/datasets/change_detection_datasets/bi_temporal/air_change_dataset/test_air_change_dataset_version_dict.py
@@ -1,11 +1,7 @@
 """Test version dict implementation for AirChangeDataset."""
 
 import pytest
-import tempfile
-import os
 from data.datasets.change_detection_datasets.bi_temporal.air_change_dataset import AirChangeDataset
-
-
 
 
 def test_air_change_dataset_has_version_dict_method():
@@ -27,35 +23,31 @@ def test_air_change_dataset_has_version_dict_method():
     assert return_annotation == Dict[str, Any] or str(return_annotation) == 'typing.Dict[str, typing.Any]'
 
 
-def test_air_change_dataset_version_dict_functionality(create_dummy_air_change_files):
+def test_air_change_dataset_version_dict_functionality(air_change_dataset_train):
     """Test that AirChangeDataset version dict method works correctly."""
     
-    with tempfile.TemporaryDirectory() as temp_dir:
-        create_dummy_air_change_files(temp_dir)
-        
-        dataset = AirChangeDataset(data_root=temp_dir, split='train')
-        version_dict = dataset._get_cache_version_dict()
-        
-        # Should return a dictionary
-        assert isinstance(version_dict, dict)
-        
-        # Should contain class_name
-        assert 'class_name' in version_dict
-        assert version_dict['class_name'] == 'AirChangeDataset'
-        
-        # Should contain base parameters
-        assert 'data_root' in version_dict
-        assert 'split' in version_dict
-        assert version_dict['split'] == 'train'
-        
-        # Should contain AirChangeDataset specific parameters
-        assert 'train_crops_per_image' in version_dict
-        assert 'image_size' in version_dict
-        assert 'test_crop_size' in version_dict
-        assert 'train_crop_size' in version_dict
-        
-        # Verify values match class constants
-        assert version_dict['train_crops_per_image'] == AirChangeDataset.TRAIN_CROPS_PER_IMAGE
-        assert version_dict['image_size'] == AirChangeDataset.IMAGE_SIZE
-        assert version_dict['test_crop_size'] == AirChangeDataset.TEST_CROP_SIZE
-        assert version_dict['train_crop_size'] == AirChangeDataset.TRAIN_CROP_SIZE
+    version_dict = air_change_dataset_train._get_cache_version_dict()
+    
+    # Should return a dictionary
+    assert isinstance(version_dict, dict)
+    
+    # Should contain class_name
+    assert 'class_name' in version_dict
+    assert version_dict['class_name'] == 'AirChangeDataset'
+    
+    # Should contain base parameters
+    assert 'data_root' in version_dict
+    assert 'split' in version_dict
+    assert version_dict['split'] == 'train'
+    
+    # Should contain AirChangeDataset specific parameters
+    assert 'train_crops_per_image' in version_dict
+    assert 'image_size' in version_dict
+    assert 'test_crop_size' in version_dict
+    assert 'train_crop_size' in version_dict
+    
+    # Verify values match class constants
+    assert version_dict['train_crops_per_image'] == AirChangeDataset.TRAIN_CROPS_PER_IMAGE
+    assert version_dict['image_size'] == AirChangeDataset.IMAGE_SIZE
+    assert version_dict['test_crop_size'] == AirChangeDataset.TEST_CROP_SIZE
+    assert version_dict['train_crop_size'] == AirChangeDataset.TRAIN_CROP_SIZE
