95eb6b4a210b4b18c8cb53f07c95f670bc053182
Daniel Mao <danielmao2019@gmail.com>
2025-07-29 14:41:01 -0400

    f

--- data/dataloaders/__init__.py
+++ data/dataloaders/__init__.py
@@ -5,7 +5,7 @@ from data.dataloaders.base_dataloader import BaseDataLoader
 from data.dataloaders.geotransformer_dataloader import GeoTransformerDataloader
 from data.dataloaders.overlappredator_dataloader import OverlapPredatorDataloader
 from data.dataloaders.buffer_dataloader import BufferDataloader
-from data.dataloaders.d3feat.d3feat_dataloader import D3FeatDataLoader
+from data.dataloaders.d3feat_dataloader import D3FeatDataLoader
 
 
 __all__ = (
