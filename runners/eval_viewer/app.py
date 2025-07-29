a9adf79bcf74dba461be0e4f897976358ee0d333
Daniel Mao <danielmao2019@gmail.com>
2025-07-28 21:42:01 -0400

    f

--- runners/eval_viewer/app.py
+++ runners/eval_viewer/app.py
@@ -73,7 +73,6 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true", help="Run in debug mode")
     parser.add_argument("--port", type=int, default=8050, help="Port number")
     parser.add_argument("--force-reload", action="store_true", help="Force recreation of cache")
-    parser.add_argument("--log-dirs", nargs="+", help="Paths to log directories to view")
     args = parser.parse_args()
 
     log_dirs = [
