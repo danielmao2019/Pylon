92a40f16bb8c9fe524eb83e3f14064ae7b17ef3a
Daniel Mao <danielmao2019@gmail.com>
2025-09-18 19:58:51 -0400

    [Data][Collators] Update D3Feat and OverlapPredator cpp wrappers setup files

--- data/collators/overlappredator/cpp_wrappers/cpp_neighbors/setup.py
+++ data/collators/overlappredator/cpp_wrappers/cpp_neighbors/setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup, Extension
-import numpy.distutils.misc_util
+from setuptools import setup, Extension
+import numpy
 
 # Adding OpenCV to project
 # ************************
@@ -14,10 +14,11 @@ SOURCES = ["../cpp_utils/cloud/cloud.cpp",
 module = Extension(name="radius_neighbors",
                     sources=SOURCES,
                     extra_compile_args=['-std=c++11',
-                                        '-D_GLIBCXX_USE_CXX11_ABI=0'])
+                                        '-D_GLIBCXX_USE_CXX11_ABI=0'],
+                    include_dirs=[numpy.get_include()])
 
 
-setup(ext_modules=[module], include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
+setup(ext_modules=[module])
 
 
 
