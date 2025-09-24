9de5f5710c744b6c5c9f28f8b03383c86172769b
Daniel Mao <danielmao2019@gmail.com>
2025-09-23 23:13:51 -0400

    f

---
+++ agents/connector/__init__.py
@@ -0,0 +1,12 @@
+"""Agents connector module (moved from utils.ssh)."""
+from agents.connector.pool import _ssh_pool, SSHConnectionPool
+from agents.connector.connector import SSHConnector, LocalhostConnector
+from agents.connector.result import SSHResult, LocalhostResult
+from agents.connector.error import SSHCommandError
+
+__all__ = [
+    '_ssh_pool', 'SSHConnectionPool',
+    'SSHConnector', 'LocalhostConnector',
+    'SSHResult', 'LocalhostResult',
+    'SSHCommandError',
+]
