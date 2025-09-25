9e48a15a03b294872e44d4257b817c28db726f04
Daniel Mao <danielmao2019@gmail.com>
2025-09-24 23:35:56 -0400

    f

--- tests/agents/manager/training_job/test_status_stuck.py
+++ tests/agents/manager/training_job/test_status_stuck.py
@@ -1,15 +1,2 @@
 from agents.manager.training_job import TrainingJob
 from agents.monitor.process_info import ProcessInfo
-
-
-
-def test_status_trainer_stuck_with_process_no_logs(temp_manager_root, write_config, make_trainer_epoch):
-    from agents.monitor.process_info import ProcessInfo
-    cfg = write_config('stuck.py', {'epochs': 10})
-    for i in range(3):
-        make_trainer_epoch('stuck', i)
-    proc = ProcessInfo(pid='1', user='u', cmd='python main.py --config-filepath ./configs/stuck.py', start_time='t')
-    job = TrainingJob('./configs/stuck.py')
-    job.populate(epochs=10, config_to_process_info={'./configs/stuck.py': proc}, sleep_time=1, outdated_days=30, force_progress_recompute=False)
-    assert job.status == 'stuck'
-
