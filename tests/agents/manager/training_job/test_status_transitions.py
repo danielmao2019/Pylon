0fd8412e7a54ba34fab89456878284e17f9227c0
Daniel Mao <danielmao2019@gmail.com>
2025-09-24 23:37:15 -0400

    f

--- tests/agents/manager/training_job/test_status_transitions.py
+++ tests/agents/manager/training_job/test_status_transitions.py
@@ -38,3 +38,15 @@ def test_status_trainer_running_with_recent_log(temp_manager_root, write_config,
     job = TrainingJob('./configs/running.py')
     job.populate(epochs=10, config_to_process_info={}, sleep_time=3600, outdated_days=30, force_progress_recompute=False)
     assert job.status == 'running'
+
+
+
+
+
+def test_status_trainer_finished_no_recent_logs(temp_manager_root, write_config, make_trainer_epoch):
+    cfg = write_config('finished.py', {'epochs': 3})
+    for i in range(3):
+        make_trainer_epoch('finished', i)
+    job = TrainingJob('./configs/finished.py')
+    job.populate(epochs=3, config_to_process_info={}, sleep_time=1, outdated_days=30, force_progress_recompute=False)
+    assert job.status == 'finished'
