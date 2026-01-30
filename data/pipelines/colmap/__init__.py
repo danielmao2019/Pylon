ccdd9d11f43902c55f11067cb9854b66abbe77e1
Daniel Mao <81886629+danielmao2019@users.noreply.github.com>
2026-01-29 23:37:36 -0500

    [Data][Pipelines][COLMAP] Restructure module and minor changes (#388)

--- data/pipelines/colmap/__init__.py
+++ data/pipelines/colmap/__init__.py
@@ -1,11 +1,11 @@
 """COLMAP pipeline implementation built from reusable steps."""
 
-from data.pipelines.colmap.colmap_commands_pipeline import ColmapCommandsPipeline
+from data.pipelines.colmap import core
 from data.pipelines.colmap.extract_cameras_step import ColmapExtractCamerasStep
 from data.pipelines.colmap.pipeline import ColmapPipeline
 
-__all__ = [
-    "ColmapPipeline",
-    "ColmapCommandsPipeline",
+__all__ = (
+    "core",
     "ColmapExtractCamerasStep",
-]
+    "ColmapPipeline",
+)
