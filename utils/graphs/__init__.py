c9142c3a83946696ef8eb772c0986d5aa1e11797
Daniel Mao <danielmao2019@gmail.com>
2026-02-14 11:55:42 -0500

    [Utils][Graphs] Expand connectivity utilities

--- utils/graphs/__init__.py
+++ utils/graphs/__init__.py
@@ -8,6 +8,10 @@ from utils.graphs.adjacency import (
     compute_weighted_degrees,
     compute_weighted_degrees_log1p,
 )
+from utils.graphs.components import (
+    build_induced_connected_components,
+    compute_component_overlap_counts,
+)
 from utils.graphs.connectivity import (
     compute_subset_connectivity,
     compute_subset_connectivity_log1p,
@@ -24,6 +28,8 @@ __all__ = (
     "compute_degrees",
     "compute_weighted_degrees",
     "compute_weighted_degrees_log1p",
+    "build_induced_connected_components",
+    "compute_component_overlap_counts",
     "compute_subset_connectivity",
     "compute_subset_connectivity_log1p",
     "count_non_subset_nodes_with_min_subset_neighbors",
